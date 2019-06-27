061780772d5cd0c08045ad0e3a1aada2de88cac9
Philipp M. Fischer <philipp.fischer@dlr.de>
2019-06-27 12:26:30 +0200

    Added API Call for automated Reviews - (Task #10)
    - Script is now adding a proper review to the report
    - Also sets approval in case everything is fine
    - Script now verifies if mailmap and known authors are untouched
    - Script also checks who initiated the pull request.
    
    ---
    Task #10: Provide Contribution Capabilities

--- bash/verify_commit_authors.sh
+++ bash/verify_commit_authors.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 echo "[Info] ------------------------------------"
 echo "[Info] Verify commit authors"
 echo "[Info] ------------------------------------"
@@ -9,17 +7,55 @@ echo "[Info] "
 
 echo "[Info] ------------------------------------"
 echo "[Info] Get the full history"
+echo "[Info] and prepare local git repo"
 echo "[Info] ------------------------------------"
 echo "[Info] "
 
-git fetch origin development development:development
-git fetch --unshallow
+# Test if it is a shallow repository as usually checked out
+# on travis ci build nodes
+
+if [ -f $(git rev-parse --git-dir)/shallow ]; then
+	echo "[Info] Current repository is shallow"
+	echo "[Info] Need to unshallow"
+	git fetch origin development development:development
+	git fetch --unshallow
+else
+	echo "[Info] Current repository is not shallow"
+fi
 
 echo "[Info] ------------------------------------"
 echo "[Info] Show current branches"
 echo "[Info] ------------------------------------"
 
 git branch -a
+
+echo "[Info] ------------------------------------"
+echo "[Info] Analyse authors integrity"
+echo "[Info] ------------------------------------"
+
+echo "[Info] Checking .mailmap"
+
+git diff --quiet development .mailmap
+CHANGED_MAILMAP=$?
+
+echo "[Info] Checking known_authors.txt"
+
+git diff --quiet development known_authors.txt
+CHANGED_KNOWN_AUTHORS=$?
+
+# Detect Travis CI Pull Request to find out the name of the author
+IS_PULL_REQUEST="false"
+if [ ! -v $TRAVIS_PULL_REQUEST ]; then 
+ 	echo "[Info] Detected TravisCI Pull Request Variable"
+  if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
+  	echo "[Info] Detected TravisCI Pull Request is  ${TRAVIS_PULL_REQUEST}"
+  	IS_PULL_REQUEST="true"
+  	PULL_REQUEST_AUTHOR=$(curl 'https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls/${TRAVIS_PULL_REQUEST}' | jq --raw-output '.user.login')
+  	echo "[Info] Pull Request Author ${PULL_REQUEST_AUTHOR}"
+  	grep -Fq ${PULL_REQUEST_AUTHOR} ./known_authors.txt
+  	PULL_REQUEST_AUTHOR_UNKNOWN=$?
+  fi 
+fi
 
 echo "[Info] ------------------------------------"
 echo "[Info] Create commit authors file"
@@ -30,7 +66,7 @@ git fetch origin development development:development
 git log development... --pretty=format:"%aN" | sort | uniq > ./commit_authors.txt
 
 echo "[Info] ------------------------------------"
-echo "[Info] List of Commits and authors"
+echo "[Info] List of Commits and Authors"
 echo "[Info] ------------------------------------"
 echo "[Info] "
 
@@ -51,22 +87,83 @@ echo "[Info] "
 
 cat ./known_authors.txt
 
-echo "[Info] "
+echo ""
 echo "[Info] ------------------------------------"
 echo "[Info] Verify against known_authors file"
 echo "[Info] ------------------------------------"
 echo "[Info] "
 
-if grep -v -F -f ./known_authors.txt ./commit_authors.txt ; then
-	
-	echo "[WARN] Unknown authors in commit history."
-	
-	if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-    	curl -H "Authorization: token ${GITHUB_API_TOKEN}" -X POST \
-		-d "{\"body\": \":warning: Unknown authors in commit History :warning:\"}" \
-		"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
-    fi
-	exit 21
+UNKNOWN_AUTHORS=$(grep -v -Fq -f ./known_authors.txt ./commit_authors.txt)
+
+echo "[Info] "
+echo "[Info] ------------------------------------"
+echo "[Info] generate Analysis Report"
+echo "[Info] ------------------------------------"
+echo "[Info] "
+
+# Set the Review Status to APPROVE
+# in case of one test failing, set it to REQUEST_CHANGES
+REVIEW_STATUS="APPROVE"
+
+REPORT=$'Author Verification Report \n'
+REPORT+=$'---------------------------\n\n'
+
+if [ -z "$UNKNOWN_AUTHORS" ]; then
+	REVIEW_STATUS="REQUEST_CHANGES"
+	REPORT+=$':x: Some Authors in commit History without CLA :x: \n'
 else
-	echo "[OK] Only known authors in commit history."
+	REPORT+=$':heavy_check_mark: All Authors in commit history with CLA. :heavy_check_mark: \n'	
 fi
+
+if [ $CHANGED_MAILMAP -ne 0 ]; then
+	REVIEW_STATUS="REQUEST_CHANGES"
+	REPORT+=$':warn: .mailmap file has been changed :warn: \n'
+else
+	REPORT+=$':heavy_check_mark: .mailmap file is not modified :heavy_check_mark: \n'	
+fi
+
+if [ $CHANGED_KNOWN_AUTHORS -ne 0 ]; then
+	REVIEW_STATUS="REQUEST_CHANGES"
+	REPORT+=$':warn: known_authors.txt file has been changed :warn: \n'
+else
+	REPORT+=$':heavy_check_mark: .known_authors file is not modified :heavy_check_mark: \n'	
+fi
+
+if [ "$IS_PULL_REQUEST" = "true" ]; then
+	if [ $PULL_REQUEST_AUTHOR_KNOWN -ne 0 ] ; then
+		REVIEW_STATUS="REQUEST_CHANGES"
+		REPORT+=$':warn: The author of the Pull Request has no CLA :warn: \n'
+	else
+		REPORT+=$':heavy_check_mark: The author of the pull :heavy_check_mark: \n'	
+	fi
+else
+	REPORT+=$'This is not a Pull Request \n'
+fi
+
+echo "[Info] "
+echo "[Info] ------------------------------------"
+echo "[Info] Create Pull Request Review"
+echo "[Info] ------------------------------------"
+echo "[Info] "
+	
+if [ "$IS_PULL_REQUEST" == "true" ] ; then
+    	# Write a comment to the Pull Request
+    	# curl -H "Authorization: token ${GITHUB_API_TOKEN}" -X POST \
+		# -d "{\"body\": \":warning: Unknown authors in commit History :warning:\"}" \
+		# "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+		
+		# Write a review and ask for changes
+		curl -H "Authorization: token ${GITHUB_API_TOKEN}" -X POST \
+		-d "{\"commit_id\": \"${TRAVIS_COMMIT}\", \"body\": \"${REPORT}\", \"event\": \"${REVIEW_STATUS}\"}" \
+		"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls/${TRAVIS_PULL_REQUEST}/reviews"
+fi
+
+echo "${REPORT}"
+if [ "$REVIEW_STATUS" == "APPROVE" ] ; then
+	echo "[Info] Report does not show anomalies"
+else
+	echo "[WARN] Report shows anomalies!"
+  exit 1
+fi
+
+    
