b2f4444b7cf07299c2278946899da87fb10d0000
Tobias Franz <tobias.franz@dlr.de>
2019-07-02 18:50:36 +0200

    Add new requirements concept - (Task #127)
    - add the concept file
    - the concept's generated code
    - maven files
    - license files
    - checkstyle files
    
    ---
    Task #127: Add requirements concept to model requirements within Virtual
    Satellite Core

---
+++ de.dlr.sc.virsat.model.extension.requirements.test/src-gen/de/dlr/sc/virsat/model/extension/requirements/test/AllTestsGen.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2019 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.sc.virsat.model.extension.requirements.test;
+
+// *****************************************************************
+// * Import Statements
+// *****************************************************************
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import junit.framework.JUnit4TestAdapter;
+
+import de.dlr.sc.virsat.model.extension.requirements.model.RequirementGroupTest;
+import de.dlr.sc.virsat.model.extension.requirements.model.RequirementsSpecificationTest;
+import de.dlr.sc.virsat.model.extension.requirements.model.EnumerationLiteralTest;
+import de.dlr.sc.virsat.model.extension.requirements.model.RequirementsConfigurationTest;
+import de.dlr.sc.virsat.model.extension.requirements.model.RequirementTypeTest;
+import de.dlr.sc.virsat.model.extension.requirements.model.EnumerationDefinitionTest;
+import de.dlr.sc.virsat.model.extension.requirements.migrator.Migrator1v0Test;
+import de.dlr.sc.virsat.model.extension.requirements.model.RequirementAttributeTest;
+import de.dlr.sc.virsat.model.extension.requirements.model.AttributeValueTest;
+import de.dlr.sc.virsat.model.extension.requirements.model.RequirementTest;
+
+/**
+ * 
+ */
+@RunWith(Suite.class)
+
+@SuiteClasses({
+	RequirementsSpecificationTest.class,
+	RequirementsConfigurationTest.class,
+	RequirementGroupTest.class,
+	RequirementTest.class,
+	AttributeValueTest.class,
+	RequirementTypeTest.class,
+	RequirementAttributeTest.class,
+	EnumerationDefinitionTest.class,
+	EnumerationLiteralTest.class,
+	Migrator1v0Test.class,
+				})
+
+/**
+ * 
+ * Test Collection
+ *
+ */
+public class AllTestsGen {
+
+	/**
+	 * Constructor
+	 */
+	private AllTestsGen() {
+	}
+	
+	/**
+	 * Test Adapter
+	 * @return Executable JUnit Tests
+	 */
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(AllTests.class);
+	}	
+}
---
+++ de.dlr.sc.virsat.model.extension.requirements/src-gen/de/dlr/sc/virsat/model/extension/requirements/model/ARequirementGroup.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2019 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.sc.virsat.model.extension.requirements.model;
+
+// *****************************************************************
+// * Import Statements
+// *****************************************************************
+import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
+import de.dlr.sc.virsat.model.extension.requirements.model.RequirementObject;
+import de.dlr.sc.virsat.model.concept.types.category.IBeanCategoryAssignment;
+import de.dlr.sc.virsat.model.dvlm.concepts.util.ActiveConceptHelper;
+import de.dlr.sc.virsat.model.dvlm.categories.CategoryAssignment;
+import de.dlr.sc.virsat.model.dvlm.categories.util.CategoryInstantiator;
+import de.dlr.sc.virsat.model.concept.list.IBeanList;
+import de.dlr.sc.virsat.model.concept.list.TypeSafeComposedPropertyInstanceList;
+import de.dlr.sc.virsat.model.dvlm.categories.Category;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.ArrayInstance;
+
+
+// *****************************************************************
+// * Class Declaration
+// *****************************************************************
+
+/**
+ * Auto Generated Abstract Generator Gap Class
+ * 
+ * Don't Manually modify this class
+ * 
+ * 
+ * 
+ */	
+public abstract class ARequirementGroup extends RequirementObject implements IBeanCategoryAssignment {
+
+	public static final String FULL_QUALIFIED_CATEGORY_NAME = "de.dlr.sc.virsat.model.extension.requirements.RequirementGroup";
+	
+	/**
+ 	* Call this method to get the full qualified name of the underlying category
+ 	* @return The FQN of the category as String
+ 	*/
+	public String getFullQualifiedCategoryName() {
+		return FULL_QUALIFIED_CATEGORY_NAME;
+	}
+	
+	// property name constants
+	public static final String PROPERTY_CHILDREN = "children";
+	
+	
+	
+	// *****************************************************************
+	// * Class Constructors
+	// *****************************************************************
+	
+	public ARequirementGroup() {
+	}
+	
+	public ARequirementGroup(Concept concept) {
+		Category categoryFromActiveCategories = ActiveConceptHelper.getCategory(concept, "RequirementGroup");
+		CategoryAssignment categoryAssignement = new CategoryInstantiator().generateInstance(categoryFromActiveCategories, "RequirementGroup");
+		setTypeInstance(categoryAssignement);
+	}
+	
+	public ARequirementGroup(CategoryAssignment categoryAssignement) {
+		setTypeInstance(categoryAssignement);
+	}
+	
+	
+	// *****************************************************************
+	// * Array Attribute: children
+	// *****************************************************************
+	private IBeanList<RequirementObject> children = new TypeSafeComposedPropertyInstanceList<>(RequirementObject.class);
+	
+	private void safeAccessChildren() {
+		if (children.getArrayInstance() == null) {
+			children.setArrayInstance((ArrayInstance) helper.getPropertyInstance("children"));
+		}
+	}
+	
+	public IBeanList<RequirementObject> getChildren() {
+		safeAccessChildren();
+		return children;
+	}
+	
+	
+}
