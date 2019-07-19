0f8dc9ebc7ec621ae66dace6feb869035770341f
Tobias Franz <tobias.franz@dlr.de>
2019-07-19 09:48:35 +0200

    Further improvement of UI requirement table - (Task #127)
    - Prepare creation of attribute instances to speed up setting values

--- de.dlr.sc.virsat.model.extension.requirements.ui/src/de/dlr/sc/virsat/model/extension/requirements/ui/command/InitializeRequirementCommand.java
+++ de.dlr.sc.virsat.model.extension.requirements.ui/src/de/dlr/sc/virsat/model/extension/requirements/ui/command/InitializeRequirementCommand.java
@@ -17,7 +17,10 @@ import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 
 import de.dlr.sc.virsat.model.dvlm.categories.CategoryAssignment;
+import de.dlr.sc.virsat.model.dvlm.concepts.util.ActiveConceptHelper;
+import de.dlr.sc.virsat.model.extension.requirements.model.AttributeValue;
 import de.dlr.sc.virsat.model.extension.requirements.model.Requirement;
+import de.dlr.sc.virsat.model.extension.requirements.model.RequirementAttribute;
 import de.dlr.sc.virsat.model.extension.requirements.model.RequirementType;
 
 /**
@@ -28,45 +31,63 @@ public class InitializeRequirementCommand extends RecordingCommand {
 
 	protected final Requirement requirement;
 	protected final RequirementType requirementType;
+	protected final TransactionalEditingDomain editingDomain;
 	protected static final String REQ_NAME_PREFIX = "req";
-	
+
 	/**
 	 * Restricted constructor
-	 * @param requirement the bean of the requirement 
-	 * @param requirementType the bean of the requirement type
-	 * @param domain the editing domain
+	 * 
+	 * @param requirement
+	 *            the bean of the requirement
+	 * @param requirementType
+	 *            the bean of the requirement type
+	 * @param domain
+	 *            the editing domain
 	 */
-	protected InitializeRequirementCommand(TransactionalEditingDomain domain, Requirement requirement, RequirementType requirementType) {
+	protected InitializeRequirementCommand(TransactionalEditingDomain domain, Requirement requirement,
+			RequirementType requirementType) {
 		super(domain);
 		this.requirement = requirement;
 		this.requirementType = requirementType;
+		this.editingDomain = domain;
 	}
-	
-	
+
 	/**
 	 * Create method for this command
-	 * @param requirement the {@link CategoryAssignment} of the new requirement
-	 * @param requirementType the {@link CategoryAssignment} of the requirement type
-	 * @param domain the editing domain
+	 * 
+	 * @param requirement
+	 *            the {@link CategoryAssignment} of the new requirement
+	 * @param requirementType
+	 *            the {@link CategoryAssignment} of the requirement type
+	 * @param domain
+	 *            the editing domain
 	 * @return the command to initialize the requirement
 	 */
-	public static Command create(TransactionalEditingDomain domain, CategoryAssignment requirement, CategoryAssignment requirementType) {
-		return new InitializeRequirementCommand(domain, new Requirement(requirement), new RequirementType(requirementType));
+	public static Command create(TransactionalEditingDomain domain, CategoryAssignment requirement,
+			CategoryAssignment requirementType) {
+		return new InitializeRequirementCommand(domain, new Requirement(requirement),
+				new RequirementType(requirementType));
 	}
-	
+
 	/**
 	 * Create method for this command
-	 * @param requirement the new requirement
-	 * @param requirementType the requirement type
-	 * @param domain the editing domain
+	 * 
+	 * @param requirement
+	 *            the new requirement
+	 * @param requirementType
+	 *            the requirement type
+	 * @param domain
+	 *            the editing domain
 	 * @return the command to initialize the requirement
 	 */
-	public static Command create(TransactionalEditingDomain domain, Requirement requirement, RequirementType requirementType) {
+	public static Command create(TransactionalEditingDomain domain, Requirement requirement,
+			RequirementType requirementType) {
 		return new InitializeRequirementCommand(domain, requirement, requirementType);
 	}
-	
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.emf.transaction.RecordingCommand#doExecute()
 	 */
 	@Override
@@ -74,6 +95,15 @@ public class InitializeRequirementCommand extends RecordingCommand {
 		requirement.setReqType(requirementType);
 		requirement.setName(REQ_NAME_PREFIX + requirementType.getName());
 		requirement.setStatus(Requirement.STATUS_Open_NAME);
+
+		// Also prepare attribute instances to simplify setting values later
+		for (RequirementAttribute attDef : requirementType.getAttributes()) {
+			AttributeValue attInstance = new AttributeValue(
+					ActiveConceptHelper.getConcept(requirement.getATypeInstance().getType()));
+			InitializeRequirementAttributeCommand composedCommand = new InitializeRequirementAttributeCommand(
+					editingDomain, attDef, requirement, attInstance);
+			composedCommand.execute();
+		}
 	}
 
 }
