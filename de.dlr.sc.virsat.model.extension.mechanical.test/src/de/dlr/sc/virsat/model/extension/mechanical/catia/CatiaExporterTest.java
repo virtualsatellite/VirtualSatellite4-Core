a8436a996e98db4bcceb1a4051eeff949abb1144
kova_an <andrii.kovalov@dlr.de>
2019-07-24 10:45:44 +0200

    Added export of Products to Json - (Task #173)
    ---
    Task #173: CATIA Export: Products

--- de.dlr.sc.virsat.model.extension.mechanical.test/src/de/dlr/sc/virsat/model/extension/mechanical/catia/CatiaExporterTest.java
+++ de.dlr.sc.virsat.model.extension.mechanical.test/src/de/dlr/sc/virsat/model/extension/mechanical/catia/CatiaExporterTest.java
@@ -25,6 +25,7 @@ import com.github.cliftonlabs.json_simple.JsonObject;
 import de.dlr.sc.virsat.concept.unittest.util.test.AConceptTestCase;
 import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
 import de.dlr.sc.virsat.model.extension.ps.model.ConfigurationTree;
+import de.dlr.sc.virsat.model.extension.ps.model.ElementConfiguration;
 import de.dlr.sc.virsat.model.extension.ps.model.ElementDefinition;
 import de.dlr.sc.virsat.model.extension.visualisation.model.Visualisation;
 
@@ -35,6 +36,7 @@ import de.dlr.sc.virsat.model.extension.visualisation.model.Visualisation;
 
 public class CatiaExporterTest extends AConceptTestCase {
 
+	private static final double EPSILON = 10e-6;
 	private Concept conceptPS;
 	private Concept conceptVis;
 	
@@ -45,7 +47,7 @@ public class CatiaExporterTest extends AConceptTestCase {
 	}
 	
 	@Test
-	public void testTransformConfigurationTree() {
+	public void testTransformEmptyConfigurationTree() {
 		ConfigurationTree ct = new ConfigurationTree(conceptPS);
 		
 		CatiaExporter catiaExporter = new CatiaExporter();
@@ -94,4 +96,110 @@ public class CatiaExporterTest extends AConceptTestCase {
 		assertEquals("Name should be copied", ed.getName(), jsonElement.getString(CatiaProperties.NAME));
 		assertEquals("UUID should be copied", ed.getUuid(), jsonElement.getString(CatiaProperties.UUID));
 	}
+
+	@Test
+	public void testTransformConfigurationTreeWithVisualisation() {
+		ConfigurationTree ct = new ConfigurationTree(conceptPS);
+		Visualisation visualisation = new Visualisation(conceptVis);
+		ct.add(visualisation);
+
+		ElementConfiguration ec = new ElementConfiguration(conceptPS);
+		ct.add(ec);
+		
+		final double POSITION_X = 1;
+		final double POSITION_Y = 2;
+		final double POSITION_Z = 3;
+
+		final double ROTATION_X = 0;
+		final double ROTATION_Y = 45;
+		final double ROTATION_Z = 90;
+		
+		visualisation.setShape(Visualisation.SHAPE_BOX_NAME);
+		visualisation.setPositionX(POSITION_X);
+		visualisation.setPositionY(POSITION_Y);
+		visualisation.setPositionZ(POSITION_Z);
+
+		visualisation.setRotationX(ROTATION_X);
+		visualisation.setRotationY(ROTATION_Y);
+		visualisation.setRotationZ(ROTATION_Z);
+		
+		CatiaExporter catiaExporter = new CatiaExporter();
+		JsonObject jsonRoot = catiaExporter.transform(ct);
+		
+		JsonArray jsonParts = jsonRoot.getCollection(CatiaProperties.PARTS);		
+		JsonObject jsonProductRoot = jsonRoot.getMap(CatiaProperties.PRODUCTS);
+		
+		assertTrue("There should be no part created", jsonParts.isEmpty());
+		
+		assertEquals(POSITION_X, jsonProductRoot.getDouble(CatiaProperties.PRODUCT_POS_X), EPSILON);
+		assertEquals(POSITION_Y, jsonProductRoot.getDouble(CatiaProperties.PRODUCT_POS_Y), EPSILON);
+		assertEquals(POSITION_Z, jsonProductRoot.getDouble(CatiaProperties.PRODUCT_POS_Z), EPSILON);
+		
+		assertEquals(ROTATION_X, jsonProductRoot.getDouble(CatiaProperties.PRODUCT_ROT_X), EPSILON);
+		assertEquals(ROTATION_Y, jsonProductRoot.getDouble(CatiaProperties.PRODUCT_ROT_Y), EPSILON);
+		assertEquals(ROTATION_Z, jsonProductRoot.getDouble(CatiaProperties.PRODUCT_ROT_Z), EPSILON);
+
+		assertEquals(Visualisation.SHAPE_BOX_NAME, jsonProductRoot.getString(CatiaProperties.PRODUCT_SHAPE));
+
+		JsonArray children = jsonProductRoot.getCollection(CatiaProperties.PRODUCT_CHILDREN);
+		assertTrue(children.isEmpty());
+	}
+
+	@Test
+	public void testTransformConfigurationTreeWithVisualisationChild() {
+		ConfigurationTree ct = new ConfigurationTree(conceptPS);
+		ElementConfiguration ec = new ElementConfiguration(conceptPS);
+		Visualisation visualisation = new Visualisation(conceptVis);
+		ec.add(visualisation);
+		
+		ct.add(ec);
+		
+		final double POSITION_X = 1;
+		final double POSITION_Y = 2;
+		final double POSITION_Z = 3;
+
+		final double ROTATION_X = 0;
+		final double ROTATION_Y = 45;
+		final double ROTATION_Z = 90;
+		
+		visualisation.setShape(Visualisation.SHAPE_BOX_NAME);
+		visualisation.setPositionX(POSITION_X);
+		visualisation.setPositionY(POSITION_Y);
+		visualisation.setPositionZ(POSITION_Z);
+
+		visualisation.setRotationX(ROTATION_X);
+		visualisation.setRotationY(ROTATION_Y);
+		visualisation.setRotationZ(ROTATION_Z);
+		
+		CatiaExporter catiaExporter = new CatiaExporter();
+		JsonObject jsonRoot = catiaExporter.transform(ct);
+		
+		JsonArray jsonParts = jsonRoot.getCollection(CatiaProperties.PARTS);		
+		JsonObject jsonProductRoot = jsonRoot.getMap(CatiaProperties.PRODUCTS);
+		
+		assertTrue("There should be no part created", jsonParts.isEmpty());
+		
+		assertEquals(0, jsonProductRoot.getDouble(CatiaProperties.PRODUCT_POS_X), EPSILON);
+		assertEquals(0, jsonProductRoot.getDouble(CatiaProperties.PRODUCT_POS_Y), EPSILON);
+		assertEquals(0, jsonProductRoot.getDouble(CatiaProperties.PRODUCT_POS_Z), EPSILON);
+		
+		assertEquals(0, jsonProductRoot.getDouble(CatiaProperties.PRODUCT_ROT_X), EPSILON);
+		assertEquals(0, jsonProductRoot.getDouble(CatiaProperties.PRODUCT_ROT_Y), EPSILON);
+		assertEquals(0, jsonProductRoot.getDouble(CatiaProperties.PRODUCT_ROT_Z), EPSILON);
+		assertEquals(Visualisation.SHAPE_NONE_NAME, jsonProductRoot.getString(CatiaProperties.PRODUCT_SHAPE));
+
+		JsonArray children = jsonProductRoot.getCollection(CatiaProperties.PRODUCT_CHILDREN);
+		assertEquals(1, children.size());
+		JsonObject subProduct = children.getMap(0);
+		
+		assertEquals(POSITION_X, subProduct.getDouble(CatiaProperties.PRODUCT_POS_X), EPSILON);
+		assertEquals(POSITION_Y, subProduct.getDouble(CatiaProperties.PRODUCT_POS_Y), EPSILON);
+		assertEquals(POSITION_Z, subProduct.getDouble(CatiaProperties.PRODUCT_POS_Z), EPSILON);
+		
+		assertEquals(ROTATION_X, subProduct.getDouble(CatiaProperties.PRODUCT_ROT_X), EPSILON);
+		assertEquals(ROTATION_Y, subProduct.getDouble(CatiaProperties.PRODUCT_ROT_Y), EPSILON);
+		assertEquals(ROTATION_Z, subProduct.getDouble(CatiaProperties.PRODUCT_ROT_Z), EPSILON);
+		assertEquals(Visualisation.SHAPE_BOX_NAME, subProduct.getString(CatiaProperties.PRODUCT_SHAPE));
+	}
+
 }
