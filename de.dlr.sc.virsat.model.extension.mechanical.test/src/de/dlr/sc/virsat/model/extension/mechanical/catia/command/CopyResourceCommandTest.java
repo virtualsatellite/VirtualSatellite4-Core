0c03ac6c9414b8d1a0335ae99cc83a87f5bc5521
Tobias Franz <tobias.franz@dlr.de>
2019-08-06 12:56:08 +0200

    Add a test class for the copy resource command  - (Task #191)

---
+++ de.dlr.sc.virsat.model.extension.mechanical.test/src/de/dlr/sc/virsat/model/extension/mechanical/catia/command/CopyResourceCommandTest.java
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2019 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.sc.virsat.model.extension.mechanical.catia.command;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test class for the copy resource command
+ *
+ */
+public class CopyResourceCommandTest {
+	
+	private static final String TEST_FILE_NAME = "dummy.test";
+	private static List<String> testContent;
+	private static Path testFolderPath;
+	private static Path testFile;
+	
+	@Before
+	public void setUp() throws CoreException, IOException {
+		testFolderPath = Files.createTempDirectory("catiaTest");
+		
+		testFile = Paths.get(testFolderPath.toString(), TEST_FILE_NAME);
+		testContent = Arrays.asList("solid test", "endsolid test");
+		Files.write(testFile, testContent);
+	}
+	
+	
+	@Test
+	public void testCanExecute() throws IOException {
+
+		Path nonExistingFile = Paths.get(TEST_FILE_NAME, "randomFile.bla");
+		
+		CopyResourceCommand commandInvalid1 = new CopyResourceCommand(nonExistingFile, testFolderPath);
+		assertFalse("Cannot copy folder", commandInvalid1.canExecute());
+
+		CopyResourceCommand commandInvalid2 = new CopyResourceCommand(null, testFolderPath);
+		assertFalse("Cannot copy null path", commandInvalid2.canExecute());
+		
+		final String TEST_TARGET_FILE_NAME = "target.test";
+		Path testTarget = Paths.get(testFolderPath.toString(), TEST_TARGET_FILE_NAME);
+		CopyResourceCommand commandValid = new CopyResourceCommand(testFile, testTarget);
+		assertTrue(commandValid.canExecute());
+	}
+	
+	
+	@Test
+	public void testExecute() throws IOException {
+		
+		final String TEST_TARGET_FILE_NAME = "target.test";
+		Path testTarget = Paths.get(testFolderPath.toString(), TEST_TARGET_FILE_NAME);
+		
+		CopyResourceCommand command = new CopyResourceCommand(testFile, testTarget);
+		command.execute();
+		
+		assertTrue("File copied? ", testTarget.toFile().exists());
+		assertArrayEquals("File is copied correctly", Files.readAllBytes(testFile),
+				Files.readAllBytes(testTarget));
+		
+	}
+	
+	@Test
+	public void testUndoRedo() throws IOException {
+		
+		final String TEST_TARGET_FILE_NAME = "target.test";
+		Path testTarget = Paths.get(testFolderPath.toString(), TEST_TARGET_FILE_NAME);
+		
+		CopyResourceCommand command = new CopyResourceCommand(testFile, testTarget);
+		command.execute();
+		
+		assertTrue("File copied? ", testTarget.toFile().exists());
+		
+		command.undo();
+		
+		assertFalse("File copy undone ", testTarget.toFile().exists());
+		
+		command.redo();
+		
+		assertTrue("File copied again? ", testTarget.toFile().exists());
+		
+	}
+
+}
