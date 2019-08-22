71cad7f120e675099ba6b463d8041b9b33050161
Tobias Franz <tobias.franz@dlr.de>
2019-08-22 16:53:37 +0200

    Improve naming - (Task #228)

--- de.dlr.sc.virsat.model.concept.test/resources/expectedOutputFilesForGenerators/ManifestMF.java
+++ de.dlr.sc.virsat.model.concept.test/resources/expectedOutputFilesForGenerators/ManifestMF.java
@@ -13,7 +13,7 @@ public class ManifestMf {
 	public static final String EXPORT_PACKAGE = "de.dlr.sc.virsat.model.extension.testResource,de.dlr.sc.virsat.model.extension.testResource.html,de.dlr.sc.virsat.model.extension.testResource.model,de.dlr.sc.virsat.model.extension.testResource.model.util,de.dlr.sc.virsat.model.extension.testResource.validator";
 	public static final String BUNDLE_NAME = "VirSat Test Resource Access Builder";
 	public static final String BUNDLE_VERSION = "4.5.0.qualifier";
-	public static final String REQUIRE_BUNDLE = "org.eclipse.ui,org.eclipse.core.runtime,de.dlr.sc.virsat.model,de.dlr.sc.virsat.model.edit,de.dlr.sc.virsat.project,org.eclipse.emf.edit,de.dlr.sc.virsat.model.extension.ps,de.dlr.sc.virsat.build,de.dlr.sc.virsat.apps,com.google.guava,org.eclipse.xtext.xbase.lib,org.eclipse.xtend.lib,org.eclipse.xtend.lib.macro,de.dlr.sc.virsat.external.lib.apache.poi,de.dlr.sc.virsat.s2tep.excel";
+	public static final String REQUIRE_BUNDLE = "org.eclipse.ui,org.eclipse.core.runtime,de.dlr.sc.virsat.model,de.dlr.sc.virsat.model.edit,de.dlr.sc.virsat.project,org.eclipse.emf.edit,de.dlr.sc.virsat.model.extension.ps,de.dlr.sc.virsat.build,de.dlr.sc.virsat.apps,com.google.guava,org.eclipse.xtext.xbase.lib,org.eclipse.xtend.lib,org.eclipse.xtend.lib.macro,de.dlr.sc.virsat.external.lib.apache.poi,de.dlr.sc.virsat.excel";
 	public static final String BUNDLE_MANIFESTVERSION = "2";
 	public static final String BUNDLE_REQUIREDEXECUTIONENVIRONMENT = "JavaSE-1.8";
 	public static final String BUNDLE_ACTIVATIONPOLICY = "lazy";
