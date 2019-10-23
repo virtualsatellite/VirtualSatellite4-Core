1b432d54ce1048a502f37588756f8aba8494adfd
Tobias Franz <tobias.franz@dlr.de>
2019-10-23 16:32:37 +0200

    Add concept projects for generic category - (Task #312)

---
+++ de.dlr.sc.virsat.model.extension.core.test/src-gen/de/dlr/sc/virsat/model/extension/core/test/AllTestsGen.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2019 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.sc.virsat.model.extension.core.test;
+
+// *****************************************************************
+// * Import Statements
+// *****************************************************************
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import junit.framework.JUnit4TestAdapter;
+
+import de.dlr.sc.virsat.model.extension.core.model.GenericCategoryTest;
+import de.dlr.sc.virsat.model.extension.core.migrator.Migrator1v0Test;
+
+/**
+ * 
+ */
+@RunWith(Suite.class)
+
+@SuiteClasses({
+	GenericCategoryTest.class,
+	Migrator1v0Test.class,
+				})
+
+/**
+ * 
+ * Test Collection
+ *
+ */
+public class AllTestsGen {
+
+	/**
+	 * Constructor
+	 */
+	private AllTestsGen() {
+	}
+	
+	/**
+	 * Test Adapter
+	 * @return Executable JUnit Tests
+	 */
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(AllTests.class);
+	}	
+}
