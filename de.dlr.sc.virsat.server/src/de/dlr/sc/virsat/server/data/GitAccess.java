a29360fc7b36f2d8fe6c2afd23038eeaa15d4468
Diana Peters <diana.peters@dlr.de>
2019-11-19 17:53:22 +0100

    Fix extraction of repo directory name - (Task #359)
    Use apache.commons.io to extract directory name from git URI.
    
    ---
    Task #359: Add JGit dependencies and make it available via rest
    interface

--- de.dlr.sc.virsat.server/src/de/dlr/sc/virsat/server/data/GitAccess.java
+++ de.dlr.sc.virsat.server/src/de/dlr/sc/virsat/server/data/GitAccess.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.apache.commons.io.FilenameUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.AbortedByHookException;
 import org.eclipse.jgit.api.errors.ConcurrentRefUpdateException;
@@ -152,7 +153,7 @@ public class GitAccess {
 		String directory = "";
 		try {
 			url = new URL(uri);
-			directory = url.getFile(); // TODO: better extraction
+			directory = FilenameUtils.getBaseName(url.getPath());
 		} catch (MalformedURLException e1) {
 			e1.printStackTrace();
 		}
