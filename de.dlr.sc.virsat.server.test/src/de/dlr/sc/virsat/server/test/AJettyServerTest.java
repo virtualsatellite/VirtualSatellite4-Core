cd76cabdcd0ac7916b6cc1a6196fd65c9aa49707
Philipp M. Fischer <philipp.fischer@dlr.de>
2019-11-19 12:52:05 +0100

    Started to implement test cases - (Task #359)
    - Trying to setup git repository beforehand.
    
    ---
    Task #359: Add JGit dependencies and make it available via rest
    interface

--- de.dlr.sc.virsat.server.test/src/de/dlr/sc/virsat/server/test/AJettyServerTest.java
+++ de.dlr.sc.virsat.server.test/src/de/dlr/sc/virsat/server/test/AJettyServerTest.java
@@ -9,11 +9,30 @@
  *******************************************************************************/
 package de.dlr.sc.virsat.server.test;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 
 public abstract class AJettyServerTest {
 
+	@BeforeClass
+	public void setUpClass() throws IOException, IllegalStateException, GitAPIException {
+		// Create an upstream repository
+		File pathToUpstreamRepository = File.createTempFile("VirSatUpstreamRepo", "");
+        Files.delete(pathToUpstreamRepository.toPath());
+		
+		Git git = Git.init().setDirectory(pathToUpstreamRepository).call();
+		assertTrue(git.status().call().isClean());
+	}
+	
 	@Before
 	public void setUp() throws Exception {
 	}
