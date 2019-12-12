5aac68f140d5bc88207654a564c7dda788374d08
Philipp M. Fischer <philipp.fischer@dlr.de>
2019-12-12 15:22:46 +0100

    Trying to generalize a Transactional Builder - (Task #402)
    - Extracted Abstract Class from VirSatInheirtance Builder
    - Refactored Inheritance Builder to improve testability
    
    ---
    Task #402: Trying to stabilize Code Coverage Reports

--- de.dlr.sc.virsat.build/src/de/dlr/sc/virsat/build/inheritance/VirSatInheritanceBuilder.java
+++ de.dlr.sc.virsat.build/src/de/dlr/sc/virsat/build/inheritance/VirSatInheritanceBuilder.java
@@ -16,19 +16,15 @@ import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
-import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
-import org.eclipse.emf.common.command.AbstractCommand;
-import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 
@@ -38,166 +34,37 @@ import de.dlr.sc.virsat.model.dvlm.inheritance.IInheritanceCopier;
 import de.dlr.sc.virsat.model.dvlm.inheritance.InheritanceCopier;
 import de.dlr.sc.virsat.model.dvlm.structural.StructuralElementInstance;
 import de.dlr.sc.virsat.project.Activator;
-import de.dlr.sc.virsat.project.editingDomain.VirSatEditingDomainRegistry;
-import de.dlr.sc.virsat.project.editingDomain.VirSatTransactionalEditingDomain;
 import de.dlr.sc.virsat.project.resources.VirSatResourceSet;
 import de.dlr.sc.virsat.project.structure.VirSatProjectCommons;
 
 /**
  * the VirSatProjectbuilder our contribution to automatically 'build' our model. As a first implementation  
- * @author fisc_ph
  *
  */
-public class VirSatInheritanceBuilder extends IncrementalProjectBuilder {
+public class VirSatInheritanceBuilder extends AVirSatTransactionalBuilder {
 
 	public static final String BUILDER_ID = "de.dlr.sc.virsat.build.inheritance";
 
+	protected VirSatInheritanceMarkerHelper vimHelper; 
+	
 	/**
 	 * public constructor
 	 */
 	public VirSatInheritanceBuilder() {
+		super(new VirSatInheritanceMarkerHelper());
+		this.vimHelper = (VirSatInheritanceMarkerHelper) this.vpmHelper;
 	}
 
-	@Override
-	protected IProject[] build(int kind, Map<String, String> args, IProgressMonitor monitor) throws CoreException {
-		IProject project = getVirSatProject();
-		
-		// The builder is supposed to build the project in an unmanaged resourceSet
-		// but since we can have inconsistencies between the resource on the eclipse workspace and the in memory representation
-		// of EMF currently displayed by the managed ResourceSet in the navigator, it can happen that newly added elements disappear because the resources in the WS doesn't knwo references
-		// and stores them once the builder is finished. This behavior is prevented by first asking the projects managed resource set
-		
-	
-		Activator.getDefault().getLog().log(new Status(Status.INFO, Activator.getPluginId(), Status.OK, "VirSatInheritanceBuilder: Try to trigger build", null));
-		
-		if (isInterrupted() || monitor.isCanceled()) {
-			rememberLastBuiltState();
-			return null;
-		}
-		
-		switch (kind) {  
-			case FULL_BUILD:
-				transactionalFullBuild(monitor, new InheritanceCopier());
-				break;  
-			case INCREMENTAL_BUILD:
-			case AUTO_BUILD:
-				IResourceDelta delta = getDelta(project);
-				if (delta == null) {
-					Activator.getDefault().getLog().log(new Status(Status.INFO, Activator.getPluginId(), Status.OK, "VirSatInheritanceBuilder: Performing full build", null));
-					transactionalFullBuild(monitor, new InheritanceCopier());
-				} else {
-					Activator.getDefault().getLog().log(new Status(Status.INFO, Activator.getPluginId(), Status.OK, "VirSatInheritanceBuilder: Performing incremental build", null));
-					transactionalIncrementalBuild(delta, monitor, new InheritanceCopier());
-				}
-				break;
-			default:
-				break;  
-		}
-		
-		Activator.getDefault().getLog().log(new Status(Status.INFO, Activator.getPluginId(), Status.OK, "VirSatInheritanceBuilder: Finished build", null));
-		
-		return null; 
-	}
-	
-	/**
-	 * the transactionalfullBuild method is executed to trigger a build with all necessary steps using an editing domain
-	 * @param monitor monitor the progress
-	 * @param inheritanceCopier  the copier to be actually used
-	 */
-	protected void transactionalFullBuild(IProgressMonitor monitor, IInheritanceCopier inheritanceCopier) {
-		VirSatTransactionalEditingDomain virSatTed = VirSatEditingDomainRegistry.INSTANCE.getEd(getVirSatProject());
-		
-		if (virSatTed == null) {
-			return;
-		}
-		
-		// Clean all Markers in Workspace and nested resources
-		vimHelper.deleteAllMarkersInWorkspace();
-		
-		Command cmd = new AbstractCommand() {
-			@Override
-			public void execute() {
-				fullBuild(monitor, inheritanceCopier);
-				virSatTed.saveAll(true, true);
-			}
-
-			@Override
-			public void redo() {
-				
-			}
-			
-			@Override
-			public boolean canUndo() {
-				return true;
-			}
-			
-			@Override
-			public boolean canExecute() {
-				return true;
-			}
-		};
-		
-		if (virSatTed.getActiveTransaction() != null) {
-			rememberLastBuiltState();
-			return;
-		}
-		
-		virSatTed.getVirSatCommandStack().executeNoUndo(cmd);
-	}
-	
-	/**
-	 * Method for incremental build using an editing domain
-	 * @param delta the delta from the BUilderManager 
-	 * @param monitor a progress monitor to track what is going on
-	 * @param inheritanceCopier  the copier to be actually used
-	 */
-	protected void transactionalIncrementalBuild(IResourceDelta delta, IProgressMonitor monitor, IInheritanceCopier inheritanceCopier) {
-		VirSatTransactionalEditingDomain virSatTed = VirSatEditingDomainRegistry.INSTANCE.getEd(getVirSatProject());
-		
-		if (virSatTed == null) {
-			return;
-		}
-
-		Command cmd = new AbstractCommand() {
-			@Override
-			public void execute() {
-				incrementalBuild(delta, monitor, inheritanceCopier);
-				virSatTed.saveAll(true, true);
-			}
-
-			@Override
-			public void redo() {
-				
-			}
-			
-			@Override
-			public boolean canUndo() {
-				return true;
-			}
-			
-			@Override
-			public boolean canExecute() {
-				return true;
-			}
-		};
-		
-		if (virSatTed.getActiveTransaction() != null) {
-			rememberLastBuiltState();
-			return;
-		}
-		
-		virSatTed.getVirSatCommandStack().executeNoUndo(cmd);
-	}
-	
-	private VirSatInheritanceMarkerHelper vimHelper = new VirSatInheritanceMarkerHelper();
-	
 	/**
 	 * the fullBuild method is executed to trigger a build with all necessary steps.
 	 * @param monitor monitor the progress
 	 * @param inheritanceCopier  the copier to be actually used
 	 */
-	protected void fullBuild(IProgressMonitor monitor, IInheritanceCopier inheritanceCopier) {
+	@Override
+	protected void fullBuild(IProgressMonitor monitor) {
 		final int MAX_TASKS = 3;
+		IInheritanceCopier inheritanceCopier = createInheritanceCopier();
+		
 		SubMonitor subMonitor = SubMonitor.convert(monitor, MAX_TASKS);
 		subMonitor.beginTask("Run Full Inheritance Build...", MAX_TASKS);
 		Activator.getDefault().getLog().log(new Status(Status.INFO, Activator.getPluginId(), Status.OK, "VirSatInheritanceBuilder: Starting full build", null));
@@ -261,8 +128,10 @@ public class VirSatInheritanceBuilder extends IncrementalProjectBuilder {
 	 * @param monitor a progress monitor to track what is going on
 	 * @param inheritanceCopier  the copier to be actually used
 	 */
-	protected void incrementalBuild(IResourceDelta delta, IProgressMonitor monitor, IInheritanceCopier inheritanceCopier) {
+	@Override
+	protected void incrementalBuild(IResourceDelta delta, IProgressMonitor monitor) {
 		final int MAX_TASKS = 3;
+		IInheritanceCopier inheritanceCopier = createInheritanceCopier();
 		
 		SubMonitor subMonitor = SubMonitor.convert(monitor, MAX_TASKS);
 		subMonitor.beginTask("Run Incremental Inheritance Build...", MAX_TASKS);
@@ -317,25 +186,16 @@ public class VirSatInheritanceBuilder extends IncrementalProjectBuilder {
 		Activator.getDefault().getLog().log(new Status(Status.INFO, Activator.getPluginId(), Status.OK, "VirSatInheritanceBuilder: Finished incremental build", null));
 	}
 	
-	/**
-	 * Facade method to be able to override getProject method
-	 * for the test cases of this tester
-	 * @return hands back the current project the builder is triggered on
-	 */
-	protected IProject getVirSatProject() {
-		return getProject();
-	}
-	
-	/**
-	 * Overrideable method to provide the resource set for the use of non transactional testers
-	 * @return gets the resource set this builder operates on
-	 */
-	protected VirSatResourceSet getResourceSet() {
-		return VirSatResourceSet.getResourceSet(getVirSatProject());
-	}
-	
 	@Override
 	public ISchedulingRule getRule(int kind, Map<String, String> args) {
 		return null;
+	}
+	
+	/**
+	 * This method creates an instance of the Inheritance Copier
+	 * @return A new instance of the Inheritance Copier
+	 */
+	protected IInheritanceCopier createInheritanceCopier() {
+		return new InheritanceCopier();
 	}
 }
