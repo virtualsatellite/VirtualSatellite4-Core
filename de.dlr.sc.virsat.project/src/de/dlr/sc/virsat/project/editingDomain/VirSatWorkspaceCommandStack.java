0f310642aaef04b44a8fc9e90181c6cf433d2b4b
Philipp M. Fischer <philipp.fischer@dlr.de>
2019-12-13 13:43:43 +0100

    Command Stack now synchronized with workspace - (Task #402)
    _ Changed the command stack to always lock the workspace when executing
    commands.
    
    ---
    Task #402: Trying to stabilize Code Coverage Reports

--- de.dlr.sc.virsat.project/src/de/dlr/sc/virsat/project/editingDomain/VirSatWorkspaceCommandStack.java
+++ de.dlr.sc.virsat.project/src/de/dlr/sc/virsat/project/editingDomain/VirSatWorkspaceCommandStack.java
@@ -10,9 +10,11 @@
 package de.dlr.sc.virsat.project.editingDomain;
 
 import java.util.Map;
-
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.operations.IOperationHistory;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.command.Command;
@@ -40,29 +42,52 @@ public class VirSatWorkspaceCommandStack extends WorkspaceCommandStackImpl {
 	private boolean triggerSave;
 	
 	/**
+	 * This workspace is used as lock to synchronize the workspace operations and
+	 * the transactions on the editing domain. E.g. The builder starts and locks the
+	 * workspace, then it tries to place a non undoable command into the stack. in the meantime
+	 * a user operation created a new SEI, this has been placed as a command into the stack in between.
+	 * This command will try to write to the workspace which is locked. The builder will get stuck
+	 * because it tries to execute a new command on the stack but cannot acquire the transaction.
+	 * 
+	 * As a way out, this lock will be used to first synchronize all operations executing a command.
+	 * This ensures that a command is always executed after another one. Second every command will first try
+	 * to lock the workspace. Then it will try to get the transaction. Commands which are already in a locked 
+	 * workspace will reenter the lock.
+	 */
+	private IWorkspace wsProject;
+	
+	/**
 	 * Constructor to the Command Stack with the OperationsHistory
 	 * @param history The operation history to be used with this domain
 	 */
 	public VirSatWorkspaceCommandStack(IOperationHistory history) {
 		super(history);
+		wsProject = ResourcesPlugin.getWorkspace();
 	}
 	
 	/**
 	 * Method to execute a command and directly take it out of the
-	 * history, so it cannot be undone.
+	 * history, so it cannot be undone. The Transaction.OPTION_NO_UNDO 
+	 * seems to be not implemented.
 	 * @param command The command to be executed.
 	 */
 	public void executeNoUndo(Command command) {
-		// Check if the command is execute able and prepare it (happens in canExecute)
-		if (command.canExecute()) {
-			// Wrap it into an EMF operation and execute the command ourselves without
-			// using the history to prevent the operation from being listed in there
-			EMFCommandOperation operation = new EMFCommandOperation(getDomain(), command);
-			try {
-				operation.execute(null, null);
-			} catch (ExecutionException e) {
-				Activator.getDefault().getLog().log(new Status(Status.ERROR, Activator.getPluginId(), "VirSatWorkspaceCommandStack: Failed to execute command without undo ", e));
-			}
+		try {
+			wsProject.run(action -> {
+				// Check if the command is execute able and prepare it (happens in canExecute)
+				if (command.canExecute()) {
+					// Wrap it into an EMF operation and execute the command ourselves without
+					// using the history to prevent the operation from being listed in there
+					EMFCommandOperation operation = new EMFCommandOperation(getDomain(), command);
+					try {
+						operation.execute(null, null);
+					} catch (ExecutionException e) {
+						Activator.getDefault().getLog().log(new Status(Status.ERROR, Activator.getPluginId(), "VirSatWorkspaceCommandStack: Failed to execute command without undo ", e));
+					}
+				}
+			}, null);
+		} catch (CoreException e) {
+			Activator.getDefault().getLog().log(new Status(Status.ERROR, Activator.getPluginId(), "VirSatWorkspaceCommandStack: Failed to execute command without undo as workspace operation", e));
 		}
 	}
 	
@@ -80,61 +105,82 @@ public class VirSatWorkspaceCommandStack extends WorkspaceCommandStackImpl {
 			command = new VirSatRecordingCommand(command);
 		}
 		
-		triggerSave = false;
+		final Command executeCommand = command;
+	
 		try {
-			// Try to see if there is already a transaction going on in the editing domain,
-			// which is associated with this Command Stack.
-			InternalTransaction activeTransaction = getDomain().getActiveTransaction();
-			
-			// In case it exists, we want to now check if it is reusable for us. Which means
-			// like if a recording command is executed, which also tries to remove a file from a SEI,
-			// These commands shall be executed in one atomic transaction. This is implemented for making 
-			// Graphiti Framework work for us. Important is, that a transaction can only be rentered by
-			// the same thread. Thus commands within a recording command can be placed into the same transaction
-			// A command from a different thread can not be placed into the already open transaction.
-			boolean usableTransactionExists = activeTransaction != null && !activeTransaction.isReadOnly()
-					&& activeTransaction.getOwner().equals(Thread.currentThread());
-			
-			// The standard case, is that there is a new command which should be executed.
-			// Since there is usually no transaction yet, it will be forwarded to the Standard Workspace
-			// Command Stack which opens a transaction and starts executing the command. Or in case there is a transaction
-			// it will be just placed on the stack as the next command.
-			if (!usableTransactionExists) {
-				// No active transaction means we have to execute the command as a
-				// top-level command
-				super.execute(command, options);
-			} else {
-				// now where a transaction for the same thread exists, the command itself will be executed
-				// within exactly that transaction. In case that the command which is already executed by the 
-				// transaction is a VirtualSatelliteRecordCommand, the executed command will be chained to it.
-				// This way it looks to the CommandStack as if only the VirSatRecordingCommand was executed.
-				// Now when e.g. an undo is triggered, the chained commands will be undone as well within the
-				// same atomic transaction.
-				if (command != null && command.canExecute()) {
-					command.execute();
-					if (getActiveCommand() instanceof VirSatRecordingCommand) {
-						getActiveCommand().chain(command);
+			wsProject.run(action -> {
+				
+				triggerSave = false;
+				try {
+					// Try to see if there is already a transaction going on in the editing domain,
+					// which is associated with this Command Stack.
+					InternalTransaction activeTransaction = getDomain().getActiveTransaction();
+					
+					// In case it exists, we want to now check if it is reusable for us. Which means
+					// like if a recording command is executed, which also tries to remove a file from a SEI,
+					// These commands shall be executed in one atomic transaction. This is implemented for making 
+					// Graphiti Framework work for us. Important is, that a transaction can only be rentered by
+					// the same thread. Thus commands within a recording command can be placed into the same transaction
+					// A command from a different thread can not be placed into the already open transaction.
+					boolean usableTransactionExists = activeTransaction != null && !activeTransaction.isReadOnly()
+							&& activeTransaction.getOwner().equals(Thread.currentThread());
+					
+					// The standard case, is that there is a new command which should be executed.
+					// Since there is usually no transaction yet, it will be forwarded to the Standard Workspace
+					// Command Stack which opens a transaction and starts executing the command. Or in case there is a transaction
+					// it will be just placed on the stack as the next command.
+					if (!usableTransactionExists) {
+						// No active transaction means we have to execute the command as a
+						// top-level command
+						super.execute(executeCommand, options);
+					} else {
+						// now where a transaction for the same thread exists, the command itself will be executed
+						// within exactly that transaction. In case that the command which is already executed by the 
+						// transaction is a VirtualSatelliteRecordCommand, the executed command will be chained to it.
+						// This way it looks to the CommandStack as if only the VirSatRecordingCommand was executed.
+						// Now when e.g. an undo is triggered, the chained commands will be undone as well within the
+						// same atomic transaction.
+						if (executeCommand != null && executeCommand.canExecute()) {
+							executeCommand.execute();
+							if (getActiveCommand() instanceof VirSatRecordingCommand) {
+								getActiveCommand().chain(executeCommand);
+							}
+						}
 					}
+				} catch (InterruptedException | RollbackException e) {
+					Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.getPluginId(), "Failed to execute command", e));
 				}
-			}
-		} catch (InterruptedException | RollbackException e) {
-			Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.getPluginId(), "Failed to execute command", e));
+				checkTriggerSaveAll();
+			}, null);
+		} catch (CoreException e) {
+			Activator.getDefault().getLog().log(new Status(Status.ERROR, Activator.getPluginId(), "VirSatWorkspaceCommandStack: Failed to execute command as workspace operation", e));
 		}
-		checkTriggerSaveAll();
 	}
 	
 	@Override
 	public void undo() {
-		triggerSave = false;
-		super.undo();
-		checkTriggerSaveAll();
+		try {
+			wsProject.run(action -> {
+				triggerSave = false;
+				super.undo();
+				checkTriggerSaveAll();
+			}, null);
+		} catch (CoreException e) {
+			Activator.getDefault().getLog().log(new Status(Status.ERROR, Activator.getPluginId(), "VirSatWorkspaceCommandStack: Failed to execute undo as workspace operation", e));
+		}
 	}
 	
 	@Override
 	public void redo() {
-		triggerSave = false;
-		super.redo();
-		checkTriggerSaveAll();
+		try {
+			wsProject.run(action -> {
+				triggerSave = false;
+				super.redo();
+				checkTriggerSaveAll();
+			}, null);
+		} catch (CoreException e) {
+			Activator.getDefault().getLog().log(new Status(Status.ERROR, Activator.getPluginId(), "VirSatWorkspaceCommandStack: Failed to execute redo as workspace operation", e));
+		}
 	}
 	
 	/**
