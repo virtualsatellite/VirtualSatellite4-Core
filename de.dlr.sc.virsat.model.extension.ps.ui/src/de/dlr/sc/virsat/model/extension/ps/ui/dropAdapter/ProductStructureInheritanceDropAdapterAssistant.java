22593841e82aa1cf6087839249f42b22618f00c2
Philipp M. Fischer <philipp.fischer@dlr.de>
2020-01-15 10:53:30 +0100

    Added Empty Implementation for PS Drop Adapter - (Task #74)
    - Added concept specific dropa dapter to Product structure model
    extension
    
    ---
    Task #74: Override DND functionality in Navigator

---
+++ de.dlr.sc.virsat.model.extension.ps.ui/src/de/dlr/sc/virsat/model/extension/ps/ui/dropAdapter/ProductStructureInheritanceDropAdapterAssistant.java
@@ -0,0 +1,23 @@
+package de.dlr.sc.virsat.model.extension.ps.ui.dropAdapter;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.swt.dnd.DropTargetEvent;
+import org.eclipse.swt.dnd.TransferData;
+import org.eclipse.ui.navigator.CommonDropAdapter;
+import org.eclipse.ui.navigator.CommonDropAdapterAssistant;
+
+public class ProductStructureInheritanceDropAdapterAssistant extends CommonDropAdapterAssistant {
+
+	@Override
+	public IStatus validateDrop(Object target, int operation, TransferData transferType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStatus handleDrop(CommonDropAdapter aDropAdapter, DropTargetEvent aDropTargetEvent, Object aTarget) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
