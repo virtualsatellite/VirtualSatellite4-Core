a24ae6fc4009f097cc159cee6357516607b94c3f
Philipp M. Fischer <philipp.fischer@dlr.de>
2020-01-24 23:21:48 +0100

    implemented Prototype for Scroll Redirection - (Task #442)
    - Added scroll listener to table.
    - Redirecting scroll when scrolling downwards.
    
    ---
    Task #442: Improve Scrolling in Editor Forms

--- de.dlr.sc.virsat.uiengine.ui/src/de/dlr/sc/virsat/uiengine/ui/editor/snippets/AUiSnippetGenericTable.java
+++ de.dlr.sc.virsat.uiengine.ui/src/de/dlr/sc/virsat/uiengine/ui/editor/snippets/AUiSnippetGenericTable.java
@@ -13,6 +13,8 @@ import java.util.Collection;
 import java.util.EventObject;
 import java.util.List;
 
+import javax.swing.text.Position;
+
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.command.Command;
@@ -51,20 +53,28 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.MouseWheelListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.RedirectingTable;
+import org.eclipse.swt.widgets.ScrollBar;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.IEditorActionBarContributor;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.commands.ICommandImageService;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
 
 import de.dlr.sc.virsat.model.concept.types.category.IBeanCategoryAssignment;
 import de.dlr.sc.virsat.model.concept.types.factory.BeanCategoryAssignmentFactory;
@@ -279,6 +289,7 @@ public abstract class AUiSnippetGenericTable extends AUiCategorySectionSnippet {
 	 */
 	protected ColumnViewer createColumnViewer(FormToolkit toolkit) {
 		Table table = createDefaultTable(toolkit, sectionBody);
+				
 		table.setData("org.eclipse.swtbot.widget.key", "table" + categoryId);
 		return new TableViewer(table);
 	}
@@ -340,7 +351,45 @@ public abstract class AUiSnippetGenericTable extends AUiCategorySectionSnippet {
 		gridDataTable.heightHint = DEFAULT_TABLE_HEIGHT;
 		gridDataTable.widthHint = DEFAULT_TABLE_WIDTH;
 
-		Table table = toolkit.createTable(sectionBody, SWT.MULTI | SWT.FULL_SELECTION);
+		Table table = new Table(sectionBody, style | toolkit.getBorderStyle() | toolkit.getOrientation());
+		toolkit.adapt(table, false, false);
+		
+		table.addMouseWheelListener(new MouseWheelListener() {
+			
+			@Override
+			public void mouseScrolled(MouseEvent e) {
+				System.out.println(e);
+				
+				int iPosition = table.getVerticalBar().getSelection();
+				int iMaxDown =  table.getVerticalBar().getMaximum();
+				int page = table.getVerticalBar().getPageIncrement();
+				
+				
+				if (iPosition >=10) {
+					Event event = new Event();
+					event.detail =  SWT.PAGE_DOWN;
+					Composite composite = sectionBody.getParent().getParent().getParent().getParent();
+					System.out.println(composite);
+					ScrollBar nextVScrollBar = composite.getVerticalBar();
+					if(nextVScrollBar != null) {
+						System.out.println(nextVScrollBar);
+						int iNextPosition = nextVScrollBar.getSelection();
+						nextVScrollBar.setSelection(iNextPosition + 3);
+						ScrolledForm sf = (ScrolledForm) composite;
+						Control content = sf.getContent();
+						
+						if (content == null) return;
+						Point location = content.getLocation ();
+						ScrollBar vBar = nextVScrollBar ;
+						int vSelection = vBar.getSelection ();
+						content.setLocation (location.x, -vSelection);
+						
+					}
+				}
+			}
+		});
+		
+		//Table table = toolkit.createTable(sectionBody, SWT.MULTI | SWT.FULL_SELECTION);
 		table.setLayoutData(gridDataTable);
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
