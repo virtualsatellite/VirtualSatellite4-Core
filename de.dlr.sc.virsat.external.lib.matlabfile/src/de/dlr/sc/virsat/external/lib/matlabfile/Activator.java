7701bddd1af220bb1f7696703bdae01a405625fb
ALEhmerDLR <anna-lena.ehmer@dlr.de>
2020-02-24 15:49:21 +0100

    created de.dlr.sc.virsat.external.lib.matlabfile - (Task #489)
    created de.dlr.sc.virsat.external.lib.matlabfile
    included in de.dlr.sc.virsat.external.lib.feature
    included in pom.xml
    
    ---
    Task #489: Include Matlab libary

---
+++ de.dlr.sc.virsat.external.lib.matlabfile/src/de/dlr/sc/virsat/external/lib/matlabfile/Activator.java
@@ -0,0 +1,9 @@
+package de.dlr.sc.virsat.external.lib.matlabfile;
+
+import de.dlr.sc.virsat.external.lib.LibPlugin;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends LibPlugin {
+}
