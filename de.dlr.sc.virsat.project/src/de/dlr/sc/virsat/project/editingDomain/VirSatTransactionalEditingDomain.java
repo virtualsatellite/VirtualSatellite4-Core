678d9e0f4a97ba2dcc151d2da1828d75ab0f4ce5
christoph-pietsch-DLR <christoph.pietsch@dlr.de>
2020-02-24 17:04:19 +0100

    Shorting line length - (Task #499)
    ---
    Task #499: Graphiti Diagram Editor: Workspace Synchronisation Problem

--- de.dlr.sc.virsat.project/src/de/dlr/sc/virsat/project/editingDomain/VirSatTransactionalEditingDomain.java
+++ de.dlr.sc.virsat.project/src/de/dlr/sc/virsat/project/editingDomain/VirSatTransactionalEditingDomain.java
@@ -251,7 +251,8 @@ public class VirSatTransactionalEditingDomain extends TransactionalEditingDomain
 							// First check if the resource which is changed is not on the list of
 							// recently resources than trigger a full reload for all resources.
 							if (!recentlyChangedResource.contains(changedResourceUri)) {
-								Activator.getDefault().getLog().log(new Status(Status.INFO, Activator.getPluginId(), "VirSatTransactionalEditingDomain: (" + changedResourceUri.toPlatformString(true) + ") not in recently saved resources. Triggering for a full relaod."));
+								Activator.getDefault().getLog().
+									log(new Status(Status.INFO, Activator.getPluginId(), "VirSatTransactionalEditingDomain: (" + changedResourceUri.toPlatformString(true) + ") not in recently saved resources. Triggering for a full relaod."));
 								triggerFullReload = true;
 							} 
 							
