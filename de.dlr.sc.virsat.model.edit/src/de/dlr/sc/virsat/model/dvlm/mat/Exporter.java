50119d2aa70cfb909defe9be9a2c7d6b1d72a41e
ALEhmerDLR <anna-lena.ehmer@dlr.de>
2020-02-28 16:19:21 +0100

    Can export Values and Units - (Task #489)
    ---
    Task #489: Include Matlab libary

---
+++ de.dlr.sc.virsat.model.edit/src/de/dlr/sc/virsat/model/dvlm/mat/Exporter.java
@@ -0,0 +1,290 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2020 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.sc.virsat.model.dvlm.mat;
+
+import java.io.IOException;
+import org.eclipse.emf.common.util.EList;
+
+import de.dlr.sc.virsat.model.concept.types.property.BeanPropertyBoolean;
+import de.dlr.sc.virsat.model.concept.types.property.BeanPropertyEReference;
+import de.dlr.sc.virsat.model.concept.types.property.BeanPropertyEnum;
+import de.dlr.sc.virsat.model.concept.types.property.BeanPropertyFloat;
+import de.dlr.sc.virsat.model.concept.types.property.BeanPropertyInt;
+import de.dlr.sc.virsat.model.concept.types.property.BeanPropertyResource;
+import de.dlr.sc.virsat.model.concept.types.property.BeanPropertyString;
+import de.dlr.sc.virsat.model.dvlm.categories.CategoryAssignment;
+import de.dlr.sc.virsat.model.dvlm.categories.propertydefinitions.BooleanProperty;
+import de.dlr.sc.virsat.model.dvlm.categories.propertydefinitions.EReferenceProperty;
+import de.dlr.sc.virsat.model.dvlm.categories.propertydefinitions.EnumProperty;
+import de.dlr.sc.virsat.model.dvlm.categories.propertydefinitions.FloatProperty;
+import de.dlr.sc.virsat.model.dvlm.categories.propertydefinitions.IntProperty;
+import de.dlr.sc.virsat.model.dvlm.categories.propertydefinitions.ReferenceProperty;
+import de.dlr.sc.virsat.model.dvlm.categories.propertydefinitions.ResourceProperty;
+import de.dlr.sc.virsat.model.dvlm.categories.propertydefinitions.StringProperty;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.APropertyInstance;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.ArrayInstance;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.ComposedPropertyInstance;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.EReferencePropertyInstance;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.EnumUnitPropertyInstance;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.ReferencePropertyInstance;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.ResourcePropertyInstance;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.UnitValuePropertyInstance;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.ValuePropertyInstance;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.util.PropertyinstancesSwitch;
+import de.dlr.sc.virsat.model.dvlm.structural.StructuralElementInstance;
+import us.hebi.matlab.mat.format.Mat5;
+import us.hebi.matlab.mat.types.Array;
+import us.hebi.matlab.mat.types.MatFile;
+import us.hebi.matlab.mat.types.Struct;
+
+/**
+ * Class for exporting data to .mat
+ */
+public class Exporter {
+	
+
+	/**
+	 * creates a new .mat with everything inside a sei
+	 * @param seiRoot sei to export
+	 */
+	public MatFile exportSei(StructuralElementInstance seiRoot) throws IOException {
+		MatFile matfile = Mat5.newMatFile();		
+		Struct myStruct = Mat5.newStruct();
+		myStruct.set("Type", Mat5.newString(seiRoot.getType().getName()))
+			.set("UUID", Mat5.newString(seiRoot.getUuid().toString()))
+			.set("Children", Mat5.newString(seiRoot.getChildren().toString()));
+		for (int i = 0; i < seiRoot.getCategoryAssignments().size(); i++) {
+			CategoryAssignment ca = seiRoot.getCategoryAssignments().get(i);
+			myStruct.set(ca.getName(), exportCatAs(ca));
+		}
+		matfile.addArray(seiRoot.getName(), myStruct);
+		return matfile;
+	}
+	
+	/**
+	 * creates a new .mat with everything inside the selected CategoryAssinments
+	 * @param cas List of categoryAssinments
+	 */	
+	public MatFile exportCas(EList<CategoryAssignment> cas) throws IOException {
+		MatFile matfile = Mat5.newMatFile();		
+		Struct myStruct = Mat5.newStruct();
+		for (int i = 0; i < cas.size(); i++) {
+			CategoryAssignment ca = cas.get(i);
+			myStruct.set(ca.getName(), exportCatAs(ca));
+		}
+		matfile.addArray("Input", myStruct);
+		return matfile;
+	}
+
+	/**
+	 * creates a new .mat-Struct with all Properties of an CategoryAssinment
+	 * @param ca CategoryAssignment
+	 */
+	public Struct exportCatAs(CategoryAssignment ca) {
+		Struct struct = Mat5.newStruct();
+		EList<APropertyInstance> propertyInstances = ca.getPropertyInstances();
+
+		for (int i = 0; i < propertyInstances.size(); i++) {
+			APropertyInstance element = propertyInstances.get(i);
+			Array propertyArray = getrightProperty(element);
+			
+			struct.set(propertyInstances.get(i).getType().getName(), propertyArray);
+		}
+		return struct;
+	}
+	
+	/**
+	 * hands back write property with all values
+	 * @param element APropertyInstance
+	 */
+	public Array getrightProperty(APropertyInstance element) {
+		Array propertyArray = new PropertyinstancesSwitch<Array>() {
+			@Override
+			public Array caseUnitValuePropertyInstance(UnitValuePropertyInstance object) {
+				return contentofProperty(object);
+			}
+
+			@Override
+			public Array caseResourcePropertyInstance(ResourcePropertyInstance object) {
+				return contentofProperty(object);
+			}
+
+			@Override
+			public Array caseEnumUnitPropertyInstance(EnumUnitPropertyInstance object) {
+				return contentofProperty(object);
+			}
+
+			@Override
+			public Array caseValuePropertyInstance(ValuePropertyInstance object) {
+				return contentofProperty(object);
+			}
+
+			@Override
+			public Array caseReferencePropertyInstance(ReferencePropertyInstance object) {
+				return contentofProperty(object);
+			}
+
+			@Override
+			public Array caseEReferencePropertyInstance(EReferencePropertyInstance object) {
+				return contentofProperty(object);
+			}
+
+			@Override
+			public Array caseComposedPropertyInstance(ComposedPropertyInstance object) {
+				return contentofProperty(object);
+			}
+
+			@Override
+			public Array caseArrayInstance(ArrayInstance object) {
+				return contentofProperty(object);
+			}
+
+		}.doSwitch(element);
+		return propertyArray;
+	}
+
+	//create a struct for the fitting PropertyInstance
+	
+	/**
+	 * creates a new .mat-Struct with all Information about UnitValuePropertyInstance
+	 * only UUID Quantity , unit and Value are saved
+	 * @param castElement PropertyInstance
+	 */
+	private Array contentofProperty(UnitValuePropertyInstance element) {
+		Struct struct = Mat5.newStruct();
+		
+		if (element.getType() instanceof FloatProperty) {
+			BeanPropertyFloat bpf = new BeanPropertyFloat(element);
+			struct.set("Unit", Mat5.newString(bpf.getUnit()));
+			struct.set("Value", (bpf.getValue() == Double.NaN) ? Mat5.newString("") : Mat5.newScalar(bpf.getValue()));
+		} else if (element.getType() instanceof IntProperty) {
+			BeanPropertyInt bpi = new BeanPropertyInt(element);
+			struct.set("Unit", Mat5.newString(bpi.getUnit()));
+			struct.set("Value", (!bpi.isSet()) ? Mat5.newString("") : Mat5.newScalar(bpi.getValue()));
+		}
+		return struct;
+	}
+	
+	/**
+	 * creates a new .mat-Struct with all Information about UnitValuePropertyInstance
+	 * only unit and Value are saved
+	 * @param castElement PropertyInstance
+	 */
+	private Struct contentofProperty(ValuePropertyInstance element) {
+		Struct struct = Mat5.newStruct();
+		if (element.getType() instanceof BooleanProperty) {
+			BeanPropertyBoolean bpb = new BeanPropertyBoolean();
+			bpb.setATypeInstance(element);
+			struct.set("Value", (!bpb.isSet()) ? Mat5.newString("") : Mat5.newLogicalScalar(bpb.getValue()));
+		} else if (element.getType() instanceof StringProperty) {
+			BeanPropertyString bps = new BeanPropertyString();
+			bps.setATypeInstance(element);
+			struct.set("Value", (!bps.isSet()) ? Mat5.newString("") : Mat5.newString(bps.getValue()));
+		}
+		return struct;
+	}
+	
+	/**
+	 * creates a new .mat-Struct with all Information about EReferencePropertyInstance
+	 * only UUID Referenz and Referenc-Class are saved
+	 * @param castElement PropertyInstance
+	 */
+	private Array contentofProperty(EReferencePropertyInstance element) {
+		Struct struct = Mat5.newStruct();
+		if (element.getType() instanceof EReferenceProperty) {
+			
+			BeanPropertyEReference<EReferenceProperty> bpe = new BeanPropertyEReference<EReferenceProperty>();
+			bpe.setATypeInstance(element);
+			struct.set("UUID", Mat5.newString(element.getUuid().toString()));
+			struct.set("Referenc", (!bpe.isSet()) ? Mat5.newString("") : Mat5.newString(bpe.getValue().toString()));
+			struct.set("Referenc-Class", (!bpe.isSet()) ? Mat5.newString("") : Mat5.newString(bpe.getClass().getName()));
+		}
+		return struct;
+	}
+	
+	/**
+	 * creates a new .mat-Struct with all Information about ReferencePropertyInstance
+	 * only UUID Referenz and Referenc-Class are saved
+	 * @param castElement PropertyInstance
+	 */
+	private Array contentofProperty(ReferencePropertyInstance element) {
+		Struct struct = Mat5.newStruct();
+		if (element.getType() instanceof ReferenceProperty) {
+			BeanPropertyEReference<ReferenceProperty> bpr = new BeanPropertyEReference<ReferenceProperty>();
+			bpr.setATypeInstance(element);
+			struct.set("UUID", Mat5.newString(element.getUuid().toString()));
+			struct.set("Referenc", (!bpr.isSet()) ? Mat5.newString("") : Mat5.newString(bpr.getValue().toString()));
+			struct.set("Referenc-Class", (!bpr.isSet()) ? Mat5.newString("") : Mat5.newString(bpr.getClass().getName()));
+		}
+		
+		return struct;
+	}
+	
+	/**
+	 * creates a new .mat-Struct with all Information about ResourcePropertyInstance
+	 * only UUID instanceType and Property Type are saved
+	 * @param castElement PropertyInstance
+	 */
+	private Array contentofProperty(ResourcePropertyInstance element) {
+		Struct struct = Mat5.newStruct();
+		if (element.getType() instanceof ResourceProperty) {
+			BeanPropertyResource bpr = new BeanPropertyResource();
+			bpr.setATypeInstance(element);			
+			struct.set("URI", (!bpr.isSet()) ? Mat5.newString("") : Mat5.newString(bpr.getValue().toString()));
+			struct.set("File", (!bpr.isSet()) ? Mat5.newString("") : Mat5.newString(bpr.getFile().getName()));
+		}
+		return struct;
+	}
+	
+	/**
+	 * creates a new .mat-Struct with all Information about EnumUnitPropertyInstance
+	 * only unit and value are saved
+	 * @param element PropertyInstance
+	 */	
+	private Array contentofProperty(EnumUnitPropertyInstance element) {
+		Struct struct = Mat5.newStruct();
+		if (element.getType() instanceof EnumProperty) {
+			BeanPropertyEnum bpe = new BeanPropertyEnum();
+			bpe.setATypeInstance(element);
+			struct.set("Unit", Mat5.newString(bpe.getUnit()));
+			struct.set("Value", (!bpe.isSet()) ? Mat5.newString("") : Mat5.newScalar(bpe.getEnumValue()));
+		}
+		return struct;
+	}
+
+	/**
+	 * creates a new .mat-Struct with all Information about ArrayInstance
+	 * @param element PropertyInstance
+	 */	
+	private Array contentofProperty(ArrayInstance element) {
+		Struct struct = Mat5.newStruct();
+		EList<APropertyInstance> propertyInstances = element.getArrayInstances();
+		for (int i = 0; i < propertyInstances.size(); i++) {
+			APropertyInstance instanceElement = propertyInstances.get(i);
+			Array propertyArray = getrightProperty(instanceElement);
+			
+			struct.set(propertyInstances.get(i).getType().getName(), propertyArray);
+		}
+		return struct;
+	}
+	
+	/**
+	 * creates a new .mat-Struct with all Information about ComposedPropertyInstance
+	 * @param element ComposedPropertyInstance
+	 */	
+	private Array contentofProperty(ComposedPropertyInstance element) {
+		return exportCatAs(element.getTypeInstance());
+	}
+	
+	//Delete First and Last Character
+	public static String shorter(String str) {
+		return str.substring(1, str.length() - 1);
+	}
+}
--- de.dlr.sc.virsat.model.extension.tests.test/src/de/dlr/sc/virsat/model/dvlm/mat/ExporterTest.java
+++ de.dlr.sc.virsat.model.extension.tests.test/src/de/dlr/sc/virsat/model/dvlm/mat/ExporterTest.java
@@ -12,17 +12,28 @@ package de.dlr.sc.virsat.model.dvlm.mat;
 import java.io.IOException;
 import java.util.List;
 
-import org.eclipse.emf.common.util.EList;
+import org.eclipse.core.runtime.CoreException;
 import org.junit.Before;
 import org.junit.Test;
 
-import de.dlr.sc.virsat.model.dvlm.categories.CategoryAssignment;
-import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
 import de.dlr.sc.virsat.model.dvlm.structural.StructuralElementInstance;
-import de.dlr.sc.virsat.model.extension.tests.model.AConceptTestCase;
+import de.dlr.sc.virsat.model.extension.tests.model.EReferenceTest;
 import de.dlr.sc.virsat.model.extension.tests.model.TestCategoryAllProperty;
+import de.dlr.sc.virsat.model.extension.tests.model.TestCategoryBase;
+import de.dlr.sc.virsat.model.extension.tests.model.TestCategoryBeanA;
+import de.dlr.sc.virsat.model.extension.tests.model.TestCategoryBeanB;
+import de.dlr.sc.virsat.model.extension.tests.model.TestCategoryBeanConcrete;
 import de.dlr.sc.virsat.model.extension.tests.model.TestCategoryComposition;
+import de.dlr.sc.virsat.model.extension.tests.model.TestCategoryCompositionArray;
+import de.dlr.sc.virsat.model.extension.tests.model.TestCategoryExtends;
+import de.dlr.sc.virsat.model.extension.tests.model.TestCategoryIntrinsicArray;
+import de.dlr.sc.virsat.model.extension.tests.model.TestCategoryReference;
+import de.dlr.sc.virsat.model.extension.tests.model.TestCategoryReferenceArray;
+import de.dlr.sc.virsat.model.extension.tests.model.TestCrossLinkedParametersWithCalculation;
+import de.dlr.sc.virsat.model.extension.tests.model.TestMassParameters;
+import de.dlr.sc.virsat.model.extension.tests.model.TestParameter;
 import de.dlr.sc.virsat.model.extension.tests.model.TestStructuralElement;
+import de.dlr.sc.virsat.model.extension.tests.test.ATestConceptTestCase;
 import us.hebi.matlab.mat.format.Mat5;
 import us.hebi.matlab.mat.types.MatFile;
 import us.hebi.matlab.mat.types.Struct;
@@ -30,27 +41,25 @@ import us.hebi.matlab.mat.types.Struct;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 
-public class ExporterTest extends AConceptTestCase {
+public class ExporterTest extends ATestConceptTestCase {
 	
 	private static final int NUMBEROFELEMENTS = 3;
-	private Concept concept;
 	private StructuralElementInstance sei;
+	private TestStructuralElement tsei;
+	private Exporter exporter;
 	
 	@Before
-	public void setUp() {
-		//get Test - StructuralElementInstance
-		prepareEditingDomain();
-		concept = loadConceptFromPlugin();
-		TestCategoryAllProperty tc = new TestCategoryAllProperty(concept);
-		TestCategoryComposition tc1 = new TestCategoryComposition(concept);
-		TestStructuralElement tsei = new TestStructuralElement(concept);
-		tsei.add(tc);
-		tsei.add(tc1);
+	public void setUp() throws CoreException {
+		exporter = new Exporter();
+		super.setUp();
+		addResourceSetAndRepository();
+		loadTestConcept();
+		tsei = new TestStructuralElement(testConcept);
 		sei = tsei.getStructuralElementInstance();
 		sei.setName("testsei");
+		repository.getRootEntities().add(sei);
 	}
 	@Test
 	public void testShorterString() {
@@ -58,27 +67,75 @@ public class ExporterTest extends AConceptTestCase {
 		assertEquals("Same String", test.substring(1, test.length() - 1), Exporter.shorter(test));
 	}
 	
-	@Test
+	@Test //Delete at the end
 	public void testExportSeiCanMatlabReadIt() throws IOException {
-		Mat5.writeToFile(Exporter.exportSei(sei), "Testfile.mat");
+		TestCategoryAllProperty tc = new TestCategoryAllProperty(testConcept);
+		TestCategoryComposition tc1 = new TestCategoryComposition(testConcept);
+		TestParameter tc11 = new TestParameter(testConcept);
+		TestMassParameters tc12 = new TestMassParameters(testConcept);
+		EReferenceTest tc14 = new  EReferenceTest(testConcept);
+		
+		TestCategoryBeanA tc6 = new TestCategoryBeanA(testConcept);
+		TestCategoryBeanB tc7 = new TestCategoryBeanB(testConcept);
+		TestCategoryBeanConcrete tc8 = new TestCategoryBeanConcrete(testConcept);
+		TestCrossLinkedParametersWithCalculation tc13 = new TestCrossLinkedParametersWithCalculation(testConcept);
+		
+		TestCategoryReference tc2 = new TestCategoryReference(testConcept);
+		TestCategoryIntrinsicArray tc3 = new TestCategoryIntrinsicArray(testConcept);
+		TestCategoryCompositionArray tc4 = new TestCategoryCompositionArray(testConcept);
+		TestCategoryReferenceArray tc5 = new TestCategoryReferenceArray(testConcept);
+		TestCategoryBase tc9 = new TestCategoryBase(testConcept);
+		TestCategoryExtends tc10 = new TestCategoryExtends(testConcept);
+		
+
+
+
+
+		
+		tsei.add(tc);
+		tsei.add(tc1);
+		tsei.add(tc11);
+		tsei.add(tc12);
+		tsei.add(tc14);
+		
+		tsei.add(tc6);
+		tsei.add(tc7);
+		tsei.add(tc8);
+		tsei.add(tc13);
+		
+//		tsei.add(tc2);	ERef != Ref
+		tsei.add(tc3);
+		tsei.add(tc4);		
+		tsei.add(tc5);	
+		tsei.add(tc9);		
+		tsei.add(tc10);		
+		
+
+
+		
+		
+		Mat5.writeToFile(exporter.exportSei(sei), "Testfile.mat");
 	}
 	@Test
 	public void testExportSeitypeUUID() throws IOException {
-		MatFile testmat = Exporter.exportSei(sei);
+		MatFile testmat = exporter.exportSei(sei);
 		assertEquals("Same UUID", sei.getUuid().toString(), Exporter.shorter(testmat.getStruct(sei.getName()).get("UUID").toString()));
 		assertEquals("Same Type", sei.getType().getName(), Exporter.shorter(testmat.getStruct(sei.getName()).get("Type").toString()));
 		
 	}
 	@Test
 	public void testExportSeiallCategories() throws IOException {
-		MatFile testmat = Exporter.exportSei(sei);
+		
+		TestCategoryAllProperty tc = new TestCategoryAllProperty(testConcept);
+		TestCategoryComposition tc1 = new TestCategoryComposition(testConcept);
+		tsei.add(tc);
+		tsei.add(tc1);
+
+		
+		MatFile testmat = exporter.exportSei(sei);
 		Struct matStruct = testmat.getStruct("testsei");
 		List<String> matCategoryAssinments = matStruct.getFieldNames();		
 		assertEquals("Number of CategoryAssinments", sei.getCategoryAssignments().size(), matStruct.getFieldNames().size() - NUMBEROFELEMENTS);
-		EList<CategoryAssignment> seiCategories = sei.getCategoryAssignments();
-		for (int i = 0; i < seiCategories.size(); i++) {
-			assertTrue("Included CategoryAssisnment" + seiCategories.get(i).getName(), matCategoryAssinments.contains(seiCategories.get(i).getName()));
-		}		
 		assertThat("Includes all CategoryAssinments", matCategoryAssinments, hasItems(TestCategoryAllProperty.class.getSimpleName(), TestCategoryComposition.class.getSimpleName()));
 		
 	}
