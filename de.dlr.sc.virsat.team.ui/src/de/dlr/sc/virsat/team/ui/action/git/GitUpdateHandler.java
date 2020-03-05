5e2b9d804e105dd8adc7061079abb1d44bad4b26
JAmmermann-DLR <joshua.ammermann@dlr.de>
2020-03-05 10:47:25 +0100

    Add version control UI Commands and Git Command Handler- (Task #519)
    ---
    Task #519: Refactor version control integration

---
+++ de.dlr.sc.virsat.team.ui/src/de/dlr/sc/virsat/team/ui/action/git/GitUpdateHandler.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2019 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.sc.virsat.team.ui.action.git;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+
+public class GitUpdateHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		return null;
+	}
+
+
+}
