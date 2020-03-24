e222543c84d00f44abf538ad9c41cc8762ae1648
SaMuellerDLR <sa.mueller@dlr.de>
2020-03-24 09:59:18 +0100

    Managed to get test setup going - (Task #519)
    ---
    Task #519: Refactor version control integration

--- de.dlr.sc.virsat.team.test/src/de/dlr/sc/virsat/team/svn/VirSatSvnVersionControlBackendTest.java
+++ de.dlr.sc.virsat.team.test/src/de/dlr/sc/virsat/team/svn/VirSatSvnVersionControlBackendTest.java
@@ -14,11 +14,24 @@ import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.team.svn.core.SVNMessages;
+import org.eclipse.team.svn.core.connector.ISVNManager;
+import org.eclipse.team.svn.core.connector.ISVNManager.RepositoryKind;
 import org.eclipse.team.svn.core.connector.SVNDepth;
-import org.eclipse.team.svn.core.operation.remote.CheckoutAsOperation;
+import org.eclipse.team.svn.core.extension.CoreExtensionsManager;
+import org.eclipse.team.svn.core.operation.AbstractActionOperation;
+import org.eclipse.team.svn.core.operation.IConsoleStream;
+import org.eclipse.team.svn.core.operation.SVNProgressMonitor;
+import org.eclipse.team.svn.core.operation.file.CheckoutAsOperation;
 import org.eclipse.team.svn.core.resource.IRepositoryResource;
+import org.eclipse.team.svn.core.utility.FileUtility;
 import org.eclipse.team.svn.core.utility.SVNUtility;
 import org.junit.Before;
 
@@ -32,21 +45,40 @@ public class VirSatSvnVersionControlBackendTest extends AVirSatVersionControlBac
 		
 		try {
 			pathRepoRemote = Files.createTempDirectory("VirtualSatelliteSvnRemote_");
-			File fileSvnRemoteRepo = pathRepoRemote.toFile();
-			fileSvnRemoteRepo.mkdir();
 			URI uriToRemoteRepoPath = pathRepoRemote.toUri();
-			String remoteRepoRawPath = uriToRemoteRepoPath.toString();
-			IRepositoryResource remoteRepo = SVNUtility.asRepositoryResource(remoteRepoRawPath, true);
+			String remoteRepoFilePath = pathRepoRemote.toString();
+			
+			IRepositoryResource remoteRepo = SVNUtility.asRepositoryResource(uriToRemoteRepoPath.toString(), true);
+			
+			AbstractActionOperation createRemoteRepoOp = new AbstractActionOperation("Operation_CreateRepository", SVNMessages.class) {
+				protected void runImpl(IProgressMonitor monitor) throws Exception {
+					ISVNManager proxy = CoreExtensionsManager.instance().getSVNConnectorFactory().createManager();
+					RepositoryKind repositoryType = ISVNManager.RepositoryKind.FSFS;
+					
+					try {					
+						StringBuffer msg = new StringBuffer();
+						msg.append("svnadmin create ");
+						msg.append("--fs-type ").append(repositoryType.toString().toLowerCase()).append(" ");
+						msg.append("\"").append(FileUtility.normalizePath(remoteRepoFilePath)).append("\"");
+						msg.append("\n"); //$NON-NLS-1$
+						this.writeToConsole(IConsoleStream.LEVEL_CMD, msg.toString());
+						proxy.create(remoteRepoFilePath, repositoryType, null, ISVNManager.Options.NONE, new SVNProgressMonitor(this, monitor, null));
+					} finally {
+						proxy.dispose();
+					}
+				}			
+			};
+			
+			createRemoteRepoOp.run(null);
 			
 			pathRepoLocal1 = Files.createTempDirectory("VirtualSatelliteSvnLocal1_");
 			File filePathToProject = pathRepoLocal1.toFile();
-			CheckoutAsOperation checkoutAsOperation1 = new CheckoutAsOperation(filePathToProject.getName(), remoteRepo, SVNDepth.INFINITY, true);
+			CheckoutAsOperation checkoutAsOperation1 = new CheckoutAsOperation(filePathToProject, remoteRepo, SVNDepth.INFINITY, true, true);
 			checkoutAsOperation1.run(null);
 			
-			uriToRemoteRepoPath = pathRepoRemote.toUri();
 			pathRepoLocal2 = Files.createTempDirectory("VirtualSatelliteSvnLocal2_");
 			filePathToProject = pathRepoLocal2.toFile();
-			CheckoutAsOperation checkoutAsOperation2 = new CheckoutAsOperation(filePathToProject.getName(), remoteRepo, SVNDepth.INFINITY, true);
+			CheckoutAsOperation checkoutAsOperation2 = new CheckoutAsOperation(filePathToProject, remoteRepo, SVNDepth.INFINITY, true, true);
 			checkoutAsOperation2.run(null);
 		} catch (IOException e) {
 			Activator.getDefault().getLog().log(new Status(Status.ERROR, Activator.getPluginId(),
@@ -56,5 +88,18 @@ public class VirSatSvnVersionControlBackendTest extends AVirSatVersionControlBac
 		super.setUp();
 
 		backend = new VirSatSvnVersionControlBackend();
+	}
+	
+	private static final int WAIT_FOR_REPO_DETECTION_TIMESPAN = 10;
+	
+	@Override
+	protected void waitForProjectToRepoMapping(IProject project) throws CoreException {
+		super.waitForProjectToRepoMapping(project);
+		try {
+			while (!FileUtility.isConnected(project)) {
+				Thread.sleep(WAIT_FOR_REPO_DETECTION_TIMESPAN);
+			}
+		} catch (InterruptedException e) {
+		}
 	}
 }
--- de.dlr.sc.virsat.team/src/de/dlr/sc/virsat/team/svn/VirSatSvnVersionControlBackend.java
+++ de.dlr.sc.virsat.team/src/de/dlr/sc/virsat/team/svn/VirSatSvnVersionControlBackend.java
@@ -9,9 +9,22 @@
  *******************************************************************************/
 package de.dlr.sc.virsat.team.svn;
 
+import java.io.File;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.team.svn.core.connector.SVNDepth;
+import org.eclipse.team.svn.core.connector.SVNEntryInfo;
+import org.eclipse.team.svn.core.operation.IConsoleStream;
+import org.eclipse.team.svn.core.operation.local.CommitOperation;
+import org.eclipse.team.svn.core.operation.local.UpdateOperation;
+import org.eclipse.team.svn.core.operation.remote.CheckoutAsOperation;
+import org.eclipse.team.svn.core.resource.IRepositoryResource;
+import org.eclipse.team.svn.core.utility.SVNUtility;
 
 import de.dlr.sc.virsat.team.IVirSatVersionControlBackend;
 
@@ -19,15 +32,31 @@ public class VirSatSvnVersionControlBackend implements IVirSatVersionControlBack
 
 	@Override
 	public void commit(IProject project, String message, IProgressMonitor monitor) throws Exception {
-		// TODO Auto-generated method stub
-
+		SubMonitor commitMonitor = SubMonitor.convert(monitor, "Virtual Satellite svn commit", 1);
+		
+		// The signature requires the project to be passed as an array
+		IResource[] projects = { project };
+		CommitOperation commitOperation = new CommitOperation(projects, message, true, true);
+		commitOperation.run(commitMonitor);
+		
+		File file = new File(project.getRawLocationURI().getRawPath());
+		SVNEntryInfo test = SVNUtility.getSVNInfo(file);
+		
+		commitMonitor.split(1);
 	}
 
 	@Override
 	public void checkout(IProjectDescription projectDescription, String remoteUri, IProgressMonitor monitor)
 			throws Exception {
-		// TODO Auto-generated method stub
+		SubMonitor checkoutMonitor = SubMonitor.convert(monitor, "Virtual Satellite svn checkout", 1);
+		File pathRepoLocal = new File(projectDescription.getLocationURI());
+		IRepositoryResource svnRepositoryResource = SVNUtility.asRepositoryResource(remoteUri, true);
+		
+		CheckoutAsOperation checkoutAsOperation = new CheckoutAsOperation(pathRepoLocal.getName(), 
+				svnRepositoryResource, SVNDepth.INFINITY, true);
+		checkoutAsOperation.run(checkoutMonitor);
 
+		checkoutMonitor.split(1);
 	}
 
 	@Override
@@ -37,7 +66,15 @@ public class VirSatSvnVersionControlBackend implements IVirSatVersionControlBack
 
 	@Override
 	public void update(IProject project, IProgressMonitor monitor) throws Exception {
-		// TODO Auto-generated method stub
+		SubMonitor updateMonitor = SubMonitor.convert(monitor, "Virtual Satellite svn update", 1);
+		
+		// The signature requires the project to be passed as an array
+		IResource[] projects = { project };
+		UpdateOperation updateOperation = new UpdateOperation(projects, true);
+		updateOperation.run(updateMonitor);
+		
+		updateMonitor.split(1);
 	}
 
 }
+
