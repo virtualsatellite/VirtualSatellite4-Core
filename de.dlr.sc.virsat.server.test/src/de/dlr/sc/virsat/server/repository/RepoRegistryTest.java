3c7117fe503c82d8326db5361e835c2bb7b12549
Tobias Franz <tobias.franz@dlr.de>
2020-03-25 01:01:22 +0100

    Add basic RepoRegistry - (Task #537)

---
+++ de.dlr.sc.virsat.server.test/src/de/dlr/sc/virsat/server/repository/RepoRegistryTest.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2019 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.sc.virsat.server.repository;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.dlr.sc.virsat.server.configuration.RepositoryConfiguration;
+
+public class RepoRegistryTest {
+	
+	private static final String REPO_NAME = "TestRepository";
+	
+	@Test
+	public void testBasicAddGet() {
+		
+		//Create basic test repsoitory 
+		final ServerRepository TEST_REPO = new ServerRepository(new RepositoryConfiguration("", "", "", ""));
+		
+		RepoRegistry.getInstance().addRepository(REPO_NAME, TEST_REPO);
+		
+		ServerRepository returnedFromRegistry = RepoRegistry.getInstance().getRepository(REPO_NAME);
+		
+		Assert.assertEquals("Object should be the same as the one added", TEST_REPO, returnedFromRegistry);
+	}
+	
+	@Test
+	public void testListAddGet() {
+		
+		//Create basic test repsoitory 
+		final ServerRepository TEST_REPO = new ServerRepository(new RepositoryConfiguration("", "", "", ""));
+		
+		RepoRegistry.getInstance().getRepositories().put(REPO_NAME, TEST_REPO);
+		
+		ServerRepository returnedFromRegistry = RepoRegistry.getInstance().getRepositories().get(REPO_NAME);
+		
+		Assert.assertEquals("Object should be the same as the one added", TEST_REPO, returnedFromRegistry);
+	}
+
+}
