c10a81cb94118cc61b2f3656538b75fbdab0092d
SaMuellerDLR <sa.mueller@dlr.de>
2020-03-26 09:23:33 +0100

    Improved svn monitoring - (Task #519)
    ---
    Task #519: Refactor version control integration

--- de.dlr.sc.virsat.team/src/de/dlr/sc/virsat/team/svn/VirSatSvnVersionControlBackend.java
+++ de.dlr.sc.virsat.team/src/de/dlr/sc/virsat/team/svn/VirSatSvnVersionControlBackend.java
@@ -17,6 +17,7 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.SubMonitor;
@@ -53,38 +54,17 @@ public class VirSatSvnVersionControlBackend implements IVirSatVersionControlBack
 	public void commit(IProject project, String message, IProgressMonitor monitor) throws Exception {
 		SubMonitor commitMonitor = SubMonitor.convert(monitor, "Virtual Satellite svn commit", 1);
 	
-		File projectWorkingCopy = new File(FileUtility.getWorkingCopyPath(project));
-		
-		List<File> filesList = new ArrayList<>();
-	
-		// The subversive implementation sets a flag that requires the parent of a file that should be commited,
-		// to be under version control as well. We check here if the parent is under version control,
-		// and if so, then we can simply commit the project file. If not, we commit the commitable files under the
-		// project. We do not go any deeper and let SVN itself handle the recursive search for further commitables.
-		if (SVNUtility.hasSVNFolderInOrAbove(projectWorkingCopy.getParentFile())) {
-			filesList.add(projectWorkingCopy);
-		} else {
-			IResource[] resources = FileUtility.getResourcesRecursive(project.members(), IStateFilter.SF_UNVERSIONED, 0);
-			for (IResource resource : resources) {
-				filesList.add(new File(FileUtility.getWorkingCopyPath(resource)));
-			}
-		}
-		
-		// Perform an add + commit operation
-		File[] files = filesList.toArray(new File[0]);
+		commitMonitor.split(1).subTask("Commiting & adding files");
+		File[] files = getFilesToCommit(project);
 		CommitOperation commitOperation = new CommitOperation(files, message, true, false);
 		CompositeOperation compositeOperation = new CompositeOperation(commitOperation.getId(), commitOperation.getMessagesClass());
 		// SVN requires adding files to the repository before commiting them
 		compositeOperation.add(new AddToSVNOperation(files, true));
 		compositeOperation.add(commitOperation);
 		
-		checkStatus(compositeOperation);
-		
 		compositeOperation.run(commitMonitor);
 		
 		checkStatus(compositeOperation);
-		
-		commitMonitor.split(1);
 	}
 
 	@Override
@@ -94,6 +74,7 @@ public class VirSatSvnVersionControlBackend implements IVirSatVersionControlBack
 		File pathRepoLocal = new File(projectDescription.getLocationURI());
 		IRepositoryResource remoteRepo = SVNUtility.asRepositoryResource(remoteUri, true);
 
+		checkoutMonitor.split(1).subTask("Checking out remote project");
 		CheckoutAsOperation checkoutAsOperation = new CheckoutAsOperation(pathRepoLocal, remoteRepo, SVNDepth.INFINITY, true, true);
 		CompositeOperation compositeOperation = new CompositeOperation(checkoutAsOperation.getId(), checkoutAsOperation.getMessagesClass());
 		compositeOperation.add(checkoutAsOperation);
@@ -109,8 +90,6 @@ public class VirSatSvnVersionControlBackend implements IVirSatVersionControlBack
 		compositeOperation.run(checkoutMonitor);
 		
 		checkStatus(compositeOperation);
-		
-		checkoutMonitor.split(1);
 	}
 
 	@Override
@@ -121,7 +100,10 @@ public class VirSatSvnVersionControlBackend implements IVirSatVersionControlBack
 		
 		// The signature requires the project to be passed as an array
 		IProject[] projects = { project };
+		// Use the local project name as the remote project name
 		IFolderNameMapper folderNameMapper = (p -> p.getName());
+		
+		checkInMonitor.split(1).subTask("Checking in local project");
 		ShareProjectOperation shareProjectOperation = new ShareProjectOperation(projects, remoteRepoLocation, 
 				folderNameMapper, project.getName(), ShareProjectOperation.LAYOUT_SINGLE, false);
 		CompositeOperation compositeOperation = new CompositeOperation(shareProjectOperation.getId(), shareProjectOperation.getMessagesClass());
@@ -132,37 +114,72 @@ public class VirSatSvnVersionControlBackend implements IVirSatVersionControlBack
 		compositeOperation.run(checkInMonitor);
 		
 		checkStatus(compositeOperation);
-		
-		checkInMonitor.split(1);
 	}
 
 	@Override
 	public void update(IProject project, IProgressMonitor monitor) throws Exception {
 		SubMonitor updateMonitor = SubMonitor.convert(monitor, "Virtual Satellite svn update", 1);
 		
-		// The default UpdateOperation provided by SubVersive was unstable and sometimes failed.
-		// The following is basically the update functionality without all the baggage.
-		AbstractActionOperation updateProjectOperation = new AbstractActionOperation("Operation_UpdateProject", SVNMessages.class) {
-			@Override
-			protected void runImpl(IProgressMonitor monitor) throws Exception {
-				File workingCopy = new File(FileUtility.getWorkingCopyPath(project));
-				IRepositoryLocation location = SVNFileStorage.instance().asRepositoryResource(workingCopy, false).getRepositoryLocation();
-				final ISVNConnector proxy = location.acquireSVNProxy();
-				String[] paths = FileUtility.asPathArray(new File[] { workingCopy });
-				
-				try {
-					proxy.update(paths, SVNRevision.HEAD, SVNDepth.INFINITY, ISVNConnector.Options.ALLOW_UNVERSIONED_OBSTRUCTIONS, new SVNProgressMonitor(this, monitor, null));
-				} finally {
-					location.releaseSVNProxy(proxy);
-				}
-			}	
-		};
-		
+		UpdateProjectOperation updateProjectOperation = new UpdateProjectOperation(project);
 		updateProjectOperation.run(updateMonitor);
 		
 		checkStatus(updateProjectOperation);
 		
 		updateMonitor.split(1);
+	}
+	
+	// The default UpdateOperation provided by SubVersive was unstable and sometimes failed.
+	// The following is basically a low level call to the svn update functionality
+	private static class UpdateProjectOperation extends AbstractActionOperation {
+		
+		private IProject project;
+		
+		UpdateProjectOperation(IProject project) {
+			super("Operation_UpdateProject", SVNMessages.class);
+			this.project = project;
+		}
+
+		@Override
+		protected void runImpl(IProgressMonitor monitor) throws Exception {
+			File workingCopy = new File(FileUtility.getWorkingCopyPath(project));
+			IRepositoryLocation location = SVNFileStorage.instance().asRepositoryResource(workingCopy, false).getRepositoryLocation();
+			final ISVNConnector proxy = location.acquireSVNProxy();
+			String[] paths = FileUtility.asPathArray(new File[] { workingCopy });
+			
+			try {
+				proxy.update(paths, SVNRevision.HEAD, SVNDepth.INFINITY, ISVNConnector.Options.ALLOW_UNVERSIONED_OBSTRUCTIONS, new SVNProgressMonitor(this, monitor, null));
+			} finally {
+				location.releaseSVNProxy(proxy);
+			}
+		}
+	}
+	
+	/**
+	 * Gets the files that should be added & commited from the eclipse project
+	 * @param project the project to commit
+	 * @return the files to add & commit
+	 * @throws CoreException
+	 */
+	protected File[] getFilesToCommit(IProject project) throws CoreException {
+		List<File> filesList = new ArrayList<>();
+		
+		File projectWorkingCopy = new File(FileUtility.getWorkingCopyPath(project));
+		
+		// The subversive implementation sets a flag that requires the parent of a file that should be commited,
+		// to be under version control as well. We check here if the parent is under version control,
+		// and if so, then we can simply commit the project file. If not, we commit the commitable files under the
+		// project. We do not go any deeper and let SVN itself handle the recursive search for further commitables.
+		if (SVNUtility.hasSVNFolderInOrAbove(projectWorkingCopy.getParentFile())) {
+			filesList.add(projectWorkingCopy);
+		} else {
+			IResource[] resources = FileUtility.getResourcesRecursive(project.members(), IStateFilter.SF_UNVERSIONED, 0);
+			for (IResource resource : resources) {
+				filesList.add(new File(FileUtility.getWorkingCopyPath(resource)));
+			}
+		}
+		
+		// Perform an add + commit operation
+		return filesList.toArray(new File[0]);
 	}
 	
 	/**
