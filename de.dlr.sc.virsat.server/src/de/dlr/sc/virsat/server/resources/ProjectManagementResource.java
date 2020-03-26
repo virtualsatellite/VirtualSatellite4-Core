bb8934999510bf522d6c3b7575690b806372e382
Tobias Franz <tobias.franz@dlr.de>
2020-03-26 18:59:26 +0100

    Fix wrong import - (Task #540)

--- de.dlr.sc.virsat.server/src/de/dlr/sc/virsat/server/resources/ProjectManagementResource.java
+++ de.dlr.sc.virsat.server/src/de/dlr/sc/virsat/server/resources/ProjectManagementResource.java
@@ -21,17 +21,13 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import de.dlr.sc.virsat.server.configuration.RepositoryConfiguration;
-import de.dlr.sc.virsat.server.configuration.SimpleConf;
 import de.dlr.sc.virsat.server.controller.RepoManagemantController;
 import de.dlr.sc.virsat.server.repository.RepoRegistry;
 import de.dlr.sc.virsat.server.repository.ServerRepository;
-import de.dlr.sc.virsat.team.VersionControlSystem;
 
 @Path(ProjectManagementResource.PATH)
 public class ProjectManagementResource {
