dfdaf6cb3e499685f2c143aa0998d1bf340c8d5d
Philipp M. Fischer <philipp.fischer@dlr.de>
2020-03-27 11:20:37 +0100

    Added Atomic Exception for handling lambdas - (Task #537)
    - Added AtomicException for handling exceptions thrown over the scope of
    lambdas.
    
    ---
    Task #537: Persistency for server project repositories

---
+++ de.dlr.sc.virsat.commons.test/src/de/dlr/sc/virsat/commons/exception/AtomicExceptionTest.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2020 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.sc.virsat.commons.exception;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AtomicExceptionTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testThrowIfSet() throws Exception {
+		AtomicException<Exception> ae = new AtomicException<>();
+		ae.throwIfSet();
+		// Test case is successful if it reaches this point
+	}
+	
+	@Test(expected = Exception.class)
+	public void testThrowIfSetWithException() throws Exception {
+		AtomicException<Exception> ae = new AtomicException<>();
+		ae.set(new Exception());
+		ae.throwIfSet();
+	}
+}
