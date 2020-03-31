9e3c7db66053394ccabc087ccad347f582258a21
Philipp M. Fischer <35496033+PhilMFischer@users.noreply.github.com>
2020-03-31 10:58:35 +0200

    Update de.dlr.sc.virsat.team/src/de/dlr/sc/virsat/team/IVirSatVersionControlBackend.java
    Co-Authored-By: andriikovalov-dlr <48556822+andriikovalov-dlr@users.noreply.github.com>

--- de.dlr.sc.virsat.team/src/de/dlr/sc/virsat/team/IVirSatVersionControlBackend.java
+++ de.dlr.sc.virsat.team/src/de/dlr/sc/virsat/team/IVirSatVersionControlBackend.java
@@ -34,7 +34,7 @@ public interface IVirSatVersionControlBackend {
 	/**
 	 * Creates a local checkout of a project from the version control system
 	 * @param projectDescription the project metadata
-	 * @param pathRepoLoacl the local path in which to checkout the project.
+	 * @param pathRepoLocal the local path in which to checkout the project.
 	 *  .git or .svn folders should be created within. This folder is usually not your 
 	 *  repository home such as the git home folder.
 	 * @param remoteUri the uri of the version control system
