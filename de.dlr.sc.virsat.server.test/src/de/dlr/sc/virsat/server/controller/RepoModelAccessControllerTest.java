2a4c4d189419681408ff163dd2a6736864a30982
Philipp M. Fischer <philipp.fischer@dlr.de>
2020-04-03 12:30:08 +0200

    Added code to correctly create sei in project - (Task #555)
    - Creating one sei in transactional project
    
    ---
    Task #555: Add Low Level Data API to access SEIs and CAs to REST server

--- de.dlr.sc.virsat.server.test/src/de/dlr/sc/virsat/server/controller/RepoModelAccessControllerTest.java
+++ de.dlr.sc.virsat.server.test/src/de/dlr/sc/virsat/server/controller/RepoModelAccessControllerTest.java
@@ -14,17 +14,23 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 
+import org.eclipse.emf.common.command.Command;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.edit.command.AddCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.Before;
 import org.junit.Test;
 
 import de.dlr.sc.virsat.commons.file.VirSatFileUtils;
+import de.dlr.sc.virsat.model.dvlm.DVLMPackage;
 import de.dlr.sc.virsat.model.dvlm.Repository;
+import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
+import de.dlr.sc.virsat.model.dvlm.concepts.ConceptsFactory;
+import de.dlr.sc.virsat.model.dvlm.structural.StructuralElement;
 import de.dlr.sc.virsat.model.dvlm.structural.StructuralElementInstance;
 import de.dlr.sc.virsat.model.dvlm.structural.StructuralFactory;
 import de.dlr.sc.virsat.project.resources.VirSatResourceSet;
@@ -39,9 +45,6 @@ import de.dlr.sc.virsat.team.VersionControlSystem;
 public class RepoModelAccessControllerTest extends AProjectTestCase {
 	
 
-	private ServerRepository testServerRepository;
-	private Path pathRepoRemote;
-	private File localRepoHome;
 	private RepoModelAccessController repoModelAccessController;
 	
 	@Before
@@ -49,60 +52,32 @@ public class RepoModelAccessControllerTest extends AProjectTestCase {
 		super.setUp();
 		
 		
-		// Setup a complete working server project with a backend
-		// TODO: delete backend if possible
-		try {
-			pathRepoRemote = VirSatFileUtils.createAutoDeleteTempDirectory("VirtualSatelliteGitRemote_");
-			localRepoHome = VirSatFileUtils.createAutoDeleteTempDirectory("VirtualSatelliteLocalRepoHome_").toFile();
-			File fileGitRemoteRepo = pathRepoRemote.toFile();
-			Git.init().setDirectory(fileGitRemoteRepo).setBare(true).call();
-		} catch (IOException | IllegalStateException | GitAPIException e) {
-			Activator.getDefault().getLog().log(
-				new Status(
-					Status.ERROR,
-					Activator.getPluginId(),
-					"Error during repository setup",
-					e
-				)
-			);
-		}
+		super.addEditingDomainAndRepository();
 		
-		RepositoryConfiguration testRepoConfig = new RepositoryConfiguration(
-			TEST_PROJECT_NAME,
-			"",
-			pathRepoRemote.toUri().toString(),
-			VersionControlSystem.GIT,
-			"",
-			""
-		); 
-		try {
-			testServerRepository = new ServerRepository(localRepoHome, testRepoConfig);
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
+		StructuralElement se = StructuralFactory.eINSTANCE.createStructuralElement();
+		se.setName("TestRootComponent");
+		se.setIsRootStructuralElement(true);
 		
-		try {
-			testServerRepository.checkoutRepository();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		Concept concept = ConceptsFactory.eINSTANCE.createConcept();
+		concept.getStructuralElements().add(se);
 		
-//		IProject testProject = testServerRepository.getProject();
+		// Now add the repository but the repo is already within the transactional editing domain
+		// thus it needs to be added using the COmmand Framework and execute it in the editingdomain
 		
-		// Register Repo
-		RepoRegistry.getInstance().addRepository(testRepoConfig.getProjectName(), testServerRepository);
+		Command addConceptToRepo = AddCommand.create(editingDomain, repository, DVLMPackage.eINSTANCE.getRepository_ActiveConcepts(), concept);
+		editingDomain.getCommandStack().execute(addConceptToRepo);
 		
-		// Add a SEI
-//		projectCommons = new VirSatProjectCommons(testProject);
-		
-//		rs =  VirSatResourceSet.createUnmanagedResourceSet(testProject);
-		rs =  testServerRepository.getResourceSet();
-//		rs.initializeModelsAndResourceSet();
-		Repository repo = rs.getRepository();
+		// Create a new StructuralElementInstance
 		StructuralElementInstance sei1 = StructuralFactory.eINSTANCE.createStructuralElementInstance();
 		rs.getStructuralElementInstanceResource(sei1);
-//		IFile seiFile = projectCommons.getStructuralElementInstanceFile(sei1);
 		
+		sei1.setName("TestSEI");
+		sei1.setType(se);
+		
+		// Now add the new SEI to the Rpeository
+		
+		Command addSeiToRepo = AddCommand.create(editingDomain, repository, DVLMPackage.eINSTANCE.getRepository_RootEntities(), sei1);
+		editingDomain.getCommandStack().execute(addSeiToRepo);
 		
 		// Create the controller with the modelapi instance
 		repoModelAccessController = new RepoModelAccessController(TEST_PROJECT_NAME);
