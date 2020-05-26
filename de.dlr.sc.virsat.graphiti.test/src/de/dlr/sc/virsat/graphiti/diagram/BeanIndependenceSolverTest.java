01e4d470f74930cd4e60ae0a684e9e46509ad3c6
christoph-pietsch-DLR <christoph.pietsch@dlr.de>
2020-05-26 12:16:04 +0200

    Add test for BeanIndependenceSolver - (Task #286)
    ---
    Task #286: Handle different property types in BeanIndependenceSolver

--- de.dlr.sc.virsat.graphiti.test/src/de/dlr/sc/virsat/graphiti/diagram/BeanIndependenceSolverTest.java
+++ de.dlr.sc.virsat.graphiti.test/src/de/dlr/sc/virsat/graphiti/diagram/BeanIndependenceSolverTest.java
@@ -30,6 +30,8 @@ import org.junit.Test;
 import de.dlr.sc.virsat.concept.unittest.util.test.AConceptProjectTestCase;
 import de.dlr.sc.virsat.graphiti.util.DiagramHelper;
 import de.dlr.sc.virsat.model.concept.types.category.ABeanCategoryAssignment;
+import de.dlr.sc.virsat.model.concept.types.property.BeanPropertyResource;
+import de.dlr.sc.virsat.model.concept.types.property.BeanPropertyString;
 import de.dlr.sc.virsat.model.concept.types.structural.ABeanStructuralElementInstance;
 import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
 import de.dlr.sc.virsat.model.dvlm.concepts.registry.ActiveConceptConfigurationElement;
@@ -54,7 +56,16 @@ public class BeanIndependenceSolverTest extends AConceptProjectTestCase {
 	private static final String DOCUMENTUUID = "ea816464-aaaa-bbbb-ae91-31d37c60a63c";
 
 	private Concept conceptEgscc;
-	
+	private Document testDocument;
+	private Document testDocument2;
+	private BeanIndependenceSolver beanIndependenceSolver;
+	private ElementDefinition ed1;
+	private ElementDefinition ed2;
+	private StructuralElementInstance sei1;
+	private StructuralElementInstance sei2;
+	private Command command;
+
+	@Override
 	@Before
 	public void setUp() throws CoreException {
 		super.setUp();
@@ -62,10 +73,12 @@ public class BeanIndependenceSolverTest extends AConceptProjectTestCase {
 
 		addEditingDomainAndRepository();
 		activateCoreConcept();
-		
+
 		//CHECKSTYLE:OFF
 		ActiveConceptConfigurationElement accePs = new ActiveConceptConfigurationElement(null) {
+			@Override
 			public String getXmi() { return "concept/concept.xmi"; };
+			@Override
 			public String getId() { return CONCEPT_ID_EGSCC; };
 		};
 
@@ -76,91 +89,125 @@ public class BeanIndependenceSolverTest extends AConceptProjectTestCase {
 				accePs.createAddActiveConceptCommand(editingDomain, repository).execute();
 				ActiveConceptHelper acHelper = new ActiveConceptHelper(repository);
 				conceptEgscc = acHelper.getConcept(CONCEPT_ID_EGSCC);
-
 			}
-		});	
+		});
+
+		Concept concept = DiagramHelper.getConcept(editingDomain, CONCEPT_ID_EGSCC);
+		assertEquals(concept, conceptEgscc);
+
+		testDocument = new Document(concept);
+		testDocument2 = new Document(concept);
+		testDocument.getTypeInstance().setUuid(new VirSatUuid(DOCUMENTUUID));
+
+		IDiagramTypeProvider dtp = getDiagramTypeProvider();
+		beanIndependenceSolver = new BeanIndependenceSolver(dtp);
+
+		ed1 = new ElementDefinition(conceptEgscc);
+		ed2 = new ElementDefinition(conceptEgscc);
+		sei1 = ed1.getStructuralElementInstance();
+		sei2 = ed2.getStructuralElementInstance();
+		sei1.setUuid(new VirSatUuid(UUID));
+		ed1.add(testDocument2);
+
+		command = CreateAddStructuralElementInstanceCommand.create(editingDomain, repository, ed1.getStructuralElementInstance());
 	}
 
 	@Test
-	public void testSolveIndependence() throws CoreException, IOException { 	
-		// test getConcept
-		Concept concept = DiagramHelper.getConcept(editingDomain, CONCEPT_ID_EGSCC);
-		assertEquals(concept, conceptEgscc);
-		
-		// create Seis and Cas
-		ElementDefinition ed = new ElementDefinition(conceptEgscc);
-		StructuralElementInstance sei = ed.getStructuralElementInstance();
-		sei.setUuid(new VirSatUuid(UUID));
-		
-		Document testDocument2 = new Document(concept);
-		ed.add(testDocument2);
+	public void testSolveIndependenceForSei() throws CoreException, IOException {
+		String seiKey = beanIndependenceSolver.getKeyForBusinessObject(ed1);
+		assertEquals(UUID, seiKey);
 
-		ElementDefinition ed2 = new ElementDefinition(conceptEgscc);
-		StructuralElementInstance sei2 = ed2.getStructuralElementInstance();
-		
-		Document testDocument = new Document(concept);
-		
+		editingDomain.getCommandStack().execute(command);
+		getAndAddStructuralElementInstance(sei2);
+
+		addStructuralElementToResource(ed2);
+		Object sei2Object = beanIndependenceSolver.getBusinessObjectForKey(ed2.getUuid());
+		ABeanStructuralElementInstance aBean = (ABeanStructuralElementInstance) sei2Object;
+		assertEquals(ed2, aBean);
+
+		getAndAddStructuralElementInstance(sei1);
+		checkDiagramWritePermissionForStructuralElement(sei1);
+
+		addStructuralElementToResource(ed1);
+		Object obj = beanIndependenceSolver.getBusinessObjectForKey(seiKey);
+		aBean = (ABeanStructuralElementInstance) obj;
+		assertEquals(ed1, aBean);
+	}
+
+	@Test
+	public void testSolveIndependenceForBeanProperty() throws CoreException, IOException {
+		editingDomain.getCommandStack().execute(command);
+		getAndAddStructuralElementInstance(sei2);
+		addStructuralElementToResource(ed2);
+		getAndAddStructuralElementInstance(sei1);
+		checkDiagramWritePermissionForStructuralElement(sei1);
+		addStructuralElementToResource(ed1);
+
+		//test property
+		Object documentObj = beanIndependenceSolver.getBusinessObjectForKey(testDocument2.getUuid());
+		ABeanCategoryAssignment aCategory = (ABeanCategoryAssignment) documentObj;
+		assertEquals(testDocument2, aCategory);
+
+		Object beanNoteObj = beanIndependenceSolver.getBusinessObjectForKey(testDocument2.getNoteBean().getUuid());
+		BeanPropertyString beanPropertyString = (BeanPropertyString) beanNoteObj;
+		assertEquals(testDocument2.getNoteBean(), beanPropertyString);
+
+		Object beanUrlObj = beanIndependenceSolver.getBusinessObjectForKey(testDocument2.getUrlBean().getUuid());
+		BeanPropertyString beanPropertyUrlString = (BeanPropertyString) beanUrlObj;
+		assertEquals(testDocument2.getUrlBean(), beanPropertyUrlString);
+
+		Object beanFileObj = beanIndependenceSolver.getBusinessObjectForKey(testDocument2.getFileBean().getUuid());
+		BeanPropertyResource beanPropertyFileResource = (BeanPropertyResource) beanFileObj;
+		assertEquals(testDocument2.getFileBean(), beanPropertyFileResource);
+	}
+
+	@Test
+	public void testSolveIndependenceForCa() throws CoreException, IOException {
+		String caKey = beanIndependenceSolver.getKeyForBusinessObject(testDocument);
+		assertEquals(DOCUMENTUUID, caKey);
+	}
+
+	@Test
+	public void testSolveIndependenceNull() throws CoreException, IOException {
+		assertEquals(null, beanIndependenceSolver.getKeyForBusinessObject(null));
+	}
+
+	private void checkDiagramWritePermissionForStructuralElement(StructuralElementInstance sei) {
+		Boolean permission = DiagramHelper.hasDiagramWritePermission(sei);
+		assertEquals(true, permission);
+	}
+
+	private void addStructuralElementToResource(ElementDefinition ed) {
+		Resource resEd2 = rs.getStructuralElementInstanceResource(ed.getStructuralElementInstance());
+		resEd2.getContents().add(ed.getStructuralElementInstance());
+	}
+
+	/**
+	 * @return IDiagramTypeProvider
+	 */
+	private IDiagramTypeProvider getDiagramTypeProvider() {
 		// Create The diagram
 		Diagram diagram = Graphiti.getPeCreateService().createDiagram("test", "testDiagram", true);
-		IFolder diagramFolder = testProject.getFolder("src/diagrams/");  
-		IFile diagramFile = diagramFolder.getFile("testDiagram" + "." + "test");  
+		IFolder diagramFolder = testProject.getFolder("src/diagrams/");
+		IFile diagramFile = diagramFolder.getFile("testDiagram" + "." + "test");
 		URI uri = URI.createPlatformResourceURI(diagramFile.getFullPath().toString(), true);
 		DiagramHelper.createDiagram(uri, diagram, rs);
 
 		IDiagramTypeProvider dtp = GraphitiInternal.getEmfService().getDTPForDiagram(diagram);
 		GraphitiInternal.getEmfService().wireDTPToDiagram(diagram, dtp);
 		dtp.init(diagram, null);
-		BeanIndependenceSolver beanIndependenceSolver = new BeanIndependenceSolver(dtp);
-		String seiKey = beanIndependenceSolver.getKeyForBusinessObject(ed);
-		assertEquals(UUID, seiKey);
+		return dtp;
+	}
 
-		seiKey = beanIndependenceSolver.getKeyForBusinessObject(ed);
-		assertEquals(UUID, seiKey);
-
-		testDocument.getTypeInstance().setUuid(new VirSatUuid(DOCUMENTUUID));
-		String caKey = beanIndependenceSolver.getKeyForBusinessObject(testDocument);
-		assertEquals(DOCUMENTUUID, caKey);
-
-		caKey = beanIndependenceSolver.getKeyForBusinessObject(testDocument);
-		assertEquals(DOCUMENTUUID, caKey);
-
-		Command command = CreateAddStructuralElementInstanceCommand.create(editingDomain, repository, ed.getStructuralElementInstance());
-		editingDomain.getCommandStack().execute(command);
-
-		editingDomain.getVirSatCommandStack().execute(new RecordingCommand(editingDomain) {
-			@Override
-			protected void doExecute() {
-				rs.getAndAddStructuralElementInstanceResource(sei2);
-			}
-		});
-
-		Resource resEd2 = rs.getStructuralElementInstanceResource(ed2.getStructuralElementInstance());
-		resEd2.getContents().add(ed2.getStructuralElementInstance());
-		Object sei2Object = beanIndependenceSolver.getBusinessObjectForKey(ed2.getUuid());
-		ABeanStructuralElementInstance aBean = (ABeanStructuralElementInstance) sei2Object;
-		assertEquals(ed2, aBean);
-		
+	/**
+	 * @param sei StructuralElementInstance
+	 */
+	private void getAndAddStructuralElementInstance(StructuralElementInstance sei) {
 		editingDomain.getVirSatCommandStack().execute(new RecordingCommand(editingDomain) {
 			@Override
 			protected void doExecute() {
 				rs.getAndAddStructuralElementInstanceResource(sei);
 			}
 		});
-
-		Boolean permission = DiagramHelper.hasDiagramWritePermission(sei);
-		assertEquals(true, permission);
-		
-		Resource resEd = rs.getStructuralElementInstanceResource(ed.getStructuralElementInstance());
-		resEd.getContents().add(ed.getStructuralElementInstance());
-		Object obj = beanIndependenceSolver.getBusinessObjectForKey(seiKey);
-		aBean = (ABeanStructuralElementInstance) obj;
-		assertEquals(ed, aBean);
-
-		Object documentObj = beanIndependenceSolver.getBusinessObjectForKey(testDocument2.getUuid());
-		ABeanCategoryAssignment aCategory = (ABeanCategoryAssignment) documentObj;
-		assertEquals(testDocument2, aCategory);
-		
-		assertEquals(null, beanIndependenceSolver.getKeyForBusinessObject(null));
-
 	}
 }
