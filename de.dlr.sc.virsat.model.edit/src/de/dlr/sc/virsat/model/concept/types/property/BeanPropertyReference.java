1846c3c9db4785d7f998192ad8ebc589d6867912
JAmmermann-DLR <joshua.ammermann@dlr.de>
2020-06-02 12:43:45 +0200

    Implement BeanPropertyReference - (Task #606)
    Implement missing methods and add test cases.
    
    ---
    Task #606: Evaluate adding a BeanPropertyReference

--- de.dlr.sc.virsat.model.edit/src/de/dlr/sc/virsat/model/concept/types/property/BeanPropertyReference.java
+++ de.dlr.sc.virsat.model.edit/src/de/dlr/sc/virsat/model/concept/types/property/BeanPropertyReference.java
@@ -9,18 +9,22 @@
  *******************************************************************************/
 package de.dlr.sc.virsat.model.concept.types.property;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.edit.domain.EditingDomain;
 
 import de.dlr.sc.virsat.model.concept.types.ABeanObject;
 import de.dlr.sc.virsat.model.concept.types.IBeanObject;
+import de.dlr.sc.virsat.model.concept.types.factory.BeanCategoryAssignmentFactory;
 import de.dlr.sc.virsat.model.dvlm.categories.ATypeInstance;
+import de.dlr.sc.virsat.model.dvlm.categories.CategoryAssignment;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.APropertyInstance;
 import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.PropertyinstancesPackage;
 import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.ReferencePropertyInstance;
-import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.ValuePropertyInstance;
 
 public class BeanPropertyReference<BEAN_TYPE extends IBeanObject<? extends ATypeInstance>> extends ABeanObject<ReferencePropertyInstance> implements IBeanProperty<ReferencePropertyInstance, BEAN_TYPE> {
+
 
 	public BeanPropertyReference() {
 	}
@@ -31,31 +35,51 @@ public class BeanPropertyReference<BEAN_TYPE extends IBeanObject<? extends AType
 	
 	@Override
 	public void setValue(BEAN_TYPE value) {
-		ti.setReference(value.getTypeInstance());		
+		ti.setReference(value.getTypeInstance());
 	}
 
 	@Override
 	public Command setValue(EditingDomain ed, BEAN_TYPE value) {
-		// TODO Auto-generated method stub
-		return null;
+		return SetCommand.create(ed, ti, PropertyinstancesPackage.Literals.REFERENCE_PROPERTY_INSTANCE__REFERENCE, value.getTypeInstance());
 	}
 
+	/*
+	 * return property bean
+	 * or 
+	 * return ca bean
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public BEAN_TYPE getValue() {
-		// TODO Auto-generated method stub
-		return null;
+		BEAN_TYPE referencedBean = null;
+		
+		if (isSet()) {
+			ATypeInstance ref = ti.getReference();
+			
+			if (ref instanceof CategoryAssignment) {
+				BeanCategoryAssignmentFactory beanFactory = new BeanCategoryAssignmentFactory();
+				try {
+					// TODO: is this type cast save? If we could get the super class here, then not
+					referencedBean = (BEAN_TYPE) beanFactory.getInstanceFor((CategoryAssignment) ref);
+				} catch (CoreException e) {
+					throw new RuntimeException(e);
+				}
+			} else if (ref instanceof APropertyInstance) {
+				// TODO: Property Factory
+			} 
+		}
+		
+		return referencedBean;
 	}
 
 	@Override
 	public boolean isSet() {
-		// TODO Auto-generated method stub
-		return false;
+		return ti.getReference() != null;
 	}
 
 	@Override
 	public void unset() {
-		// TODO Auto-generated method stub
-		
+		ti.setReference(null);
 	}
 
 	
--- de.dlr.sc.virsat.model.extension.tests.test/src/de/dlr/sc/virsat/model/concept/types/property/BeanPropertyReferenceTest.java
+++ de.dlr.sc.virsat.model.extension.tests.test/src/de/dlr/sc/virsat/model/concept/types/property/BeanPropertyReferenceTest.java
@@ -1,7 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2020 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
 package de.dlr.sc.virsat.model.concept.types.property;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import org.eclipse.emf.common.command.Command;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,54 +30,90 @@ import de.dlr.sc.virsat.model.extension.tests.model.TestCategoryReference;
 public class BeanPropertyReferenceTest extends AConceptTestCase {
 
 	private Concept concept;
+	private TestCategoryAllProperty testCategoryAllProperty;
+	private TestCategoryReference testCategoryReference;
+	private CategoryAssignment testCategoryRefernceCa;
+	private CategoryAssignmentHelper caHelper;
+	private ReferencePropertyInstance rpi;
+	private BeanPropertyReference<TestCategoryAllProperty> beanTestRefCategory;
 
 	@Before
 	public void setup() {
 		prepareEditingDomain();
 
 		concept = loadConceptFromPlugin();
-	}
+	
+		testCategoryAllProperty = new TestCategoryAllProperty(concept);
+		testCategoryReference = new TestCategoryReference(concept);
+		
+		testCategoryRefernceCa = testCategoryReference.getTypeInstance();
+		caHelper = new CategoryAssignmentHelper(testCategoryRefernceCa);
+		rpi = (ReferencePropertyInstance) caHelper.getPropertyInstance(TestCategoryReference.PROPERTY_TESTREFCATEGORY);
 
+		beanTestRefCategory = new BeanPropertyReference<>(rpi);
+	}
+	
 	@After
 	public void tearDown() throws Exception {
 	}
 
 	@Test
 	public void testSetValueType() {
-		TestCategoryAllProperty testCategoryAllProperty = new TestCategoryAllProperty(concept);
-		TestCategoryReference testCategoryReference = new TestCategoryReference(concept);
-		
 		assertNull("No reference is set", testCategoryReference.getTestRefCategory());
-		
-		CategoryAssignment testCategoryRefernceCa = testCategoryReference.getTypeInstance();
-		CategoryAssignmentHelper caHelper = new CategoryAssignmentHelper(testCategoryRefernceCa);
-		ReferencePropertyInstance rpi = (ReferencePropertyInstance) caHelper.getPropertyInstance(TestCategoryReference.PROPERTY_TESTREFCATEGORY);
 
-		BeanPropertyReference<TestCategoryAllProperty> beanTestRefCategory = new BeanPropertyReference<>(rpi);
 		beanTestRefCategory.setValue(testCategoryAllProperty);
 
+		// TODO: remove one?
 		assertEquals("Correct reference has been set", rpi.getReference(), testCategoryAllProperty.getATypeInstance());
 		assertEquals("Correct Category assignment has been set", testCategoryAllProperty, testCategoryReference.getTestRefCategory());
 	}
 
 	@Test
 	public void testSetValueEditingDomainType() {
-		fail("Not yet implemented");
+		assertNull("No reference is set", testCategoryReference.getTestRefCategory());
+		
+		Command setCommand = beanTestRefCategory.setValue(editingDomain, testCategoryAllProperty);
+		editingDomain.getCommandStack().execute(setCommand);
+
+		// TODO: remove one?
+		assertEquals("Correct reference has been set", rpi.getReference(), testCategoryAllProperty.getATypeInstance());
+		assertEquals("Correct Category assignment has been set", testCategoryAllProperty, testCategoryReference.getTestRefCategory());
 	}
 
 	@Test
 	public void testGetValue() {
-		fail("Not yet implemented");
+		assertNull("No value set initially", beanTestRefCategory.getValue());
+		
+		beanTestRefCategory.setValue(testCategoryAllProperty);
+
+		assertEquals("Correct reference to ca returned", testCategoryAllProperty, beanTestRefCategory.getValue());
+		
+		ReferencePropertyInstance rpiToProperty = (ReferencePropertyInstance) caHelper.getPropertyInstance(TestCategoryReference.PROPERTY_TESTREFPROPERTY);
+		BeanPropertyReference<BeanPropertyString> beanTestRefProperty = new BeanPropertyReference<>(rpiToProperty);
+		
+		BeanPropertyString beanTestString = testCategoryAllProperty.getTestStringBean();
+		beanTestRefProperty.setValue(beanTestString);
+		
+		assertEquals("Correct reference to property returned", beanTestString, beanTestRefProperty.getValue());
 	}
 
 	@Test
 	public void testIsSet() {
-		fail("Not yet implemented");
+		assertFalse("No value set initially", beanTestRefCategory.isSet());
+		
+		beanTestRefCategory.setValue(testCategoryAllProperty);
+		
+		assertTrue("Now a value is set", beanTestRefCategory.isSet());
 	}
 
 	@Test
 	public void testUnset() {
-		fail("Not yet implemented");
+		beanTestRefCategory.setValue(testCategoryAllProperty);
+		assertEquals("Correct reference has been set", testCategoryAllProperty.getATypeInstance(), rpi.getReference());
+		
+		beanTestRefCategory.unset();
+		
+		assertNull("Reference has been unset", rpi.getReference());
 	}
 
 }
