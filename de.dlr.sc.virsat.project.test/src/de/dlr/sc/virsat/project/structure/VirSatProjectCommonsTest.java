4399df41f958ee0347e3d70c0681464cf222a1de
Philipp M. Fischer <philipp.fischer@dlr.de>
2020-06-09 08:42:15 +0200

    Extended Test case in VirSatProjectCommons - (Task #632)
    - Checked that getWorkspaceResource is handing back correct Null in case
    the resource for the eObject does not exist anymore.
    
    ---
    Task #632: Fix shakey code coverage

--- de.dlr.sc.virsat.project.test/src/de/dlr/sc/virsat/project/structure/VirSatProjectCommonsTest.java
+++ de.dlr.sc.virsat.project.test/src/de/dlr/sc/virsat/project/structure/VirSatProjectCommonsTest.java
@@ -16,6 +16,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -344,7 +345,7 @@ public class VirSatProjectCommonsTest extends AProjectTestCase {
 	}
 	
 	@Test
-	public void testGetWorkspaceResource() throws IOException {
+	public void testGetWorkspaceResource() throws IOException, CoreException {
 		VirSatResourceSet resSet = VirSatResourceSet.createUnmanagedResourceSet(testProject);
 		resSet.getResources().clear();
 		VirSatProjectCommons projectCommons = new VirSatProjectCommons(testProject); 
@@ -375,8 +376,16 @@ public class VirSatProjectCommonsTest extends AProjectTestCase {
 		IFile fileRepo = projectCommons.getRepositoryFile();
 		IFile fileSc = projectCommons.getStructuralElementInstanceFile(seiEdSc);
 		
+		// Test the getWorkspace method with two files that actually exist.
 		assertEquals("Got correct Resource", fileRepo, VirSatProjectCommons.getWorkspaceResource(repo));
 		assertEquals("Got correct Resource", fileSc, VirSatProjectCommons.getWorkspaceResource(seiEdSc));
+		
+		// Now remove both files from the workspace, thus the getWorkspaceResource
+		fileRepo.delete(true, null);
+		fileSc.delete(true, null);
+		
+		assertNull("There is no resource in the Workspace anymore", VirSatProjectCommons.getWorkspaceResource(repo));
+		assertNull("There is no resource in the Workspace anymore", VirSatProjectCommons.getWorkspaceResource(seiEdSc));
 	}
 	
 	@Test
