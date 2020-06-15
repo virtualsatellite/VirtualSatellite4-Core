e06b9c6d565c465fa7d45935b8c0540fe9abb862
JAmmermann-DLR <joshua.ammermann@dlr.de>
2020-06-15 14:13:24 +0200

    Initial version for svn ui test setup - (Task #642)
    Add setup and share project functionality
    
    ---
    Task #642: SwtBot tests for SVN/Git backend

--- de.dlr.sc.virsat.swtbot.test/src/de/dlr/sc/virsat/swtbot/test/versioningbackend/SvnVersioningBackendAndUserRightsManagementTest.java
+++ de.dlr.sc.virsat.swtbot.test/src/de/dlr/sc/virsat/swtbot/test/versioningbackend/SvnVersioningBackendAndUserRightsManagementTest.java
@@ -9,18 +9,49 @@
  *******************************************************************************/
 package de.dlr.sc.virsat.swtbot.test.versioningbackend;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
+import de.dlr.sc.virsat.commons.file.VirSatFileUtils;
+
 public class SvnVersioningBackendAndUserRightsManagementTest extends AVersioningBackendAndUserRightsManagementTest {
 
+	public static final String TEST_REPO_PATH_UPSTREAM = "SwtBotSvnBackendUpstreamRepo";
+	
 	@Override
-	protected void setUpVersioningBackend() {
-		// TODO Auto-generated method stub
+	protected void setUpVersioningBackend() throws IOException {
+
+		Path upstreamRepoPath = VirSatFileUtils.createAutoDeleteTempDirectory(TEST_REPO_PATH_UPSTREAM);
+		
+		String upstreamRepoPathName = upstreamRepoPath.toString();
+		
+		// First step open the perspective for svn operations
+		bot.toolbarButtonWithTooltip("Open Perspective").click();
+		bot.table().select("SVN Repository Exploring");
+		bot.table().getTableItem("SVN Repository Exploring").doubleClick();
+		bot.viewByTitle("SVN Repositories").show();
+		
+		// Open the dialog to create a new remote repository
+		bot.toolbarButtonWithTooltip("New Repository").click();
+		bot.textWithLabel("Repository Path:").setText(upstreamRepoPathName);
+		bot.button("OK").click();
 		
 	}
 
 	@Override
 	protected void shareTestProjectWithVersioningBackend() {
-		// TODO Auto-generated method stub
+		// Open the context menu of the test project
+		bot.viewByTitle("Project Explorer").show();
+		bot.tree().getTreeItem(SWTBOT_TEST_PROJECTNAME).contextMenu("Team").menu("Share Project...").click();
 		
+		// Open the share project dialog and share the project
+		bot.table().select("SVN");
+		bot.button("Next >").click();
+		bot.button("Finish").click();
+		
+		// Create the initial commit
+		bot.styledText().setText("Initial commit");
+		bot.button("OK").click();
 	}
 
 }
