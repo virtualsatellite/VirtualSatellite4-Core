c5d5fc8636608ebfb900de9566ea1c3059e7bc86
SaMuellerDLR <sa.mueller@dlr.de>
2020-06-26 03:13:10 +0200

    Moving test class to appropriate plugin - (Task #665)
    ---
    Task #665: Upgrade to new Eclipse Version 2020-06

--- de.dlr.sc.virsat.model.extension.tests.edit.test/src/de/dlr/sc/virsat/model/extension/tests/edit/test/AllTests.java
+++ de.dlr.sc.virsat.model.extension.tests.edit.test/src/de/dlr/sc/virsat/model/extension/tests/edit/test/AllTests.java
@@ -13,7 +13,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import de.dlr.sc.virsat.project.resources.dmf.DmfResourceTest;
 import junit.framework.JUnit4TestAdapter;
 
 /**
@@ -21,8 +20,7 @@ import junit.framework.JUnit4TestAdapter;
  */
 @RunWith(Suite.class)
 
-@SuiteClasses({	DmfResourceTest.class,
-				})
+@SuiteClasses({})
 
 /**
  * 
