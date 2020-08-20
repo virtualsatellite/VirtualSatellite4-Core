dbe05b6093d3e7c91a3705dd1e45eddcc4fc3ddd
Anna-Lena-DLR <anna-lena.gefke@dlr.de>
2020-08-20 11:52:54 +0200

    Updated Overtarget version to latest release - (Task #722)
    ---
    Task #722: Update Overtarget version

--- bash/maven_build.sh
+++ bash/maven_build.sh
@@ -22,8 +22,8 @@ set -e
 COMMAND=$0
 
 # set general variables to correctly execute the build process
-export OVERTARGET_VERSION=1.1.0.r201903120902
-export OVERTARGET_REPO=https://sourceforge.net/projects/overtarget/files/release/1.1.0/25/plugins
+export OVERTARGET_VERSION=1.2.1.r202007301216
+export OVERTARGET_REPO=https://sourceforge.net/projects/overtarget/files/release/1.2.1/483/plugins/
 export OVERTARGET_GROUP=de.dlr.sc.overtarget
 export JUNIT_DEBUG_PROJECT_TEST_CASE=true
 export SWTBOT_SCREENSHOT=true
