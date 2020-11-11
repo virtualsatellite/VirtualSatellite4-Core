a8eabbb3948adce17ff939c0f59c977392ae9071
Tobias Franz <tobias.franz@dlr.de>
2020-11-11 15:14:49 +0100

    Update generated codeof requirements concept

--- de.dlr.sc.virsat.model.extension.requirements/src-gen/de/dlr/sc/virsat/model/extension/requirements/model/ARequirementsConfiguration.java
+++ de.dlr.sc.virsat.model.extension.requirements/src-gen/de/dlr/sc/virsat/model/extension/requirements/model/ARequirementsConfiguration.java
@@ -12,14 +12,17 @@ package de.dlr.sc.virsat.model.extension.requirements.model;
 // *****************************************************************
 // * Import Statements
 // *****************************************************************
+import javax.xml.bind.annotation.XmlAccessorType;
 import de.dlr.sc.virsat.model.concept.types.category.IBeanCategoryAssignment;
 import de.dlr.sc.virsat.model.dvlm.concepts.util.ActiveConceptHelper;
 import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.ValuePropertyInstance;
+import javax.xml.bind.annotation.XmlRootElement;
 import de.dlr.sc.virsat.model.dvlm.categories.util.CategoryInstantiator;
 import de.dlr.sc.virsat.model.concept.list.IBeanList;
 import de.dlr.sc.virsat.model.dvlm.categories.Category;
 import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.ArrayInstance;
 import de.dlr.sc.virsat.model.concept.types.property.BeanPropertyString;
+import javax.xml.bind.annotation.XmlAccessType;
 import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.common.command.Command;
@@ -28,6 +31,7 @@ import de.dlr.sc.virsat.model.dvlm.categories.CategoryAssignment;
 import de.dlr.sc.virsat.model.concept.list.TypeSafeComposedPropertyInstanceList;
 import de.dlr.sc.virsat.model.concept.types.property.BeanPropertyComposed;
 import de.dlr.sc.virsat.model.ext.core.model.GenericCategory;
+import javax.xml.bind.annotation.XmlElement;
 
 
 // *****************************************************************
@@ -42,6 +46,8 @@ import de.dlr.sc.virsat.model.ext.core.model.GenericCategory;
  * 
  * 
  */	
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
 public abstract class ARequirementsConfiguration extends GenericCategory implements IBeanCategoryAssignment {
 
 	public static final String FULL_QUALIFIED_CATEGORY_NAME = "de.dlr.sc.virsat.model.extension.requirements.RequirementsConfiguration";
@@ -104,6 +110,7 @@ public abstract class ARequirementsConfiguration extends GenericCategory impleme
 		return fileName.getValue();
 	}
 	
+	@XmlElement
 	public BeanPropertyString getFileNameBean() {
 		safeAccessFileName();
 		return fileName;
@@ -133,6 +140,7 @@ public abstract class ARequirementsConfiguration extends GenericCategory impleme
 		}
 	}
 	
+	@XmlElement
 	public IBeanList<BeanPropertyComposed<RequirementType>> getTypeDefinitionsBean() {
 		safeAccessTypeDefinitionsBean();
 		return typeDefinitionsBean;
