7802b1998e9eb9dc169e561b37152cffcc82b344
SaMuellerDLR <48356419+SaMuellerDLR@users.noreply.github.com>
2020-11-25 15:46:59 +0100

    Update SceneGraphClientTest.java

--- de.dlr.sc.virsat.model.extension.visualisation.test/src/de/dlr/sc/virsat/model/extension/visualisation/treemanager/networking/SceneGraphClientTest.java
+++ de.dlr.sc.virsat.model.extension.visualisation.test/src/de/dlr/sc/virsat/model/extension/visualisation/treemanager/networking/SceneGraphClientTest.java
@@ -9,39 +9,8 @@
  *******************************************************************************/
 package de.dlr.sc.virsat.model.extension.visualisation.treemanager.networking;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
-import de.dlr.sc.virsat.model.extension.visualisation.treemanager.IVisUpdateHandler;
-import de.dlr.sc.virsat.model.extension.visualisation.treemanager.TreeManager;
-import de.dlr.sc.visproto.VisProto.SceneGraph;
 
 public class SceneGraphClientTest {
 	
-	@Test
-	public void testConstructor() {
-		TreeManager treeManager = new TreeManager();
-		
-		class MockSceneGraphClient extends SceneGraphClient {
-			protected boolean sendRequest = false;
-			
-			MockSceneGraphClient(TreeManager treeManager, IVisUpdateHandler visUpdateHandler) {
-				super(treeManager, visUpdateHandler);
-			}
-			
-			@Override
-			public void sendSceneGraph(SceneGraph sceneGraph) {
-				sendRequest = true;
-			}
-		}
-		
-		MockSceneGraphClient sceneGraphClient = new MockSceneGraphClient(treeManager, null);
-		
-		assertFalse("No send request received yet", sceneGraphClient.sendRequest);
-		treeManager.resumeSending();
-		assertTrue("Send request received", sceneGraphClient.sendRequest);
-	}
 
 }
