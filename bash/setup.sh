405b410cd7867b145c791c3c90c1e6ce4bf3725b
SaMuellerDLR <sa.mueller@dlr.de>
2020-11-25 12:57:38 +0100

    Starting xvfb in a different way - (Task #830)
    ---
    Task #830: Try out GitHub Actions

--- bash/setup.sh
+++ bash/setup.sh
@@ -6,7 +6,7 @@
 # ---------
 
 echo "Installing tools"
-sudo apt-get install libvtk6-java libzmq5 libzmq-java libzmq-jni metacity ant expect
+sudo apt-get install libvtk6-java libzmq5 libzmq-java libzmq-jni metacity ant expect xvfb
 
 # -----------------------------------
 # Startup the metacity window manager
