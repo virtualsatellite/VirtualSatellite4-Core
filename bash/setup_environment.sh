21a52792a77e99098ba0a41fd898ff2a63cef2fa
Philipp M. Fischer <35496033+PhilMFischer@users.noreply.github.com>
2020-11-25 16:04:34 +0100

    Update setup_environment.sh

--- bash/setup_environment.sh
+++ bash/setup_environment.sh
@@ -71,7 +71,7 @@ augmentLdLibraryPath $(dirname $JAVA_LIB_JVM)
 # -------------------------------------
 # Start setting up vtk and zmq so dirs
 # ------------------------------------
-
+ls -l /usr/lib/x86_64-linux-gnu/jni/
 EXPECTED_JNI_SO_DIR=/usr/lib/x86_64-linux-gnu/jni/
 EXPECTED_SO_DIR=/usr/lib/x86_64-linux-gnu/
 
@@ -79,8 +79,8 @@ EXPECTED_SO_DIR=/usr/lib/x86_64-linux-gnu/
 # Some debug - list what is in the java dir, try to find vtk.jar on travis-ci
 ls -l /usr/share/java/
 #export VS_JAR_VTK=/usr/share/java/vtk7.jar
-export VS_JAR_VTK=/usr/share/java/vtk6.jar
-export VS_JAR_ZMQ=/usr/share/java/jzmq.jar
+export VS_JAR_VTK=$(find /usr/share/java/vtk6.jar)
+export VS_JAR_ZMQ=$(find /usr/share/java/jzmq.jar)
 
 echo "Setting VS_JAR_VTK to: ${VS_JAR_VTK}"
 echo "Setting VS_JAR_ZMQ to: ${VS_JAR_ZMQ}"
