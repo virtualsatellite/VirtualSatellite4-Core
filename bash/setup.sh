eaf24456c0f1be899ac2078779fb27b00de6ae14
Tobias Franz <tobias.franz@dlr.de>
2020-11-26 14:22:46 +0100

    Add jq to setup script

--- bash/setup.sh
+++ bash/setup.sh
@@ -7,7 +7,7 @@
 
 echo "Installing tools"
 sudo apt-get update
-sudo apt-get install xvfb metacity libvtk6-java libzmq5 libzmq-java libzmq-jni ant expect
+sudo apt-get install xvfb metacity libvtk6-java libzmq5 libzmq-java libzmq-jni ant expect jq 
   
 # -------------------------------------------------------------------
 # create adirectory for Overtarget and try to receive language plugin
