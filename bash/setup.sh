278ed2a2e6523959a8287d77f6ed72e2333e09c0
SaMuellerDLR <48356419+SaMuellerDLR@users.noreply.github.com>
2020-11-27 21:56:33 +0100

    Streamlining variable names

--- bash/setup.sh
+++ bash/setup.sh
@@ -48,8 +48,8 @@ mkdir -p -m 700 ./tmp/.sourceforge_ssh
 echo "OpenSSL is version"
 openssl version
 echo "Executing openssl"
-openssl aes-256-cbc -d -a -pbkdf2 -in ./id_ed25519.enc -out ./tmp/.sourceforge_ssh/id_ed25519_dec -pass pass:t${openssl_pass}
+openssl aes-256-cbc -d -a -pbkdf2 -in ./id_ed25519.enc -out ./tmp/.sourceforge_ssh/id_ed25519_dec -pass pass:${openssl_pass}
 echo "Adjusting rights"
 chmod 600 ./tmp/.sourceforge_ssh/id_ed25519_dec
 echo "Adding passwords"
-./bash/ssh-add-password.sh -k ./tmp/.sourceforge_ssh/id_ed25519_dec -p ${ssh_key_pass} 2>/dev/null
+./bash/ssh-add-password.sh -k ./tmp/.sourceforge_ssh/id_ed25519_dec -p ${ssh_password} 2>/dev/null
