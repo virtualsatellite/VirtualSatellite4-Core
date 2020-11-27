cf413713b3401b5b0734b046f9e7a5b650e14fc9
SaMuellerDLR <48356419+SaMuellerDLR@users.noreply.github.com>
2020-11-27 22:24:27 +0100

    Moved decript to deploy sourceforge bash script

--- bash/deploy_sourceforge.sh
+++ bash/deploy_sourceforge.sh
@@ -57,6 +57,18 @@ while [ "$1" != "" ]; do
     shift
 done
 
+# Prepare sourceforge secrets
+echo "Connecting to sourceforge"
+mkdir -p -m 700 tmp/.sourceforge_ssh
+echo "OpenSSL is version"
+openssl version
+echo "Executing openssl"
+openssl aes-256-cbc -d -a -pbkdf2 -in id_ed25519.enc -out tmp/.sourceforge_ssh/id_ed25519_dec -pass pass:${openssl_pass}
+echo "Adjusting rights"
+chmod 600 tmp/.sourceforge_ssh/id_ed25519_dec
+echo "Adding passwords"
+bash/ssh-add-password.sh -k tmp/.sourceforge_ssh/id_ed25519_dec -p ${ssh_key_pass} 2>/dev/null
+
 # Setup ssh with sourceforge secrets
 sourceforgeDecryptSecret
 
