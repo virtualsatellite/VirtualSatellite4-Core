6045055b42a5f7704f4566bbfb1d97dad8acee6a
SaMuellerDLR <48356419+SaMuellerDLR@users.noreply.github.com>
2020-11-27 18:11:42 +0100

    Added missing whitespace

--- bash/setup.sh
+++ bash/setup.sh
@@ -38,7 +38,7 @@ ssh-keyscan "frs.sourceforge.net" >> "${SSH_DIR}/known_hosts"
 echo "Connecting to sourceforge"
 mkdir -p -m 700 ./tmp/.sourceforge_ssh
 echo "Executing openssl"
-openssl aes-256-cbc -d -a -pbkdf2 -in ./id_ed25519.enc -out ./tmp/.sourceforge_ssh/id_ed25519_dec -pass pass:t${{ secrets.SF_SSH_KEY_OPENSSL_PASS}}
+openssl aes-256-cbc -d -a -pbkdf2 -in ./id_ed25519.enc -out ./tmp/.sourceforge_ssh/id_ed25519_dec -pass pass:t${{ secrets.SF_SSH_KEY_OPENSSL_PASS }}
 echo "Adjusting rights"
 chmod 600 ./tmp/.sourceforge_ssh/id_ed25519_dec
 echo "Adding passwords"
