f70d7066d80ae92031879d2fc96621512f3044f0
SaMuellerDLR <48356419+SaMuellerDLR@users.noreply.github.com>
2020-11-27 19:44:20 +0100

    Trying to update openssl from fixed versio

--- bash/setup.sh
+++ bash/setup.sh
@@ -7,7 +7,14 @@
 
 echo "Installing tools"
 sudo apt-get update
-sudo apt-get install openjdk-8-jdk xvfb metacity libvtk6-java libzmq5 libzmq-java libzmq-jni ant expect jq openssl
+sudo apt-get install openjdk-8-jdk xvfb metacity libvtk6-java libzmq5 libzmq-java libzmq-jni ant expect jq
+
+wget https://www.openssl.org/source/old/1.1.0/openssl-1.1.0g.tar.gz
+tar xzvf openssl-1.1.0g.tar.gz
+cd openssl-1.1.0g
+./config
+make
+sudo make install
   
 # -------------------------------------------------------------------
 # create adirectory for Overtarget and try to receive language plugin
