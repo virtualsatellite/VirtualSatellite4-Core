cea3de454e66a848fcb6f836ebcdde1fbca6dcca
SaMuellerDLR <48356419+SaMuellerDLR@users.noreply.github.com>
2020-11-27 22:18:25 +0100

    Moving sourceforge setup to deploy

--- bash/setup.sh
+++ bash/setup.sh
@@ -42,14 +42,3 @@ mkdir -p "${SSH_DIR}"
 touch "${SSH_DIR}/known_hosts"
 chmod 600 "${SSH_DIR}/known_hosts"
 ssh-keyscan "frs.sourceforge.net" >> "${SSH_DIR}/known_hosts"
-
-echo "Connecting to sourceforge"
-mkdir -p -m 700 tmp/.sourceforge_ssh
-echo "OpenSSL is version"
-openssl version
-echo "Executing openssl"
-openssl aes-256-cbc -d -a -pbkdf2 -in id_ed25519.enc -out tmp/.sourceforge_ssh/id_ed25519_dec -pass pass:${openssl_pass}
-echo "Adjusting rights"
-chmod 600 tmp/.sourceforge_ssh/id_ed25519_dec
-echo "Adding passwords"
-bash/ssh-add-password.sh -k tmp/.sourceforge_ssh/id_ed25519_dec -p ${ssh_key_pass} 2>/dev/null
