7bc2ce38f5000ceaa993aa4a0d3cb7f44351fc23
SaMuellerDLR <48356419+SaMuellerDLR@users.noreply.github.com>
2020-11-27 22:32:00 +0100

    Moved sourceforge deployment code into one method

--- bash/deploy_sourceforge.sh
+++ bash/deploy_sourceforge.sh
@@ -23,19 +23,49 @@ printUsage() {
     echo "usage: ${COMMAND} -u [development|integration|release]"
 }
 
+prepareSourceforgeSecrets() {
+	# Start the ssh agent
+	echo "Starting ssh-agent"
+	eval "$(ssh-agent -s)"
+
+	# Decrypt the key for accessign the 
+	# deployment store and add it to ssh-agent
+	echo "Adding sourceforge as known SSH host"
+	SSH_DIR="$HOME/.ssh"
+	mkdir -p "${SSH_DIR}"
+	touch "${SSH_DIR}/known_hosts"
+	chmod 600 "${SSH_DIR}/known_hosts"
+	ssh-keyscan "frs.sourceforge.net" >> "${SSH_DIR}/known_hosts"
+
+	# Prepare sourceforge secrets
+	echo "Connecting to sourceforge"
+	mkdir -p -m 700 tmp/.sourceforge_ssh
+	echo "OpenSSL is version"
+	openssl version
+	echo "Executing openssl"
+	openssl aes-256-cbc -d -a -pbkdf2 -in id_ed25519.enc -out tmp/.sourceforge_ssh/id_ed25519_dec -pass pass:${openssl_pass}
+	echo "Adjusting rights"
+	chmod 600 tmp/.sourceforge_ssh/id_ed25519_dec
+	echo "Adding passwords"
+	bash/ssh-add-password.sh -k tmp/.sourceforge_ssh/id_ed25519_dec -p ${ssh_key_pass} 2>/dev/null
+}
+
 uploadDevelopment() {
+	prepareSourceforgeSecrets();
 	rsync -e ssh -avP --delete deploy/unsecured/p2/VirSat4_Core_Application/development/  dlrscmns@frs.sourceforge.net:/home/frs/project/virtualsatellite/VirtualSatellite4-Core/development/
 	rsync -e ssh -avP --delete deploy/unsecured/p2/VirSat4_Dvlm_ConceptIDE/development/  dlrscmns@frs.sourceforge.net:/home/frs/project/virtualsatellite/VirtualSatellite4-DVLM/development/
 	rsync -e ssh -avP --delete deploy/unsecured/bin/VirSat4_Core_Application/development/  dlrscmns@frs.sourceforge.net:/home/frs/project/virtualsatellite/VirtualSatellite4-Core/bin/development/
 }
 
 uploadIntegration() {
+	prepareSourceforgeSecrets();
 	rsync -e ssh -avP deploy/unsecured/p2/VirSat4_Core_Application/integration/  dlrscmns@frs.sourceforge.net:/home/frs/project/virtualsatellite/VirtualSatellite4-Core/integration/
 	rsync -e ssh -avP deploy/unsecured/p2/VirSat4_Dvlm_ConceptIDE/integration/  dlrscmns@frs.sourceforge.net:/home/frs/project/virtualsatellite/VirtualSatellite4-DVLM/integration/
 	rsync -e ssh -avP deploy/unsecured/bin/VirSat4_Core_Application/integration/  dlrscmns@frs.sourceforge.net:/home/frs/project/virtualsatellite/VirtualSatellite4-Core/bin/integration/
 }
 
 uploadRelease() {
+	prepareSourceforgeSecrets();
 	rsync -e ssh -avP deploy/secured/p2/VirSat4_Core_Application/release/  dlrscmns@frs.sourceforge.net:/home/frs/project/virtualsatellite/VirtualSatellite4-Core/release/
 	rsync -e ssh -avP deploy/secured/p2/VirSat4_Dvlm_ConceptIDE/release/  dlrscmns@frs.sourceforge.net:/home/frs/project/virtualsatellite/VirtualSatellite4-DVLM/release/
 	rsync -e ssh -avP deploy/secured/bin/VirSat4_Core_Application/release/  dlrscmns@frs.sourceforge.net:/home/frs/project/virtualsatellite/VirtualSatellite4-Core/bin/release/
@@ -56,18 +86,6 @@ while [ "$1" != "" ]; do
     esac
     shift
 done
-
-# Prepare sourceforge secrets
-echo "Connecting to sourceforge"
-mkdir -p -m 700 tmp/.sourceforge_ssh
-echo "OpenSSL is version"
-openssl version
-echo "Executing openssl"
-openssl aes-256-cbc -d -a -pbkdf2 -in id_ed25519.enc -out tmp/.sourceforge_ssh/id_ed25519_dec -pass pass:${openssl_pass}
-echo "Adjusting rights"
-chmod 600 tmp/.sourceforge_ssh/id_ed25519_dec
-echo "Adding passwords"
-bash/ssh-add-password.sh -k tmp/.sourceforge_ssh/id_ed25519_dec -p ${ssh_key_pass} 2>/dev/null
 
 # Setup ssh with sourceforge secrets
 sourceforgeDecryptSecret
