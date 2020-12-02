8fe6a4d1b5c371c2ad3e421fd99f3cc8e328fab4
fisc_ph <fisc_ph@SC-010165.intra.dlr.de>
2020-12-02 08:25:17 +0100

    Added locale setup to Actions workflow script - (Task #846)
    - SWTBot needs a specific locale. Changed script to use en_US
    - Also removed run on overtarget which is intrinsic to the
    maven_build.sh script
    
    ---
    Task #846: GitHub Actions - Bionic Migration

--- bash/setup.sh
+++ bash/setup.sh
@@ -22,8 +22,15 @@ sudo apt-get install openjdk-8-jdk xvfb metacity libvtk6-java libzmq5 libzmq-jav
 # cd ..
   
 # -------------------------------------------------------------------
-# create adirectory for Overtarget and try to receive language plugin
+# generating and setting locale for en_US needed by swtbot
 # -------------------------------------------------------------------
 
-echo "Installing and running OverTarget"
-./bash/maven_build.sh -j dependencies -p dependencies
+echo "Generating locale"
+sudo locale-gen en_US.UTF-8
+
+echo "Setting up locale"
+sudo update-locale LANG=en_US.UTF-8
+
+echo "Show current locales"
+locale
+
