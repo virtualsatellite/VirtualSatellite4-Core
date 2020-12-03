3ec2460b943366573194ff07e021eb8a6f4b1660
SaMuellerDLR <sa.mueller@dlr.de>
2020-12-03 23:20:44 +0100

    Readding timeout rule - (Task #887)
    ---
    Task #887: Improve stability of RoleManagementTest

--- de.dlr.sc.virsat.project.test/src/de/dlr/sc/virsat/project/test/AProjectTestCase.java
+++ de.dlr.sc.virsat.project.test/src/de/dlr/sc/virsat/project/test/AProjectTestCase.java
@@ -26,7 +26,10 @@ import org.eclipse.emf.transaction.RecordingCommand;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.rules.DisableOnDebug;
 import org.junit.rules.TestName;
+import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
 
 import de.dlr.sc.virsat.model.dvlm.Repository;
 import de.dlr.sc.virsat.model.dvlm.roles.IUserContext;
@@ -46,6 +49,8 @@ public abstract class AProjectTestCase {
 	protected static final int MAX_TEST_CASE_TIMEOUT_SECONDS = 30;
 	protected static final int MAX_TEST_CASE_WAIT_TIME_MILLI_SECONDS = 1000;
 	
+	@Rule
+	public TestRule globalTimeout = new DisableOnDebug(Timeout.seconds(MAX_TEST_CASE_TIMEOUT_SECONDS));
 	
 	protected static final String TEST_PROJECT_NAME = "testProject";
 	private static final String JUNIT_DEBUG_PROJECT_TEST_CASE = "JUNIT_DEBUG_PROJECT_TEST_CASE";
