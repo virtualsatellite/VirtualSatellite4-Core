ed23c1c5fb03df29f84fed0bd2a956daac596dbb
SaMuellerDLR <48356419+SaMuellerDLR@users.noreply.github.com>
2021-03-05 11:27:53 +0100

    Merge pull request #947 from virtualsatellite/feature/task_942-Generic_verification_interface
    Create generic verification interface for requirements

---
+++ de.dlr.sc.virsat.model.extension.requirements.test/src/de/dlr/sc/virsat/model/extension/requirements/verification/build/steps/ModelVerificationImpl.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2019 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.sc.virsat.model.extension.requirements.verification.build.steps;
+
+import de.dlr.sc.virsat.model.extension.requirements.model.ModelVerification;
+
+/**
+ * Dummy bean for verification registration (ModelVerification is abstract but needs to be instantiated for the test)
+ *
+ */
+public class ModelVerificationImpl extends ModelVerification {
+
+}
