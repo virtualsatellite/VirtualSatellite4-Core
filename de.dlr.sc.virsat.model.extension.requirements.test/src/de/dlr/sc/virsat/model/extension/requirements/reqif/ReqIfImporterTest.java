9b0077faba17420c4bd2c1ee70381257f1f5649e
Tobias Franz <tobias.franz@dlr.de>
2021-03-16 18:46:06 +0100

    Register reqIF factory - (Task #807)

--- de.dlr.sc.virsat.model.extension.requirements.test/src/de/dlr/sc/virsat/model/extension/requirements/reqif/ReqIfImporterTest.java
+++ de.dlr.sc.virsat.model.extension.requirements.test/src/de/dlr/sc/virsat/model/extension/requirements/reqif/ReqIfImporterTest.java
@@ -32,6 +32,7 @@ import org.eclipse.rmf.reqif10.ReqIF;
 import org.eclipse.rmf.reqif10.ReqIF10Factory;
 import org.eclipse.rmf.reqif10.ReqIF10Package;
 import org.eclipse.rmf.reqif10.Specification;
+import org.eclipse.rmf.reqif10.impl.ReqIF10FactoryImpl;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -188,6 +189,7 @@ public class ReqIfImporterTest extends AConceptProjectTestCase {
 	@Test
 	public void testImportRequirements() {
 		URI modelURI = URI.createPlatformPluginURI(PLATFORM_REQ_IF_MODEL_PATH, true);
+		Resource.Factory.Registry.INSTANCE.getContentTypeToFactoryMap().put("reqif", new ReqIF10FactoryImpl());
 		rs.getPackageRegistry().put(ReqIF10Package.eNS_URI, ReqIF10Package.eINSTANCE);
 		Resource modelResource = rs.getResource(modelURI, true);
 		ReqIF reqIfFileContent = (ReqIF) modelResource.getContents().get(0);
