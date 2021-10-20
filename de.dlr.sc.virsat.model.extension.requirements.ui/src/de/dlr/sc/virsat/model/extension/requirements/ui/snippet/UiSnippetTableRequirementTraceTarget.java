dd2043386263fb1a03736e40ce90bbd9475c446a
franzTobiasDLR <tobias.franz@dlr.de>
2021-10-20 17:45:39 +0200

    UI imporvements

--- de.dlr.sc.virsat.model.extension.requirements.ui/src/de/dlr/sc/virsat/model/extension/requirements/ui/snippet/UiSnippetTableRequirementTraceTarget.java
+++ de.dlr.sc.virsat.model.extension.requirements.ui/src/de/dlr/sc/virsat/model/extension/requirements/ui/snippet/UiSnippetTableRequirementTraceTarget.java
@@ -9,11 +9,25 @@
  *******************************************************************************/
 package de.dlr.sc.virsat.model.extension.requirements.ui.snippet;
 
+import java.util.Collection;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.widgets.FormToolkit;
 
+import de.dlr.sc.virsat.model.dvlm.categories.ATypeInstance;
 import de.dlr.sc.virsat.model.dvlm.categories.propertydefinitions.AProperty;
 import de.dlr.sc.virsat.model.dvlm.categories.propertydefinitions.ReferenceProperty;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.PropertyinstancesPackage;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.ReferencePropertyInstance;
+import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
 import de.dlr.sc.virsat.model.extension.requirements.model.RequirementTrace;
 import de.dlr.sc.virsat.model.extension.requirements.ui.celleditor.RequirementTargetReferenceCellEditingSupport;
 import de.dlr.sc.virsat.uiengine.ui.editor.snippets.IUiSnippet;
@@ -29,11 +43,44 @@ import de.dlr.sc.virsat.uiengine.ui.editor.snippets.IUiSnippet;
  */
 public class UiSnippetTableRequirementTraceTarget extends AUiSnippetTableRequirementTraceTarget implements IUiSnippet {
 	
+	private static final String ADD_BUTTON_LABEL = "Add Target";
+	
 	@Override
 	protected EditingSupport createEditingSupport(EditingDomain editingDomain, AProperty property) {
 		if (property.getName().equals(RequirementTrace.PROPERTY_TARGET)) {
 			return new RequirementTargetReferenceCellEditingSupport(editingDomain, columnViewer, (ReferenceProperty) property);
 		}
 		return super.createEditingSupport(editingDomain, property);
+	}
+	
+	@Override
+	protected void createAddButton(FormToolkit toolkit, EditingDomain editingDomain, Composite compositeButtons) {
+		if ((style & STYLE_ADD_BUTTON) != 0) {
+			Button buttonAdd = toolkit.createButton(compositeButtons, ADD_BUTTON_LABEL, SWT.PUSH);
+			buttonAdd.addSelectionListener(new SelectionListener() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					Concept activeConcept = acHelper.getConcept(conceptId);
+					Command addCommand = createAddCommand(editingDomain, activeConcept);
+					
+					editingDomain.getCommandStack().execute(addCommand);
+					Collection<?> objectes = addCommand.getAffectedObjects();
+					ReferencePropertyInstance arrayElement = (ReferencePropertyInstance) objectes.iterator().next();
+					ReferenceProperty typeDef = (ReferenceProperty) arrayElement.getType();
+					
+					Object selection = new RequirementTargetReferenceCellEditingSupport(editingDomain, columnViewer, typeDef).startCellEditor(arrayElement, typeDef.getReferenceType(), adapterFactory);
+					if (selection instanceof ATypeInstance) {
+						Command cmd = SetCommand.create(editingDomain, arrayElement, PropertyinstancesPackage.Literals.REFERENCE_PROPERTY_INSTANCE__REFERENCE, selection);
+						editingDomain.getCommandStack().execute(cmd);
+					} 
+				}
+	
+				@Override
+				public void widgetDefaultSelected(SelectionEvent e) {
+					widgetSelected(e);
+				}
+			});
+			checkWriteAccess(buttonAdd);
+		}
 	}
 }
--- de.dlr.sc.virsat.model.extension.requirements.ui/src/de/dlr/sc/virsat/model/extension/requirements/ui/snippet/UiSnippetTableRequirementsViewReqTypesToView.java
+++ de.dlr.sc.virsat.model.extension.requirements.ui/src/de/dlr/sc/virsat/model/extension/requirements/ui/snippet/UiSnippetTableRequirementsViewReqTypesToView.java
@@ -9,6 +9,28 @@
  *******************************************************************************/
 package de.dlr.sc.virsat.model.extension.requirements.ui.snippet;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.edit.command.SetCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+
+import de.dlr.sc.virsat.model.dvlm.categories.ATypeInstance;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.PropertyinstancesPackage;
+import de.dlr.sc.virsat.model.dvlm.categories.propertyinstances.ReferencePropertyInstance;
+import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
+import de.dlr.sc.virsat.model.dvlm.concepts.util.ActiveConceptHelper;
+import de.dlr.sc.virsat.model.extension.requirements.model.RequirementType;
+import de.dlr.sc.virsat.uiengine.ui.dialog.ReferenceSelectionDialog;
 import de.dlr.sc.virsat.uiengine.ui.editor.snippets.IUiSnippet;
 
 
@@ -21,4 +43,74 @@ import de.dlr.sc.virsat.uiengine.ui.editor.snippets.IUiSnippet;
  * 
  */
 public class UiSnippetTableRequirementsViewReqTypesToView extends AUiSnippetTableRequirementsViewReqTypesToView implements IUiSnippet {
+	
+	protected Button buttonAdd;
+	private static final String SCETION_HEADING = "Filter requirements by slecting which types to show:";
+	private static final String ADD_BUTTON_LABEL = "Add RequirementType";
+	private static final String REMOVE_BUTTON_LABEL = "Remove RequirementType";
+	
+	@Override
+	public Composite createSectionBody(FormToolkit toolkit, String sectionHeading, String sectionDescription,
+			int numberColumns) {
+		Composite sectionBody = super.createSectionBody(toolkit, sectionHeading, sectionDescription, numberColumns);
+		getSection().setText(SCETION_HEADING);
+		return sectionBody;
+	}
+	
+	@Override
+	protected void createAddButton(FormToolkit toolkit, EditingDomain editingDomain, Composite compositeButtons) {
+		if ((style & STYLE_ADD_BUTTON) != 0) {
+			buttonAdd = toolkit.createButton(compositeButtons, ADD_BUTTON_LABEL, SWT.PUSH);
+			buttonAdd.addSelectionListener(new SelectionListener() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					Concept activeConcept = acHelper.getConcept(conceptId);
+					Command addCommand = createAddCommand(editingDomain, activeConcept);
+					editingDomain.getCommandStack().execute(addCommand);
+					Collection<?> objectes = addCommand.getAffectedObjects();
+					ReferencePropertyInstance arrayElement = (ReferencePropertyInstance) objectes.iterator().next();
+					ReferenceSelectionDialog dialog = ReferenceSelectionDialog.createRefernceSelectionDialog(Display.getCurrent().getActiveShell(), ActiveConceptHelper.getCategory(activeConcept, RequirementType.FULL_QUALIFIED_CATEGORY_NAME), adapterFactory);
+					dialog.setAllowMultiple(false);
+					dialog.setInput(arrayElement.eResource());
+					dialog.setDoubleClickSelects(true);
+					if (dialog.open() == Dialog.OK) {
+						Object selection = dialog.getFirstResult();
+						if (selection instanceof ATypeInstance) {
+							Command cmd = SetCommand.create(editingDomain, arrayElement, PropertyinstancesPackage.Literals.REFERENCE_PROPERTY_INSTANCE__REFERENCE, dialog.getFirstResult());
+							editingDomain.getCommandStack().execute(cmd);
+						}
+					} 
+				}
+	
+				@Override
+				public void widgetDefaultSelected(SelectionEvent e) {
+					widgetSelected(e);
+				}
+			});
+			checkWriteAccess(buttonAdd);
+		}
+	}
+	
+	@Override
+	protected void createRemoveButton(FormToolkit toolkit, EditingDomain editingDomain, Composite compositeButtons) {
+		if ((style & STYLE_REMOVE_BUTTON) != 0) {
+			Button buttonRemove = toolkit.createButton(compositeButtons, REMOVE_BUTTON_LABEL, SWT.PUSH);
+			buttonRemove.addSelectionListener(new SelectionListener() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					List<?> selection = columnViewer.getStructuredSelection().toList();
+					if (!selection.isEmpty()) {
+						Command cmd = createDeleteCommand(editingDomain, selection);
+						editingDomain.getCommandStack().execute(cmd);
+					}
+				}
+	
+				@Override
+				public void widgetDefaultSelected(SelectionEvent e) {
+					widgetSelected(e);
+				}
+			});
+			checkWriteAccess(buttonRemove);
+		}
+	}
 }
