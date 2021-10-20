4bbbd0fffd95d57cf2b3516d5951af82f635a03e
Anna-Lena-DLR <anna-lena.gefke@dlr.de>
2021-10-20 17:07:43 +0200

    Code clean up and added new methods for querying requirements

--- de.dlr.sc.virsat.model.extension.requirements/src/de/dlr/sc/virsat/model/extension/requirements/doors/client/DoorsSynchroClient.java
+++ de.dlr.sc.virsat.model.extension.requirements/src/de/dlr/sc/virsat/model/extension/requirements/doors/client/DoorsSynchroClient.java
@@ -11,12 +11,14 @@
 package de.dlr.sc.virsat.model.extension.requirements.doors.client;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-
 import javax.ws.rs.core.Response;
 
 import org.eclipse.lyo.client.OSLCConstants;
@@ -34,47 +36,106 @@ import org.eclipse.lyo.oslc4j.core.model.ResourceShape;
 import org.eclipse.lyo.oslc4j.core.model.Service;
 import org.eclipse.lyo.oslc4j.core.model.ServiceProvider;
 
+import de.dlr.sc.virsat.model.extension.requirements.Activator;
+
 public class DoorsSynchroClient {
 
-	private DoorsSynchroClient() {
-
-	}
-
-//	private static final String SERVER_NAME = "https://gk-sl0002.intra.dlr.de:9443/rm/";
 	private static final String CATALOG_URL = "https://gk-sl0002.intra.dlr.de:9443/rm/oslc_rm/catalog";
-//	private static final String PROJECT_NAME = "MBSE4GK Testprojekt";
-	private static final int SUCCESCFULL_HTTP_STATUS = 200;
+	private static final int SUCCESSFULL_HTTP_STATUS = 200;
+
+	private DoorsUtils doorsUtils;
 
 	private static ArrayList<RequirementCollection> reqSpecificationsList = new ArrayList<RequirementCollection>();
 	private static HashMap<RequirementCollection, ArrayList<Requirement>> mapOfRequirements = new HashMap<RequirementCollection, ArrayList<Requirement>>();
+	private static OslcClient client;
+	private static String serviceProviderUrl;
 
-	public static String lookUpServiceProviderUrl(OslcClient client, String projectName)
+	/**
+	 * Init the doors synchro client by getting an Oslc Client and look up provided
+	 * services
+	 * 
+	 * @param server   server url
+	 * @param user     user name
+	 * @param password password
+	 * @param project  project name
+	 * @throws UnrecoverableKeyException
+	 * @throws MalformedURLException
+	 * @throws CertificateException
+	 * @throws IOException
+	 * @throws ResourceNotFoundException
+	 * @throws URISyntaxException
+	 */
+	public void init(String server, String user, String password, String project) throws UnrecoverableKeyException,
+			MalformedURLException, CertificateException, IOException, ResourceNotFoundException, URISyntaxException {
+		doorsUtils = new DoorsUtils(server, user, password);
+		client = doorsUtils.getClient();
+		serviceProviderUrl = lookUpServiceProviderUrl(project);
+	}
+
+	/**
+	 * Look up service provider url to access doors projects in further requests
+	 * 
+	 * @param project project name
+	 * @return serviceProviderUrl
+	 * @throws ResourceNotFoundException
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	public static String lookUpServiceProviderUrl(String project)
 			throws ResourceNotFoundException, IOException, URISyntaxException {
-		String serviceProviderUrl = client.lookupServiceProviderUrl(CATALOG_URL, projectName);
+		String serviceProviderUrl = client.lookupServiceProviderUrl(CATALOG_URL, project);
 		return serviceProviderUrl;
 	}
 
-	public static String lookUpQueryCapability(OslcClient client, String serviceProvider)
-			throws ResourceNotFoundException, IOException, URISyntaxException {
-		String queryCapabilty = client.lookupQueryCapability(serviceProvider, OSLCConstants.OSLC_RM_V2,
+	/**
+	 * Look up query capability url for further requests
+	 * 
+	 * @return queryCapability
+	 * @throws ResourceNotFoundException
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	public static String lookUpQueryCapability() throws ResourceNotFoundException, IOException, URISyntaxException {
+		String queryCapabilty = client.lookupQueryCapability(serviceProviderUrl, OSLCConstants.OSLC_RM_V2,
 				OSLCConstants.RM_REQUIREMENT_TYPE);
 		return queryCapabilty;
 	}
 
 	/**
+	 * Query requirement by id
 	 * 
-	 * @param client
-	 * @param serviceProviderUrl
-	 * @return
-	 * @throws URISyntaxException
-	 * @throws IOException
+	 * @param id id of a requirement
+	 * @return requirement
 	 * @throws ResourceNotFoundException
+	 * @throws IOException
+	 * @throws URISyntaxException
 	 */
-	public static ServiceProvider getServiceProvider(OslcClient client, String serverName)
+	public static Requirement getRequirementById(String id)
 			throws ResourceNotFoundException, IOException, URISyntaxException {
-		String serviceProviderUrl = lookUpServiceProviderUrl(client, serverName);
+		String queryCapability = lookUpQueryCapability();
+		OslcQueryParameters queryParams = new OslcQueryParameters();
+		queryParams.setSelect("*");
+		queryParams.setPrefix(
+				"rdf=<http://www.w3.org/1999/02/22-rdf-syntax-ns%23>,dcterms=<http://purl.org/dc/terms/>,oslc_rm=<http://open-services.net/ns/rm%23");
+		queryParams.setWhere("dcterms:identifier=" + id);
+		OslcQuery queryReqCollections = new OslcQuery(client, queryCapability, queryParams);
+		OslcQueryResult result = queryReqCollections.submit();
+		Requirement req = result.getMembers(Requirement.class).iterator().next();
+		return req;
+	}
+
+	/**
+	 * Query all services (projects)
+	 * 
+	 * @return serviceProvider or null if not existing
+	 * @throws ResourceNotFoundException
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	public static ServiceProvider getServiceProvider()
+			throws ResourceNotFoundException, IOException, URISyntaxException {
 		Response responseServer = client.getResource(serviceProviderUrl, OslcMediaType.APPLICATION_RDF_XML);
-		if (responseServer.getStatus() == SUCCESCFULL_HTTP_STATUS) {
+		if (responseServer.getStatus() == SUCCESSFULL_HTTP_STATUS) {
 			ServiceProvider serviceProvider = responseServer.readEntity(ServiceProvider.class);
 			return serviceProvider;
 		} else {
@@ -83,12 +144,16 @@ public class DoorsSynchroClient {
 	}
 
 	/**
+	 * Query to get all possible resource shapes
 	 * 
-	 * @param serviceProvider
-	 * @param client
-	 * @return
+	 * @return resourceShapes or null if not existing
+	 * @throws ResourceNotFoundException
+	 * @throws IOException
+	 * @throws URISyntaxException
 	 */
-	public static ArrayList<ResourceShape> getResourceShapes(ServiceProvider serviceProvider, OslcClient client) {
+	public static ArrayList<ResourceShape> getResourceShapes()
+			throws ResourceNotFoundException, IOException, URISyntaxException {
+		ServiceProvider serviceProvider = getServiceProvider();
 		for (Service service : serviceProvider.getServices()) {
 			URI domain = service.getDomain();
 			String oslcDomain = OSLCConstants.OSLC_RM_V2;
@@ -120,20 +185,33 @@ public class DoorsSynchroClient {
 	}
 
 	/**
+	 * Get resource shape from given requirement
 	 * 
-	 * @param client
-	 * @param queryCapability
-	 * @return
-	 * @return
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 * @throws ResourceNotFoundException
+	 * @param req given requirement
+	 * @return resourceShape or null
 	 */
-	public static ArrayList<RequirementCollection> queryRequirementsSpecifications(OslcClient client,
-			String projectName) throws IOException, ResourceNotFoundException, URISyntaxException {
-		String serviceProvider = lookUpServiceProviderUrl(client, projectName);
-		String queryCapability = lookUpQueryCapability(client, serviceProvider);
+	public static ResourceShape getResourceShape(Requirement req) {
+		URI reqLink = req.getInstanceShape().iterator().next().getValue();
+		Response response = client.getResource(reqLink.toString(), OSLCConstants.CT_RDF);
+		ResourceShape resourceShape = response.readEntity(ResourceShape.class);
+		if (resourceShape != null) {
+			return resourceShape;
+		}
+		return null;
+	}
 
+	/**
+	 * Query all requirement collections (specifications) of a given project
+	 * 
+	 * @param projectName Doors project
+	 * @return reqSpecifications
+	 * @throws IOException
+	 * @throws ResourceNotFoundException
+	 * @throws URISyntaxException
+	 */
+	public static ArrayList<RequirementCollection> queryRequirementsSpecifications(String projectName)
+			throws IOException, ResourceNotFoundException, URISyntaxException {
+		String queryCapability = lookUpQueryCapability();
 		// query requirementCollections which are the specifications in virsat
 		OslcQueryParameters queryParams = new OslcQueryParameters();
 		queryParams.setSelect("*");
@@ -154,38 +232,71 @@ public class DoorsSynchroClient {
 	}
 
 	/**
+	 * Query all requirements of a given project
 	 * 
-	 * @param client
 	 * @param projectName
 	 * @throws ResourceNotFoundException
 	 * @throws IOException
 	 * @throws URISyntaxException
 	 */
-	public static void queryRequirements(OslcClient client, String projectName)
+	public static void queryRequirements(String projectName)
 			throws ResourceNotFoundException, IOException, URISyntaxException {
 		for (RequirementCollection reqCollection : reqSpecificationsList) {
-
-			// do another get on the collection to have access to getUses(), which are the
-			// underlying requirements
-			Response requirementGet = client.getResource(reqCollection.getAbout().toURL().toString(),
+			Response response = client.getResource(reqCollection.getAbout().toURL().toString(),
 					OslcMediaType.APPLICATION_RDF_XML);
-			RequirementCollection requirementCollection = requirementGet.readEntity(RequirementCollection.class);
+			RequirementCollection requirementCollection = response.readEntity(RequirementCollection.class);
 			ArrayList<Requirement> listOfRequirements = new ArrayList<Requirement>();
 			try {
 				if (requirementCollection != null) {
 					if (requirementCollection.getUses() != null) {
+						// every link (getUses()) is a requirement contained by the
+						// requirementCollection
 						for (Link uses : requirementCollection.getUses()) {
-							Response reqResource = client.getResource(uses.getValue().toString(),
+							Response reqRequest = client.getResource(uses.getValue().toString(),
 									OslcMediaType.APPLICATION_RDF_XML);
-							Requirement req = reqResource.readEntity(Requirement.class);
-							listOfRequirements.add(req);
+							Requirement requirement = reqRequest.readEntity(Requirement.class);
+							listOfRequirements.add(requirement);
 						}
 						mapOfRequirements.put(requirementCollection, listOfRequirements);
 					}
 				}
 			} catch (NullPointerException np) {
-				np.getStackTrace();
+				Activator.getDefault().getLog().error(np.getMessage());
 			}
 		}
+	}
+
+	/**
+	 * Query all requirements of a given Requirement Collection
+	 * 
+	 * @param reqCollection the given Requirement Collection
+	 * @return list of requirements of given RequirementCollection
+	 * @throws ResourceNotFoundException
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	public static ArrayList<Requirement> queryRequirementsOfCollection(RequirementCollection reqCollection)
+			throws ResourceNotFoundException, IOException, URISyntaxException {
+		Response response = client.getResource(reqCollection.getAbout().toURL().toString(),
+				OslcMediaType.APPLICATION_RDF_XML);
+		RequirementCollection requirementCollection = response.readEntity(RequirementCollection.class);
+		ArrayList<Requirement> listOfRequirements = new ArrayList<Requirement>();
+		try {
+			if (requirementCollection != null) {
+				if (requirementCollection.getUses() != null) {
+					// every link (getUses()) is a requirement contained by the
+					// requirementCollection
+					for (Link uses : requirementCollection.getUses()) {
+						Response reqRequest = client.getResource(uses.getValue().toString(),
+								OslcMediaType.APPLICATION_RDF_XML);
+						Requirement requirement = reqRequest.readEntity(Requirement.class);
+						listOfRequirements.add(requirement);
+					}
+				}
+			}
+		} catch (NullPointerException np) {
+			Activator.getDefault().getLog().error(np.getMessage());
+		}
+		return listOfRequirements;
 	}
 }
