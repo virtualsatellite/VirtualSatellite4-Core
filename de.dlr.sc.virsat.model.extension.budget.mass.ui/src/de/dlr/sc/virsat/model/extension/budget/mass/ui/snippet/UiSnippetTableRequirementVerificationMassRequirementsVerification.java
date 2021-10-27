fb80c85a1958bf163959a79113edcc835c040e8e
franzTobiasDLR <tobias.franz@dlr.de>
2021-10-27 12:58:58 +0200

    Fix active status of snippet

--- de.dlr.sc.virsat.model.extension.budget.mass.ui/src/de/dlr/sc/virsat/model/extension/budget/mass/ui/snippet/UiSnippetTableRequirementVerificationMassRequirementsVerification.java
+++ de.dlr.sc.virsat.model.extension.budget.mass.ui/src/de/dlr/sc/virsat/model/extension/budget/mass/ui/snippet/UiSnippetTableRequirementVerificationMassRequirementsVerification.java
@@ -10,10 +10,12 @@
 package de.dlr.sc.virsat.model.extension.budget.mass.ui.snippet;
 
 import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.domain.EditingDomain;
 
 import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
 import de.dlr.sc.virsat.model.dvlm.concepts.util.ActiveConceptHelper;
+import de.dlr.sc.virsat.model.extension.budget.mass.Activator;
 import de.dlr.sc.virsat.model.extension.requirements.ui.command.CreateAddArrayElementVerificationCommand;
 import de.dlr.sc.virsat.uiengine.ui.editor.snippets.AUiSnippetArrayInstanceCategoryTable;
 import de.dlr.sc.virsat.uiengine.ui.editor.snippets.IUiSnippet;
@@ -24,7 +26,7 @@ public class UiSnippetTableRequirementVerificationMassRequirementsVerification e
 	 * Constructor
 	 */
 	public UiSnippetTableRequirementVerificationMassRequirementsVerification() {
-		super("de.dlr.sc.virsat.model.extension.requirements",
+		super("de.dlr.sc.virsat.model.extension.budget.mass",
 			"MassRequirementsVerification",
 			"verification",
 			"Requirement",
@@ -37,5 +39,13 @@ public class UiSnippetTableRequirementVerificationMassRequirementsVerification e
 		return new CreateAddArrayElementVerificationCommand().create(editingDomain, getArrayInstance(model),  ActiveConceptHelper.getCategory(activeConcept, "MassRequirementsVerification"));
 	}
 	
+	@Override
+	public boolean isActive(EObject model) {
+		initializeHelperForModel(model);
+		if (model == null || acHelper.getConcept(Activator.getPluginId()) == null) {
+			return false; // Concept not active
+		}
+		return super.isActive(model);
+	}
 	
 }
