29cb66301f293d380707da5b71a2e2715310eaf0
franzTobiasDLR <tobias.franz@dlr.de>
2021-12-06 22:09:40 +0100

    Add tests for property-based verifications - (Task #1036)

--- de.dlr.sc.virsat.model.extension.requirements.test/src/de/dlr/sc/virsat/model/extension/requirements/model/BoundedValueVerificationTest.java
+++ de.dlr.sc.virsat.model.extension.requirements.test/src/de/dlr/sc/virsat/model/extension/requirements/model/BoundedValueVerificationTest.java
@@ -9,6 +9,19 @@
  *******************************************************************************/
 package de.dlr.sc.virsat.model.extension.requirements.model;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.dlr.sc.virsat.concept.unittest.util.test.AConceptProjectTestCase;
+import de.dlr.sc.virsat.model.dvlm.categories.ATypeDefinition;
+import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
+
 // *****************************************************************
 // * Import Statements
 // *****************************************************************
@@ -27,6 +40,163 @@ package de.dlr.sc.virsat.model.extension.requirements.model;
  * 
  * 
  */
-public class BoundedValueVerificationTest extends ABoundedValueVerificationTest {
-
+public class BoundedValueVerificationTest extends AConceptProjectTestCase {
+	
+	private static final String REQ_CONCEPT_ID = "de.dlr.sc.virsat.model.extension.requirements";
+	private Concept conceptRequirements;
+	
+	private static final double UPPER_LIMIT = 100;
+	private static final double LOWER_LIMIT = 10;
+	private static final double VALUE_COMPLIANT = 50;
+	private static final double VALUE_TOO_HIGH = 150;
+	private static final double VALUE_TOO_LOW = 5;
+	
+	@Before
+	public void setUp() throws CoreException  {
+		super.setUp();
+		addResourceSetAndRepository();
+		loadConceptAndInstallToRepository(CONCEPT_ID_CORE, repository);
+		conceptRequirements = loadConceptAndInstallToRepository(REQ_CONCEPT_ID, repository);
+	}
+	
+	public class TestBoundedValueRequirementsVerification extends BoundedValueVerification {
+		boolean statusCompliant = false;
+		boolean statusOpen = false;
+		boolean statusNonCompliant = false;
+		boolean statusPartlyCompliant = false;
+		
+		public TestBoundedValueRequirementsVerification() {
+			super(conceptRequirements);
+		}
+		
+		@Override
+		protected Command setStatusCompliant(EditingDomain editingDomain) {
+			statusCompliant = true;
+			return null;
+		}
+		
+		@Override
+		protected Command setStatusOpen(EditingDomain editingDomain) {
+			statusOpen = true;
+			return null;
+		}
+		
+		@Override
+		protected Command setStatusNonCompliant(EditingDomain editingDomain) {
+			statusNonCompliant = true;
+			return null;
+		}
+		
+		@Override
+		protected Command setStatusPartlyCompliant(EditingDomain editingDomain) {
+			statusPartlyCompliant = true;
+			return null;
+		}
+	}
+	
+	@Test
+	public void testRunCustomVerificationNoTrace() {
+		TestBoundedValueRequirementsVerification testObject = new TestBoundedValueRequirementsVerification();
+		
+		Requirement requirementObject = new Requirement(conceptRequirements);
+		
+		assertFalse("No status should be set", testObject.statusCompliant);
+		assertFalse("No status should be set", testObject.statusOpen);
+		assertFalse("No status should be set", testObject.statusNonCompliant);
+		assertFalse("No status should be set", testObject.statusPartlyCompliant);
+		
+		testObject.runCustomVerification(null, requirementObject, null);
+		
+		assertTrue("As no trace was set, status should be open", testObject.statusOpen);
+		
+		assertFalse(testObject.statusCompliant);
+		assertFalse(testObject.statusNonCompliant);
+		assertFalse(testObject.statusPartlyCompliant);
+	}
+	
+	@Test
+	public void testRunCustomVerificationCompliant() {
+		Requirement requirementObject = new Requirement(conceptRequirements);
+		
+		// The upper limit verification element is used as random element with a value property for testing 
+		// (-> not actually used as verification here)
+		UpperLimitVerification someValueElement = new UpperLimitVerification(conceptRequirements);
+		someValueElement.setUpperBound(VALUE_COMPLIANT);
+		requirementObject.getTrace().getTarget().add(someValueElement);
+		
+		// Set up the verification element
+		TestBoundedValueRequirementsVerification testVerification = new TestBoundedValueRequirementsVerification();
+		testVerification.setUpperBound(UPPER_LIMIT);
+		testVerification.setLowerBound(LOWER_LIMIT);
+		testVerification.setStatus(IVerification.STATUS_Open_NAME);
+		ATypeDefinition propertyToBeVerified = someValueElement.getUpperBoundBean().getATypeInstance().getType();
+		testVerification.setElementToBeVerified(propertyToBeVerified);
+		
+		// Run the verification
+		testVerification.runCustomVerification(null, requirementObject, null);
+		
+		// Check results
+		assertTrue("As value was in range, status should be compliant", testVerification.statusCompliant);
+		assertFalse(testVerification.statusOpen);
+		assertFalse(testVerification.statusNonCompliant);
+		assertFalse(testVerification.statusPartlyCompliant);
+	}
+	
+	@Test
+	public void testRunCustomVerificationTooLow() {
+		Requirement requirementObject = new Requirement(conceptRequirements);
+		
+		// The upper limit verification element is used as random element with a value property for testing 
+		// (-> not actually used as verification here)
+		UpperLimitVerification someValueElement = new UpperLimitVerification(conceptRequirements);
+		someValueElement.setUpperBound(VALUE_TOO_LOW);
+		requirementObject.getTrace().getTarget().add(someValueElement);
+		
+		// Set up the verification element
+		TestBoundedValueRequirementsVerification testVerification = new TestBoundedValueRequirementsVerification();
+		testVerification.setUpperBound(UPPER_LIMIT);
+		testVerification.setLowerBound(LOWER_LIMIT);
+		testVerification.setStatus(IVerification.STATUS_Open_NAME);
+		ATypeDefinition propertyToBeVerified = someValueElement.getUpperBoundBean().getATypeInstance().getType();
+		testVerification.setElementToBeVerified(propertyToBeVerified);
+		
+		// Run the verification
+		testVerification.runCustomVerification(null, requirementObject, null);
+		
+		// Check results
+		assertTrue("As value was not in range, status should non-compliant", testVerification.statusNonCompliant);
+		assertFalse(testVerification.statusOpen);
+		assertFalse(testVerification.statusCompliant);
+		assertFalse(testVerification.statusPartlyCompliant);
+	}
+	
+	
+	@Test
+	public void testRunCustomVerificationTooHigh() {
+		Requirement requirementObject = new Requirement(conceptRequirements);
+		
+		// The upper limit verification element is used as random element with a value property for testing 
+		// (-> not actually used as verification here)
+		UpperLimitVerification someValueElement = new UpperLimitVerification(conceptRequirements);
+		someValueElement.setUpperBound(VALUE_TOO_HIGH);
+		requirementObject.getTrace().getTarget().add(someValueElement);
+		
+		// Set up the verification element
+		TestBoundedValueRequirementsVerification testVerification = new TestBoundedValueRequirementsVerification();
+		testVerification.setUpperBound(UPPER_LIMIT);
+		testVerification.setLowerBound(LOWER_LIMIT);
+		testVerification.setStatus(IVerification.STATUS_Open_NAME);
+		ATypeDefinition propertyToBeVerified = someValueElement.getUpperBoundBean().getATypeInstance().getType();
+		testVerification.setElementToBeVerified(propertyToBeVerified);
+		
+		// Run the verification
+		testVerification.runCustomVerification(null, requirementObject, null);
+		
+		// Check results
+		assertTrue("As value was not in range, status should non-compliant", testVerification.statusNonCompliant);
+		assertFalse(testVerification.statusOpen);
+		assertFalse(testVerification.statusCompliant);
+		assertFalse(testVerification.statusPartlyCompliant);
+	}
+	
 }
--- de.dlr.sc.virsat.model.extension.requirements.test/src/de/dlr/sc/virsat/model/extension/requirements/model/UpperLimitVerificationTest.java
+++ de.dlr.sc.virsat.model.extension.requirements.test/src/de/dlr/sc/virsat/model/extension/requirements/model/UpperLimitVerificationTest.java
@@ -9,6 +9,19 @@
  *******************************************************************************/
 package de.dlr.sc.virsat.model.extension.requirements.model;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.dlr.sc.virsat.concept.unittest.util.test.AConceptProjectTestCase;
+import de.dlr.sc.virsat.model.dvlm.categories.ATypeDefinition;
+import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
+
 // *****************************************************************
 // * Import Statements
 // *****************************************************************
@@ -27,6 +40,130 @@ package de.dlr.sc.virsat.model.extension.requirements.model;
  * 
  * 
  */
-public class UpperLimitVerificationTest extends AUpperLimitVerificationTest {
-
+public class UpperLimitVerificationTest extends AConceptProjectTestCase {
+	
+	private static final String REQ_CONCEPT_ID = "de.dlr.sc.virsat.model.extension.requirements";
+	private Concept conceptRequirements;
+	
+	private static final double UPPER_LIMIT = 100;
+	private static final double VALUE_COMPLIANT = 50;
+	private static final double VALUE_TOO_HIGH = 150;
+	
+	@Before
+	public void setUp() throws CoreException  {
+		super.setUp();
+		addResourceSetAndRepository();
+		loadConceptAndInstallToRepository(CONCEPT_ID_CORE, repository);
+		conceptRequirements = loadConceptAndInstallToRepository(REQ_CONCEPT_ID, repository);
+	}
+	
+	public class TestUpperLimitRequirementsVerification extends UpperLimitVerification {
+		boolean statusCompliant = false;
+		boolean statusOpen = false;
+		boolean statusNonCompliant = false;
+		boolean statusPartlyCompliant = false;
+		
+		public TestUpperLimitRequirementsVerification() {
+			super(conceptRequirements);
+		}
+		
+		@Override
+		protected Command setStatusCompliant(EditingDomain editingDomain) {
+			statusCompliant = true;
+			return null;
+		}
+		
+		@Override
+		protected Command setStatusOpen(EditingDomain editingDomain) {
+			statusOpen = true;
+			return null;
+		}
+		
+		@Override
+		protected Command setStatusNonCompliant(EditingDomain editingDomain) {
+			statusNonCompliant = true;
+			return null;
+		}
+		
+		@Override
+		protected Command setStatusPartlyCompliant(EditingDomain editingDomain) {
+			statusPartlyCompliant = true;
+			return null;
+		}
+	}
+	
+	@Test
+	public void testRunCustomVerificationNoTrace() {
+		TestUpperLimitRequirementsVerification testObject = new TestUpperLimitRequirementsVerification();
+		
+		Requirement requirementObject = new Requirement(conceptRequirements);
+		
+		assertFalse("No status should be set", testObject.statusCompliant);
+		assertFalse("No status should be set", testObject.statusOpen);
+		assertFalse("No status should be set", testObject.statusNonCompliant);
+		assertFalse("No status should be set", testObject.statusPartlyCompliant);
+		
+		testObject.runCustomVerification(null, requirementObject, null);
+		
+		assertTrue("As no trace was set, status should be open", testObject.statusOpen);
+		
+		assertFalse(testObject.statusCompliant);
+		assertFalse(testObject.statusNonCompliant);
+		assertFalse(testObject.statusPartlyCompliant);
+	}
+	
+	@Test
+	public void testRunCustomVerificationCompliant() {
+		Requirement requirementObject = new Requirement(conceptRequirements);
+		
+		// The upper limit verification element is used as random element with a value property for testing 
+		// (-> not actually used as verification here)
+		UpperLimitVerification someValueElement = new UpperLimitVerification(conceptRequirements);
+		someValueElement.setUpperBound(VALUE_COMPLIANT);
+		requirementObject.getTrace().getTarget().add(someValueElement);
+		
+		// Set up the verification element
+		TestUpperLimitRequirementsVerification testVerification = new TestUpperLimitRequirementsVerification();
+		testVerification.setUpperBound(UPPER_LIMIT);
+		testVerification.setStatus(IVerification.STATUS_Open_NAME);
+		ATypeDefinition propertyToBeVerified = someValueElement.getUpperBoundBean().getATypeInstance().getType();
+		testVerification.setElementToBeVerified(propertyToBeVerified);
+		
+		// Run the verification
+		testVerification.runCustomVerification(null, requirementObject, null);
+		
+		// Check results
+		assertTrue("As value was in range, status should be compliant", testVerification.statusCompliant);
+		assertFalse(testVerification.statusOpen);
+		assertFalse(testVerification.statusNonCompliant);
+		assertFalse(testVerification.statusPartlyCompliant);
+	}
+	
+	
+	@Test
+	public void testRunCustomVerificationTooHigh() {
+		Requirement requirementObject = new Requirement(conceptRequirements);
+		
+		// The upper limit verification element is used as random element with a value property for testing 
+		// (-> not actually used as verification here)
+		LowerLimitVerification someValueElement = new LowerLimitVerification(conceptRequirements);
+		someValueElement.setLowerBound(VALUE_TOO_HIGH);
+		requirementObject.getTrace().getTarget().add(someValueElement);
+		
+		// Set up the verification element
+		TestUpperLimitRequirementsVerification testVerification = new TestUpperLimitRequirementsVerification();
+		testVerification.setUpperBound(UPPER_LIMIT);
+		testVerification.setStatus(IVerification.STATUS_Open_NAME);
+		ATypeDefinition propertyToBeVerified = someValueElement.getLowerBoundBean().getATypeInstance().getType();
+		testVerification.setElementToBeVerified(propertyToBeVerified);
+		
+		// Run the verification
+		testVerification.runCustomVerification(null, requirementObject, null);
+		
+		// Check results
+		assertTrue("As value was not in range, status should non-compliant", testVerification.statusNonCompliant);
+		assertFalse(testVerification.statusOpen);
+		assertFalse(testVerification.statusCompliant);
+		assertFalse(testVerification.statusPartlyCompliant);
+	}
 }
