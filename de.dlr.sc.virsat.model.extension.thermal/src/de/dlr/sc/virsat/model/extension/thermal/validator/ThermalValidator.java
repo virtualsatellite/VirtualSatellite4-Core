6dcb5c0852c6ccd0405955e4a6cbac8d76fc4191
maur_pa <maur_pa@SC-010203.intra.dlr.de>
2022-02-14 14:46:37 +0100

    Added checkstyle to thermal.u, thermal and thermal.test

--- de.dlr.sc.virsat.model.extension.thermal/src/de/dlr/sc/virsat/model/extension/thermal/validator/ThermalValidator.java
+++ de.dlr.sc.virsat.model.extension.thermal/src/de/dlr/sc/virsat/model/extension/thermal/validator/ThermalValidator.java
@@ -1,11 +1,12 @@
-/**
- * This file is part of the VirSat project.
+/*******************************************************************************
+ * Copyright (c) 2008-2019 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
  *
- * Copyright (c) 2008-2016
- * German Aerospace Center (DLR), Simulation and Software Technology, Germany
- * All rights reserved
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
  *
- */
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
 package de.dlr.sc.virsat.model.extension.thermal.validator;
 
 import de.dlr.sc.virsat.model.dvlm.structural.StructuralElementInstance;
