643ea2a305041de38eef0b14187706fd149da3cc
SaMuellerDLR <sa.mueller@dlr.de>
2022-02-16 11:32:13 +0100

    Integrated app for writing main.inp file - Task #1054

---
+++ de.dlr.sc.virsat.model.extension.thermal.test/src/de/dlr/sc/virsat/model/extension/thermal/cad/CadExporterThermalTest.java
@@ -0,0 +1,103 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2022 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.sc.virsat.model.extension.thermal.cad;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.eclipse.core.runtime.CoreException;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.dlr.sc.virsat.commons.file.VirSatFileUtils;
+import de.dlr.sc.virsat.concept.unittest.util.test.AConceptProjectTestCase;
+import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
+import de.dlr.sc.virsat.model.dvlm.types.impl.VirSatUuid;
+import de.dlr.sc.virsat.model.extension.ps.model.ConfigurationTree;
+import de.dlr.sc.virsat.model.extension.ps.model.ElementConfiguration;
+import de.dlr.sc.virsat.model.extension.thermal.model.AnalysisType;
+import de.dlr.sc.virsat.model.extension.thermal.model.ThermalAnalysis;
+import de.dlr.sc.virsat.model.extension.thermal.model.ThermalData;
+import de.dlr.sc.virsat.model.extension.thermal.test.TestActivator;
+import de.dlr.sc.virsat.model.extension.visualisation.model.Visualisation;
+
+public class CadExporterThermalTest extends AConceptProjectTestCase {
+
+	private Concept conceptPS;
+	private Concept conceptVis;
+	private Concept conceptThermal;
+	private ElementConfiguration ecThermal;
+	private ThermalData thermalData;
+	private ThermalAnalysis thermalAnalysis;
+	
+	@Before
+	public void setUp() throws CoreException {
+		super.setUp();
+
+		conceptPS = loadConceptFromPlugin(de.dlr.sc.virsat.model.extension.ps.Activator.getPluginId());
+		conceptVis = loadConceptFromPlugin(de.dlr.sc.virsat.model.extension.visualisation.Activator.getPluginId());
+		conceptThermal = loadConceptFromPlugin(de.dlr.sc.virsat.model.extension.thermal.Activator.getPluginId());
+	
+		ConfigurationTree ct = new ConfigurationTree(conceptPS);
+		ct.setName("ConfigurationTree");
+
+		ElementConfiguration ecComponent = new ElementConfiguration(conceptPS);
+		ecComponent.setName("Component");
+		ecComponent.getStructuralElementInstance().setUuid(new VirSatUuid("c7430cd8-5bd2-49b6-bf7d-529cc8627b70"));
+		ct.add(ecComponent);
+		
+		Visualisation visualisation = new Visualisation(conceptVis);
+		ecComponent.add(visualisation);
+		
+		thermalData = new ThermalData(conceptThermal);
+		ecComponent.add(thermalData);
+		
+		ecThermal = new ElementConfiguration(conceptPS);
+		ecThermal.setName("Thermal");
+		ct.add(ecThermal);
+
+		thermalAnalysis = new ThermalAnalysis(conceptThermal);
+		ecThermal.add(thermalAnalysis);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testWriteCadMainInputNoAnalysisType() throws IOException {
+		Path outputPath = VirSatFileUtils.createAutoDeleteTempDirectory("cadTest");
+		String filePath = outputPath.toString() + File.separator + "main.inp";
+		
+		CadExporterThermal cadExporter = new CadExporterThermal();
+		cadExporter.writeCadMainInput(thermalAnalysis, filePath);
+	}
+	
+	@Test
+	public void testWriteCadMainInputStatic() throws IOException, CoreException {
+		thermalAnalysis.getAnalysisType().setAnalysisType(AnalysisType.ANALYSISTYPE_Static_NAME);
+		
+		Path outputPath = VirSatFileUtils.createAutoDeleteTempDirectory("cadTest");
+		String filePath = outputPath.toString() + File.separator + "main.inp";
+		File expectedMainFile = new File(filePath);
+
+		assertFalse("Main input file is not there initially", expectedMainFile.exists());
+
+		CadExporterThermal cadExporter = new CadExporterThermal();
+		cadExporter.writeCadMainInput(thermalAnalysis, outputPath.toString());
+
+		assertTrue("Main input file is created", expectedMainFile.exists());
+		assertEquals("Main input file is correct", Files.readAllLines(Paths.get(filePath)),
+				TestActivator.getResourceContentAsString("/resources/main.inp"));
+	}
+}
---
+++ de.dlr.sc.virsat.model.extension.thermal/src/de/dlr/sc/virsat/model/extension/thermal/cad/CadExporterThermal.java
@@ -0,0 +1,162 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2022 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.sc.virsat.model.extension.thermal.cad;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
+import de.dlr.sc.virsat.model.concept.types.structural.BeanStructuralElementInstance;
+import de.dlr.sc.virsat.model.concept.types.structural.IBeanStructuralElementInstance;
+import de.dlr.sc.virsat.model.dvlm.structural.StructuralElementInstance;
+import de.dlr.sc.virsat.model.extension.thermal.model.AnalysisType;
+import de.dlr.sc.virsat.model.extension.thermal.model.ThermalAnalysis;
+import de.dlr.sc.virsat.model.extension.thermal.model.ThermalData;
+import de.dlr.sc.virsat.model.extension.visualisation.model.Visualisation;
+
+/**
+ * 
+ * Exports thermal data for CAD programs
+ *
+ */
+public class CadExporterThermal {
+	
+	/**
+	 * Converts the virsat analysis type to the analysis type for the CAD program
+	 * 
+	 * @param analysisType the virsat analysis type
+	 * @return a string analysis type for the CAD program
+	 */
+	private String getAnalysisTypeCad(AnalysisType analysisType) {
+		String analysisTypeString = analysisType.getAnalysisType();
+		if (analysisTypeString == null) {
+			throw new RuntimeException("Analysis type is not specified! Please specify analysis type!");
+		}
+		
+		if (analysisTypeString.equals(AnalysisType.ANALYSISTYPE_Static_NAME)) {
+			return "STEADY STATE";
+		} else {
+			return "";
+		}
+	}
+	
+	/**
+	 * Creates the main input file for the CAD program
+	 * @param thermalAnalysis the thermal analysis
+	 * @param path the path where to create the main file
+	 * @throws IOException 
+	 */
+	public void writeCadMainInput(ThermalAnalysis thermalAnalysis, String path) throws IOException {
+		// Get the root element and all elements in the tree
+		StructuralElementInstance rootSei = (StructuralElementInstance) EcoreUtil.getRootContainer(thermalAnalysis.getTypeInstance());
+		IBeanStructuralElementInstance rootBean = new BeanStructuralElementInstance(rootSei);
+		List<IBeanStructuralElementInstance> ecs = rootBean.getDeepChildren(IBeanStructuralElementInstance.class);
+		
+		AnalysisType analysisType = thermalAnalysis.getAnalysisType();
+		String analysisTypeCad = getAnalysisTypeCad(analysisType);
+
+		// Create the main input file
+		File mainFile = new File(path + File.separatorChar + "main.inp");
+		mainFile.createNewFile();
+		Writer output = new FileWriter(mainFile);
+
+		// Write the geometry files
+		for (IBeanStructuralElementInstance ec : ecs) {
+			List<ThermalData> td = ec.getAll(ThermalData.class);
+			Visualisation visShape = ec.getFirst(Visualisation.class);
+			
+			// Attention! The main file must be created without including a
+			// cflux file when this conditions is true!
+			if ((td.size() > 0) && (visShape != null)) {
+				String uuid = ec.getUuid().replace("-", "_");
+				String name = ec.getName();
+				
+				output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".inp\n");
+			}
+		}
+
+		// Perform setup
+		output.write("\n*PHYSICAL CONSTANTS, ABSOLUTE ZERO = 0, STEFAN BOLTZMANN = 5.669E-8\n");
+		output.write("\n*INITIAL CONDITIONS,TYPE=TEMPERATURE\n");
+		output.write("Nall,293\n");
+		if (analysisTypeCad.equals("")) {
+			for (IBeanStructuralElementInstance ec : ecs) {
+				List<ThermalData> td = ec.getAll(ThermalData.class);
+				Visualisation visShape = ec.getFirst(Visualisation.class);
+				if ((td.size() > 0) && (visShape != null)) {
+					double initTemp = td.get(0).getThermalelementparameters().getInitialTemperature();
+					// node set with component name necessary!
+					output.write(ec.getName() + "," + initTemp + "\n");
+				}
+
+			}
+		}
+
+		// Write references to the material and the contacts file
+		output.write("\n*INCLUDE, INPUT=Materials.inp\n");
+		output.write("\n*INCLUDE, INPUT=add_contact.inp\n");
+		output.write("*INCLUDE, INPUT=contact_surfaces.inp\n");
+
+		// Write the simulation type
+		if (analysisTypeCad.equals("")) {
+			output.write("*TIME POINTS,NAME=T1,GENERATE\n");
+			output.write("0," + analysisType.getTotalTimeBean().getValueToBaseUnit() + ","
+					+ analysisType.getTimeStepBean().getValueToBaseUnit() + "\n");
+			output.write("\n*STEP\n");
+			output.write("\n*HEAT TRANSFER, DIRECT," + analysisTypeCad + "\n");
+			output.write(analysisType.getTimeStep() + "," + analysisType.getTotalTime() + "\n");
+		} else {
+			output.write("\n*STEP\n");
+			output.write("\n*HEAT TRANSFER, " + analysisTypeCad + "\n");
+		}
+
+		// Write face and body Heat Flux
+		for (IBeanStructuralElementInstance ec : ecs) {
+			List<ThermalData> td = ec.getAll(ThermalData.class);
+			Visualisation visShape = ec.getFirst(Visualisation.class);
+			if ((td.size() > 0) && (visShape != null)) {
+				String uuid = ec.getUuid().replace("-", "_");
+				String name = ec.getName();
+				
+				output.write("\n*DFLUX\n");
+				output.write("**INCLUDE, INPUT=" + name + "_" + uuid + ".dfl\n");
+				output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".bfl\n");
+				output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".hf\n");
+				output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".bc\n\n");
+			}
+		}
+
+		// Write radiation
+		output.write("\n*RADIATE\n");
+		for (IBeanStructuralElementInstance ec : ecs) {
+			List<ThermalData> td = ec.getAll(ThermalData.class);
+			Visualisation visShape = ec.getFirst(Visualisation.class);
+			if ((td.size() > 0) && (visShape != null)) {
+				String uuid = ec.getUuid().replace("-", "_");
+				String name = ec.getName();
+				
+				// write RADIATE here
+				output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".rad\n");
+			}
+		}
+
+		// Perform final post processing by writing necessary end statements
+		output.write("\n*NODE FILE\n");
+		output.write("NT\n");
+		output.write("*EL File\n");
+		output.write("HFL\n");
+		output.write("*END STEP");
+		output.close();
+	}
+}
