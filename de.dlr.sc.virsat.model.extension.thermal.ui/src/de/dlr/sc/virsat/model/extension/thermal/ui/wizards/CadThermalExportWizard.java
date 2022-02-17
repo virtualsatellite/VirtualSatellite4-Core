84c6ecc0503ab21a69ceaea1b21675f31935e792
franzTobiasDLR <tobias.franz@dlr.de>
2022-02-17 17:20:41 +0100

    Run actual exporter - Task #1052

--- de.dlr.sc.virsat.model.extension.thermal.ui/src/de/dlr/sc/virsat/model/extension/thermal/ui/wizards/CadThermalExportWizard.java
+++ de.dlr.sc.virsat.model.extension.thermal.ui/src/de/dlr/sc/virsat/model/extension/thermal/ui/wizards/CadThermalExportWizard.java
@@ -1,15 +1,25 @@
 package de.dlr.sc.virsat.model.extension.thermal.ui.wizards;
 
+import java.io.IOException;
+
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.DialogSettings;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.statushandlers.StatusManager;
 
+import de.dlr.sc.virsat.model.dvlm.categories.CategoryAssignment;
 import de.dlr.sc.virsat.model.extension.thermal.Activator;
+import de.dlr.sc.virsat.model.extension.thermal.cad.CadExporterThermal;
+import de.dlr.sc.virsat.model.extension.thermal.model.ThermalAnalysis;
 
 public class CadThermalExportWizard extends Wizard implements INewWizard {
 	
@@ -42,8 +52,27 @@ public class CadThermalExportWizard extends Wizard implements INewWizard {
 
 	@Override
 	public boolean performFinish() {
-
+		CategoryAssignment selectedCA = (CategoryAssignment) page.getSelection();
+		CadExporterThermal exporter = new CadExporterThermal(new ThermalAnalysis(selectedCA));
 		
+		String outputFilePath = page.getDestination();
+		
+		Job exportJob = new Job("Performing Thermal CAD Export") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				try {
+					exporter.writeCadInput(outputFilePath);
+					return Status.OK_STATUS;
+				} catch (IOException e) {
+					Status status = new Status(Status.ERROR, Activator.getPluginId(), 
+							"CadThermalExportWizard: Failed to perform export!", e);
+					StatusManager.getManager().handle(status, StatusManager.LOG | StatusManager.SHOW);
+					return Status.CANCEL_STATUS;
+				}
+			}
+		};
+		exportJob.schedule();
+
 		return true;
 	}
 	
