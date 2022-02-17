a4da7b4f009887824d9adb92ef6a4a0c9b9971d9
SaMuellerDLR <sa.mueller@dlr.de>
2022-02-17 17:28:59 +0100

    Fixed spotbugs errors - Task #1054

--- de.dlr.sc.virsat.model.extension.thermal/src/de/dlr/sc/virsat/model/extension/thermal/cad/CadExporterThermal.java
+++ de.dlr.sc.virsat.model.extension.thermal/src/de/dlr/sc/virsat/model/extension/thermal/cad/CadExporterThermal.java
@@ -101,6 +101,7 @@ public class CadExporterThermal {
 		writeCadVolumeHeatFluxInput(path);
 		writeCadBoundaryConditionsInput(path);
 		writeCadContacsInput(path);
+		writeCadMeshSizesInput(path);
 	}
 
 	/**
@@ -117,94 +118,94 @@ public class CadExporterThermal {
 		File mainFile = new File(path + File.separatorChar + "main.inp");
 		mainFile.createNewFile();
 
-		Writer output = new FileWriter(mainFile);
+		try (Writer output = new FileWriter(mainFile)) {
 
-		// Write the geometry files
-		for (IBeanStructuralElementInstance ec : ecs) {
-			ThermalData td = ec.getFirst(ThermalData.class);
-			Visualisation visShape = ec.getFirst(Visualisation.class);
-
-			// Attention! The main file must be created without including a
-			// cflux file when this conditions is true!
-			if ((td != null) && (visShape != null)) {
-				String uuid = ec.getUuid().replace("-", "_");
-				String name = ec.getName();
-
-				output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".inp\n");
-			}
-		}
-
-		// Perform setup
-		output.write("\n*PHYSICAL CONSTANTS, ABSOLUTE ZERO = 0, STEFAN BOLTZMANN = 5.669E-8\n");
-		output.write("\n*INITIAL CONDITIONS,TYPE=TEMPERATURE\n");
-		output.write("Nall,293\n");
-		if (analysisTypeCad.equals("")) {
+			// Write the geometry files
 			for (IBeanStructuralElementInstance ec : ecs) {
 				ThermalData td = ec.getFirst(ThermalData.class);
 				Visualisation visShape = ec.getFirst(Visualisation.class);
+
+				// Attention! The main file must be created without including a
+				// cflux file when this conditions is true!
 				if ((td != null) && (visShape != null)) {
-					double initTemp = td.getThermalelementparameters().getInitialTemperature();
-					// node set with component name necessary!
-					output.write(ec.getName() + "," + initTemp + "\n");
+					String uuid = ec.getUuid().replace("-", "_");
+					String name = ec.getName();
+
+					output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".inp\n");
 				}
 			}
-		}
 
-		// Write references to the material and the contacts file
-		output.write("\n*INCLUDE, INPUT=Materials.inp\n");
-		output.write("\n*INCLUDE, INPUT=add_contact.inp\n");
-		output.write("*INCLUDE, INPUT=contact_surfaces.inp\n");
-
-		// Write the simulation type
-		if (analysisTypeCad.equals("")) {
-			output.write("*TIME POINTS,NAME=T1,GENERATE\n");
-			output.write("0," + analysisType.getTotalTimeBean().getValueToBaseUnit() + ","
-					+ analysisType.getTimeStepBean().getValueToBaseUnit() + "\n");
-			output.write("\n*STEP\n");
-			output.write("\n*HEAT TRANSFER, DIRECT," + analysisTypeCad + "\n");
-			output.write(analysisType.getTimeStep() + "," + analysisType.getTotalTime() + "\n");
-		} else {
-			output.write("\n*STEP\n");
-			output.write("\n*HEAT TRANSFER, " + analysisTypeCad + "\n");
-		}
-
-		// Write face and body Heat Flux
-		for (IBeanStructuralElementInstance ec : ecs) {
-			List<ThermalData> td = ec.getAll(ThermalData.class);
-			Visualisation visShape = ec.getFirst(Visualisation.class);
-			if ((td.size() > 0) && (visShape != null)) {
-				String uuid = ec.getUuid().replace("-", "_");
-				String name = ec.getName();
-
-				output.write("\n*DFLUX\n");
-				output.write("**INCLUDE, INPUT=" + name + "_" + uuid + ".dfl\n");
-				output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".bfl\n");
-				output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".hf\n");
-				output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".bc\n\n");
+			// Perform setup
+			output.write("\n*PHYSICAL CONSTANTS, ABSOLUTE ZERO = 0, STEFAN BOLTZMANN = 5.669E-8\n");
+			output.write("\n*INITIAL CONDITIONS,TYPE=TEMPERATURE\n");
+			output.write("Nall,293\n");
+			if (analysisTypeCad.equals("")) {
+				for (IBeanStructuralElementInstance ec : ecs) {
+					ThermalData td = ec.getFirst(ThermalData.class);
+					Visualisation visShape = ec.getFirst(Visualisation.class);
+					if ((td != null) && (visShape != null)) {
+						double initTemp = td.getThermalelementparameters().getInitialTemperature();
+						// node set with component name necessary!
+						output.write(ec.getName() + "," + initTemp + "\n");
+					}
+				}
 			}
-		}
 
-		// Write radiation
-		output.write("\n*RADIATE\n");
-		for (IBeanStructuralElementInstance ec : ecs) {
-			List<ThermalData> td = ec.getAll(ThermalData.class);
-			Visualisation visShape = ec.getFirst(Visualisation.class);
-			if ((td.size() > 0) && (visShape != null)) {
-				String uuid = ec.getUuid().replace("-", "_");
-				String name = ec.getName();
+			// Write references to the material and the contacts file
+			output.write("\n*INCLUDE, INPUT=Materials.inp\n");
+			output.write("\n*INCLUDE, INPUT=add_contact.inp\n");
+			output.write("*INCLUDE, INPUT=contact_surfaces.inp\n");
 
-				// write RADIATE here
-				output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".rad\n");
+			// Write the simulation type
+			if (analysisTypeCad.equals("")) {
+				output.write("*TIME POINTS,NAME=T1,GENERATE\n");
+				output.write("0," + analysisType.getTotalTimeBean().getValueToBaseUnit() + ","
+						+ analysisType.getTimeStepBean().getValueToBaseUnit() + "\n");
+				output.write("\n*STEP\n");
+				output.write("\n*HEAT TRANSFER, DIRECT," + analysisTypeCad + "\n");
+				output.write(analysisType.getTimeStep() + "," + analysisType.getTotalTime() + "\n");
+			} else {
+				output.write("\n*STEP\n");
+				output.write("\n*HEAT TRANSFER, " + analysisTypeCad + "\n");
 			}
-		}
 
-		// Perform final post processing by writing necessary end statements
-		output.write("\n*NODE FILE\n");
-		output.write("NT\n");
-		output.write("*EL File\n");
-		output.write("HFL\n");
-		output.write("*END STEP");
-		output.close();
+			// Write face and body Heat Flux
+			for (IBeanStructuralElementInstance ec : ecs) {
+				List<ThermalData> td = ec.getAll(ThermalData.class);
+				Visualisation visShape = ec.getFirst(Visualisation.class);
+				if ((td.size() > 0) && (visShape != null)) {
+					String uuid = ec.getUuid().replace("-", "_");
+					String name = ec.getName();
+
+					output.write("\n*DFLUX\n");
+					output.write("**INCLUDE, INPUT=" + name + "_" + uuid + ".dfl\n");
+					output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".bfl\n");
+					output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".hf\n");
+					output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".bc\n\n");
+				}
+			}
+
+			// Write radiation
+			output.write("\n*RADIATE\n");
+			for (IBeanStructuralElementInstance ec : ecs) {
+				List<ThermalData> td = ec.getAll(ThermalData.class);
+				Visualisation visShape = ec.getFirst(Visualisation.class);
+				if ((td.size() > 0) && (visShape != null)) {
+					String uuid = ec.getUuid().replace("-", "_");
+					String name = ec.getName();
+
+					// write RADIATE here
+					output.write("*INCLUDE, INPUT=" + name + "_" + uuid + ".rad\n");
+				}
+			}
+
+			// Perform final post processing by writing necessary end statements
+			output.write("\n*NODE FILE\n");
+			output.write("NT\n");
+			output.write("*EL File\n");
+			output.write("HFL\n");
+			output.write("*END STEP");
+		}
 	}
 
 	/**
@@ -217,35 +218,35 @@ public class CadExporterThermal {
 		File materialFile = new File(path + File.separatorChar + "Materials.inp");
 		materialFile.createNewFile();
 
-		Writer output = new FileWriter(materialFile);
+		try (Writer output = new FileWriter(materialFile)) {
 
-		for (IBeanStructuralElementInstance ec : ecs) {
-			ThermalData td = ec.getFirst(ThermalData.class);
-			Visualisation visShape = ec.getFirst(Visualisation.class);
+			for (IBeanStructuralElementInstance ec : ecs) {
+				ThermalData td = ec.getFirst(ThermalData.class);
+				Visualisation visShape = ec.getFirst(Visualisation.class);
 
-			if ((td != null) && (visShape != null)) {
-				String name = ec.getName();
+				if ((td != null) && (visShape != null)) {
+					String name = ec.getName();
 
-				ThermalElementParameters tep = td.getThermalelementparameters();
-				// CHECKSTYLE:OFF
-				double thermalConductivity = tep.getPredefinedMaterial().getThermalConductivityBean()
-						.getValueToBaseUnit() * 1000;
-				double heatCapacity = tep.getPredefinedMaterial().getHeatCapacityBean().getValueToBaseUnit() * 1000000;
-				double densityBean = tep.getPredefinedMaterial().getDensityBean().getValueToBaseUnit() / 1000000000;
-				// CHECKSTYLE:ON
+					ThermalElementParameters tep = td.getThermalelementparameters();
+					// CHECKSTYLE:OFF
+					double thermalConductivity = tep.getPredefinedMaterial().getThermalConductivityBean()
+							.getValueToBaseUnit() * 1000;
+					double heatCapacity = tep.getPredefinedMaterial().getHeatCapacityBean().getValueToBaseUnit()
+							* 1000000;
+					double densityBean = tep.getPredefinedMaterial().getDensityBean().getValueToBaseUnit() / 1000000000;
+					// CHECKSTYLE:ON
 
-				output.write("\n*MATERIAL, NAME=" + name + "_material\n");
-				output.write("*CONDUCTIVITY\n");
-				output.write(thermalConductivity + "\n");
-				output.write("*SPECIFIC HEAT\n");
-				output.write(heatCapacity + "\n");
-				output.write("*DENSITY\n");
-				output.write(densityBean + "\n");
-				output.write("*SOLID SECTION,MATERIAL=" + name + "_material,ELSET=" + name + "\n\n");
+					output.write("\n*MATERIAL, NAME=" + name + "_material\n");
+					output.write("*CONDUCTIVITY\n");
+					output.write(thermalConductivity + "\n");
+					output.write("*SPECIFIC HEAT\n");
+					output.write(heatCapacity + "\n");
+					output.write("*DENSITY\n");
+					output.write(densityBean + "\n");
+					output.write("*SOLID SECTION,MATERIAL=" + name + "_material,ELSET=" + name + "\n\n");
+				}
 			}
 		}
-
-		output.close();
 	}
 
 	/**
@@ -265,17 +266,17 @@ public class CadExporterThermal {
 				File mainFile = new File(path + File.separatorChar + name + "_" + uuid + ".rad");
 				mainFile.createNewFile();
 
-				Writer output = new FileWriter(mainFile);
-				double emissivity = td.getThermalelementparameters().getPredefinedMaterial().getElementEmissivity();
-				output.write("0, " + emissivity + "\n");
+				try (Writer output = new FileWriter(mainFile)) {
+					double emissivity = td.getThermalelementparameters().getPredefinedMaterial().getElementEmissivity();
+					output.write("0, " + emissivity + "\n");
 
-				List<FaceRadiation> radiationList = td.getSinglefaceradiationaList();
-				for (FaceRadiation faceRadiation : radiationList) {
-					long freeCADFaceNumber = faceRadiation.getFreeCADFaceNumber();
-					double faceEmissivity = faceRadiation.getFaceEmissivity();
-					output.write(freeCADFaceNumber + ", " + faceEmissivity + "\n");
+					List<FaceRadiation> radiationList = td.getSinglefaceradiationaList();
+					for (FaceRadiation faceRadiation : radiationList) {
+						long freeCADFaceNumber = faceRadiation.getFreeCADFaceNumber();
+						double faceEmissivity = faceRadiation.getFaceEmissivity();
+						output.write(freeCADFaceNumber + ", " + faceEmissivity + "\n");
+					}
 				}
-				output.close();
 			}
 		}
 	}
@@ -299,13 +300,13 @@ public class CadExporterThermal {
 				File loadFile = new File(path + File.separatorChar + name + "_" + uuid + ".bfl");
 				loadFile.createNewFile();
 
-				Writer output = new FileWriter(loadFile);
-				ThermalElementParameters tep = td.getThermalelementparameters();
-				// CHECKSTYLE:OFF
-				String powerBalance = Double.toString(tep.getPowerBalance() * 1000000);
-				// CHECKSTYLE:ON
-				output.write(powerBalance);
-				output.close();
+				try (Writer output = new FileWriter(loadFile)) {
+					ThermalElementParameters tep = td.getThermalelementparameters();
+					// CHECKSTYLE:OFF
+					String powerBalance = Double.toString(tep.getPowerBalance() * 1000000);
+					// CHECKSTYLE:ON
+					output.write(powerBalance);
+				}
 			}
 		}
 	}
@@ -327,33 +328,31 @@ public class CadExporterThermal {
 
 				File ehfFile = new File(path + File.separatorChar + name + "_" + uuid + ".ehf");
 				ehfFile.createNewFile();
-				Writer ehfOutput = new FileWriter(ehfFile);
 
 				File bcFile = new File(path + File.separatorChar + name + "_" + uuid + ".bcf");
 				bcFile.createNewFile();
-				Writer bcOutput = new FileWriter(bcFile);
 
-				BoundaryConditions boundaryConditions = thermalAnalysis.getBoundaryConditions();
-				for (HeatFlowToFace ehf : boundaryConditions.getHeatflowface()) {
-					String ehfParentComponentUuid = ehf.getComponent().getParent().getUuid();
-					if (ehfParentComponentUuid.equals(ec.getUuid())) {
-						ehfOutput.write(ehf.getFreeCADFaceNumberHF() + ", " + ehf.getHeatFlow() + "\n");
+				try (Writer ehfOutput = new FileWriter(ehfFile); Writer bcOutput = new FileWriter(bcFile)) {
+
+					BoundaryConditions boundaryConditions = thermalAnalysis.getBoundaryConditions();
+					for (HeatFlowToFace ehf : boundaryConditions.getHeatflowface()) {
+						String ehfParentComponentUuid = ehf.getComponent().getParent().getUuid();
+						if (ehfParentComponentUuid.equals(ec.getUuid())) {
+							ehfOutput.write(ehf.getFreeCADFaceNumberHF() + ", " + ehf.getHeatFlow() + "\n");
+						}
 					}
-				}
 
-				for (TemperatureBoundary tb : boundaryConditions.getBoundaries()) {
-					String tbParentComponentUuid = tb.getComponent().getParent().getUuid();
-					if (tbParentComponentUuid.equals(ec.getUuid())) {
-						if (tb.getBoundaryType().equals(TemperatureBoundary.BOUNDARYTYPE_Face_NAME)) {
-							bcOutput.write(tb.getFreeCADFaceNumberBC() + ", " + tb.getBoundaryTemperature() + "\n");
-						} else if (tb.getBoundaryType().equals(TemperatureBoundary.BOUNDARYTYPE_Volume_NAME)) {
-							bcOutput.write(tb.getBoundaryTemperature() + "\n");
+					for (TemperatureBoundary tb : boundaryConditions.getBoundaries()) {
+						String tbParentComponentUuid = tb.getComponent().getParent().getUuid();
+						if (tbParentComponentUuid.equals(ec.getUuid())) {
+							if (tb.getBoundaryType().equals(TemperatureBoundary.BOUNDARYTYPE_Face_NAME)) {
+								bcOutput.write(tb.getFreeCADFaceNumberBC() + ", " + tb.getBoundaryTemperature() + "\n");
+							} else if (tb.getBoundaryType().equals(TemperatureBoundary.BOUNDARYTYPE_Volume_NAME)) {
+								bcOutput.write(tb.getBoundaryTemperature() + "\n");
+							}
 						}
 					}
 				}
-
-				ehfOutput.close();
-				bcOutput.close();
 			}
 		}
 	}
@@ -369,45 +368,42 @@ public class CadExporterThermal {
 
 		File addContactFile = new File(path + File.separatorChar + "add_contact.inp");
 		addContactFile.createNewFile();
-		Writer output = new FileWriter(addContactFile);
 
 		File contactValidationMaster = new File(path + File.separatorChar + "validateContactsMaster.txt");
 		contactValidationMaster.createNewFile();
-		Writer outputMaster = new FileWriter(contactValidationMaster);
 
 		File contactValidationSlave = new File(path + File.separatorChar + "validateContactsSlave.txt");
 		contactValidationSlave.createNewFile();
-		Writer outputSlave = new FileWriter(contactValidationSlave);
 
-		List<ThermalInterface> tis = tcs.getThermalinterfacelist();
+		try (Writer output = new FileWriter(addContactFile);
+				Writer outputMaster = new FileWriter(contactValidationMaster);
+				Writer outputSlave = new FileWriter(contactValidationSlave);) {
+			List<ThermalInterface> tis = tcs.getThermalinterfacelist();
 
-		for (int i = 0; i < tis.size(); ++i) {
-			ThermalInterface ti = tis.get(i);
-			List<ThermalPort> tp = ti.getContacts();
+			for (int i = 0; i < tis.size(); ++i) {
+				ThermalInterface ti = tis.get(i);
+				List<ThermalPort> tp = ti.getContacts();
 
-			double tcc = ti.getThermalContactConductivity();
-			output.write("\n*SURFACE INTERACTION, NAME=SI" + i + "\n");
-			output.write("*SURFACE BEHAVIOR, PRESSURE-OVERCLOSURE=LINEAR\n");
-			output.write("1e10\n");
-			output.write("\n*GAP CONDUCTANCE\n");
-			output.write(tcc + ",,300\n");
+				double tcc = ti.getThermalContactConductivity();
+				output.write("\n*SURFACE INTERACTION, NAME=SI" + i + "\n");
+				output.write("*SURFACE BEHAVIOR, PRESSURE-OVERCLOSURE=LINEAR\n");
+				output.write("1e10\n");
+				output.write("\n*GAP CONDUCTANCE\n");
+				output.write(tcc + ",,300\n");
 
-			IBeanStructuralElementInstance component1 = tp.get(0).getPortComponent().getParent();
-			IBeanStructuralElementInstance component2 = tp.get(1).getPortComponent().getParent();
+				IBeanStructuralElementInstance component1 = tp.get(0).getPortComponent().getParent();
+				IBeanStructuralElementInstance component2 = tp.get(1).getPortComponent().getParent();
 
-			String component1UUID = component1.getUuid().replace('-', '_');
-			String component2UUID = component2.getUuid().replace('-', '_');
+				String component1UUID = component1.getUuid().replace('-', '_');
+				String component2UUID = component2.getUuid().replace('-', '_');
 
-			double maxMeshElementSize0 = ti.getContactMaxMeshElementSize0Bean().getValueToBaseUnit();
-			double maxMeshElementSize1 = ti.getContactMaxMeshElementSize1Bean().getValueToBaseUnit();
+				double maxMeshElementSize0 = ti.getContactMaxMeshElementSize0Bean().getValueToBaseUnit();
+				double maxMeshElementSize1 = ti.getContactMaxMeshElementSize1Bean().getValueToBaseUnit();
 
-			outputMaster.write(component1.getName() + "_" + component1UUID + "," + maxMeshElementSize0 + "\n");
-			outputSlave.write(component2.getName() + "_" + component2UUID + "," + maxMeshElementSize1 + "\n");
+				outputMaster.write(component1.getName() + "_" + component1UUID + "," + maxMeshElementSize0 + "\n");
+				outputSlave.write(component2.getName() + "_" + component2UUID + "," + maxMeshElementSize1 + "\n");
+			}
 		}
-
-		output.close();
-		outputMaster.close();
-		outputSlave.close();
 	}
 
 	/**
@@ -419,32 +415,34 @@ public class CadExporterThermal {
 	public void writeCadMeshSizesInput(String path) throws IOException {
 		File meshSizeFile = new File(path + File.separatorChar + "meshSizes.txt");
 		meshSizeFile.createNewFile();
-		Writer meshSizeOutput = new FileWriter(meshSizeFile);
-		List<ComponentMeshSize> cmss = thermalAnalysis.getMeshsizes();
 		
-		for (IBeanStructuralElementInstance ec : ecs) {
-			ThermalData td = ec.getFirst(ThermalData.class);
-			Visualisation visShape = ec.getFirst(Visualisation.class);
-			Boolean meshSizeFound = false;
+		try (Writer meshSizeOutput = new FileWriter(meshSizeFile)) {
+			List<ComponentMeshSize> cmss = thermalAnalysis.getMeshsizes();
 
-			if ((td != null) && (visShape != null)) {
-				String name = ec.getName();
-				String uuid = ec.getUuid().replace("-", "_");
-				for (ComponentMeshSize cms : cmss) {
-					if (cms.getMeshComponent().getParent().getName().equals(ec.getName())) {
-						// CHECKSTYLE:OFF
-						double maximumCharacteristicMeshLength = cms.getMaximumCharacteristicMeshLengthBean().getValueToBaseUnit() * 1000;
-						// CHECKSTYLE:ON
-						meshSizeOutput.write(name + "_" + uuid + "," + maximumCharacteristicMeshLength + "\n");
-						meshSizeFound = true;
+			for (IBeanStructuralElementInstance ec : ecs) {
+				ThermalData td = ec.getFirst(ThermalData.class);
+				Visualisation visShape = ec.getFirst(Visualisation.class);
+				Boolean meshSizeFound = false;
+
+				if ((td != null) && (visShape != null)) {
+					String name = ec.getName();
+					String uuid = ec.getUuid().replace("-", "_");
+					for (ComponentMeshSize cms : cmss) {
+						if (cms.getMeshComponent().getParent().getName().equals(ec.getName())) {
+							// CHECKSTYLE:OFF
+							double maximumCharacteristicMeshLength = cms.getMaximumCharacteristicMeshLengthBean()
+									.getValueToBaseUnit() * 1000;
+							// CHECKSTYLE:ON
+							meshSizeOutput.write(name + "_" + uuid + "," + maximumCharacteristicMeshLength + "\n");
+							meshSizeFound = true;
+						}
 					}
-				}
 
-				if (!meshSizeFound) {
-					meshSizeOutput.write(name + "_" + uuid + ",0\n");
+					if (!meshSizeFound) {
+						meshSizeOutput.write(name + "_" + uuid + ",0\n");
+					}
 				}
 			}
 		}
-		meshSizeOutput.close();
 	}
 }
