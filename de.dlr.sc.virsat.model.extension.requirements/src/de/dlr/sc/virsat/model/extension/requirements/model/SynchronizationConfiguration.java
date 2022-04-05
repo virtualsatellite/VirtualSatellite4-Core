bc50e201544bc594359bb733509b66e51b1de418
Anna-Lena-DLR <anna-lena.gefke@dlr.de>
2022-04-05 16:46:08 +0200

    Add synchronization configuration to concept (#743)

---
+++ de.dlr.sc.virsat.model.extension.requirements/src/de/dlr/sc/virsat/model/extension/requirements/model/SynchronizationConfiguration.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2019 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.sc.virsat.model.extension.requirements.model;
+
+// *****************************************************************
+// * Import Statements
+// *****************************************************************
+import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
+import javax.xml.bind.annotation.XmlType;
+import de.dlr.sc.virsat.model.dvlm.categories.CategoryAssignment;
+
+// *****************************************************************
+// * Class Declaration
+// *****************************************************************
+
+@XmlType(name = ASynchronizationConfiguration.FULL_QUALIFIED_CATEGORY_NAME)
+/**
+ * Auto Generated Class inheriting from Generator Gap Class
+ * 
+ * This class is generated once, do your changes here
+ * 
+ * 
+ * 
+ */
+public  class SynchronizationConfiguration extends ASynchronizationConfiguration {
+	
+	/**
+	 * Constructor of Concept Class
+	 */
+	public SynchronizationConfiguration() {
+		super();
+	}
+
+	/**
+	 * Constructor of Concept Class which will instantiate 
+	 * a CategoryAssignment in the background from the given concept
+	 * @param concept the concept where it will find the correct Category to instantiate from
+	 */
+	public SynchronizationConfiguration(Concept concept) {
+		super(concept);
+	}	
+
+	/**
+	 * Constructor of Concept Class that can be initialized manually by a given Category Assignment
+	 * @param categoryAssignment The category Assignment to be used for background initialization of the Category bean
+	 */
+	public SynchronizationConfiguration(CategoryAssignment categoryAssignment) {
+		super(categoryAssignment);
+	}
+}
