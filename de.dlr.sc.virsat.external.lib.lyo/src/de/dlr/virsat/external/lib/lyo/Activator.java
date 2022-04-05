ce0d0808a60540b2da7729c2e995a4ec4d1ba0d4
Anna-Lena-DLR <anna-lena.gefke@dlr.de>
2022-04-05 10:10:26 +0200

    Add lyo lib (#743)

---
+++ de.dlr.sc.virsat.external.lib.lyo/src/de/dlr/virsat/external/lib/lyo/Activator.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2019 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package de.dlr.virsat.external.lib.lyo;
+
+import de.dlr.sc.virsat.external.lib.LibPlugin;
+
+/**
+ * The plugin root class which is the entry point for the OSGi bundle activation
+ * @author fisc_ph
+ *
+ */
+public class Activator extends LibPlugin {
+
+	
+}
