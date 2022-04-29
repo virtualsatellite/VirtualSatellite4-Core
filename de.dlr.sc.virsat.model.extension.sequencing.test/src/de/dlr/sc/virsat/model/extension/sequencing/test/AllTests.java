7cf7bb0ed463f15dd0a31c6c0273f64314719b03
maur_pa <maur_pa@SC-010203.intra.dlr.de>
2022-04-29 13:42:32 +0200

    Added Test Cases to AllTests (Task -#992)

--- de.dlr.sc.virsat.model.extension.sequencing.test/src/de/dlr/sc/virsat/model/extension/sequencing/test/AllTests.java
+++ de.dlr.sc.virsat.model.extension.sequencing.test/src/de/dlr/sc/virsat/model/extension/sequencing/test/AllTests.java
@@ -17,6 +17,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import de.dlr.sc.virsat.model.extension.sequencing.model.EventToInterfaceEndMappingTest;
+import de.dlr.sc.virsat.model.extension.sequencing.model.EventToOutputMappingTest;
+import de.dlr.sc.virsat.model.extension.sequencing.model.SequenceEntryTest;
+import de.dlr.sc.virsat.model.extension.sequencing.model.SequenceTest;
 import junit.framework.JUnit4TestAdapter;
 
 
@@ -25,8 +29,11 @@ import junit.framework.JUnit4TestAdapter;
  */
 @RunWith(Suite.class)
 
-@SuiteClasses({		
-				})
+@SuiteClasses({
+	EventToInterfaceEndMappingTest.class,
+	EventToOutputMappingTest.class,
+	SequenceEntryTest.class,
+	SequenceTest.class})
 
 /**
  * 
