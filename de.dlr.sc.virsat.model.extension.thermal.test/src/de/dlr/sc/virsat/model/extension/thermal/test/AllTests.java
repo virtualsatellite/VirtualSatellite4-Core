3c621bfae73091a864f585c7940542a3b712fafe
maur_pa <maur_pa@SC-010203.intra.dlr.de>
2022-05-04 15:33:33 +0200

    Add Test to Thermal (Test -#992)

--- de.dlr.sc.virsat.model.extension.thermal.test/src/de/dlr/sc/virsat/model/extension/thermal/test/AllTests.java
+++ de.dlr.sc.virsat.model.extension.thermal.test/src/de/dlr/sc/virsat/model/extension/thermal/test/AllTests.java
@@ -15,6 +15,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import de.dlr.sc.virsat.model.extension.thermal.cad.CadExporterThermalTest;
 import de.dlr.sc.virsat.model.extension.thermal.cad.CadImporterThermalTest;
+import de.dlr.sc.virsat.model.extension.thermal.model.AnalysisResultTest;
+import de.dlr.sc.virsat.model.extension.thermal.model.AnalysisTypeTest;
 import de.dlr.sc.virsat.model.extension.thermal.validator.ValidatorTest;
 import junit.framework.JUnit4TestAdapter;
 
@@ -26,7 +28,9 @@ import junit.framework.JUnit4TestAdapter;
 @SuiteClasses({
 	ValidatorTest.class,
 	CadExporterThermalTest.class,
-	CadImporterThermalTest.class})
+	CadImporterThermalTest.class,
+	AnalysisResultTest.class,
+	AnalysisTypeTest.class})
 
 public class AllTests {
 	
