d67dec4c5a09e2833f85d36083e22154221e1de3
franzTobiasDLR <tobias.franz@dlr.de>
2022-07-05 14:49:45 +0200

    Fix method arcording to @andriikovalov-dlr comment - Task #1107

--- de.dlr.sc.virsat.model.extension.requirements.ui/src/de/dlr/sc/virsat/model/extension/requirements/ui/snippet/UiSnippetSectionExistenceVerification.java
+++ de.dlr.sc.virsat.model.extension.requirements.ui/src/de/dlr/sc/virsat/model/extension/requirements/ui/snippet/UiSnippetSectionExistenceVerification.java
@@ -9,15 +9,11 @@
  *******************************************************************************/
 package de.dlr.sc.virsat.model.extension.requirements.ui.snippet;
 
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.domain.EditingDomain;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
-import de.dlr.sc.virsat.model.dvlm.categories.Category;
 import de.dlr.sc.virsat.model.dvlm.categories.propertydefinitions.AProperty;
-import de.dlr.sc.virsat.model.dvlm.concepts.util.ActiveConceptHelper;
 import de.dlr.sc.virsat.model.extension.requirements.model.ExistenceVerification;
 import de.dlr.sc.virsat.uiengine.ui.editor.snippets.IUiSnippet;
 
@@ -33,21 +29,17 @@ import de.dlr.sc.virsat.uiengine.ui.editor.snippets.IUiSnippet;
 public class UiSnippetSectionExistenceVerification extends AUiSnippetSectionExistenceVerification implements IUiSnippet {
 	
 	@Override
-	public void createSwt(FormToolkit toolkit, EditingDomain editingDomain, Composite composite, EObject initModel) {
-		super.createSwt(toolkit, editingDomain, composite, initModel);
-		initializeHelperForModel(initModel);
-
-		Composite sectionBody = createSectionBody(toolkit, SECTION_HEADING, null, 1);
-		sectionBody.setLayout(new GridLayout(UI_LAYOUT_NR_COLUMNS, false));
-
-		Category viewerCategory = acHelper.getCategory(conceptId, categoryId);
-
-		// Don't show property elementsToBeVerified as it is not relvant for the user (only for verification)
-		for (AProperty property : ActiveConceptHelper.getNonArrayProperties(viewerCategory)) {
-			if (!property.getName().equals(ExistenceVerification.PROPERTY_ELEMENTTOBEVERIFIED)) {
-				createCommonPropertyWidgets(toolkit, sectionBody, property);
-				createCustomPropertyWidgets(toolkit, editingDomain, sectionBody, property);
-			}
+	protected void createCommonPropertyWidgets(FormToolkit toolkit, Composite sectionBody, AProperty property) {
+		if (!property.getName().equals(ExistenceVerification.PROPERTY_ELEMENTTOBEVERIFIED)) {
+			super.createCommonPropertyWidgets(toolkit, sectionBody, property);
+		}
+	}
+	
+	@Override
+	protected void createCustomPropertyWidgets(FormToolkit toolkit, EditingDomain editingDomain, Composite sectionBody,
+			AProperty property) {
+		if (!property.getName().equals(ExistenceVerification.PROPERTY_ELEMENTTOBEVERIFIED)) {
+			super.createCustomPropertyWidgets(toolkit, editingDomain, sectionBody, property);
 		}
 	}
 	
